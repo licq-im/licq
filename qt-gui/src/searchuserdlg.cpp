518da1eff2842df67c294fd50a384a43883a6b33
Graham Roff <graham@unknown>
1999-11-14 01:59:03 +0000

    Moved include icqd.h to the code from the header.

--- qt-gui/src/searchuserdlg.cpp
+++ qt-gui/src/searchuserdlg.cpp
@@ -3,17 +3,17 @@
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or
     (at your option) any later version.
- 
+
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
- 
+
     You should have received a copy of the GNU General Public License
     along with this program; if not, write to the Free Software
     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- 
-*/ 
+
+*/
 
 // Search user code base written by Alan Penner (apenner@andrew.cmu.edu)
 // modified by Graham Roff
@@ -33,6 +33,8 @@
 #include <iostream.h>
 
 #include "searchuserdlg.h"
+#include "user.h"
+#include "icqd.h"
 
 
 SearchUserView::SearchUserView(QWidget *parent, char *name)
