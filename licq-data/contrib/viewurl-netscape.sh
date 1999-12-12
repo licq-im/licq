104406991d1a2aaf5d9785c31b97d18300602e39
Graham Roff <graham@unknown>
1999-12-12 18:37:22 +0000

    improved script by Brian Hunt

--- licq-data/contrib/viewurl-netscape.sh
+++ licq-data/contrib/viewurl-netscape.sh
@@ -1,18 +1,43 @@
 #!/bin/sh
 #
-# A shell script to view a url using Netscape
-# Checks to see if Netscape is running, and if so passes the URL
-# to it, otherwise starts up a new copy.
-# Set the NETSCAPE variable to where your netscape binary is or
-# leave it as is if the binary is in your path.
-# 
-# Graham Roff, February 1999.
+# Brian Hunt ( bmh at canada.com ), December 1999
+#
+# set NETSCAPE to your netscape binary.
+# set URLHOME to your default homepage.
+# --
+# (): Open new window, at:
+# 	(1) specified link
+#	(2) home page given in URLHOME
+# Conditions of execution:
+# Netscape lock file exists (~/.netscape/lock):
+#	(1) netscape running
+# 	-> Open new window
+#	(2) netscape not running
+#		(as per `ps` , which echos netscape's pids)
+#	  delete ~/.netscape/lock 
+#	-> Open new window
+# Netscape lockfile does not exist;
+#	-> Open new window.
 #
 
 NETSCAPE=netscape
+URLHOME='http://www.linux.org'
+
+if [ "$1" = "" ] ; then
+  URL=$URLHOME
+else
+  URL=$1
+fi
 
 if [ -h ${HOME}/.netscape/lock ]; then
-  ${NETSCAPE} -remote openURL\("$1"\,new_window\)
+  if ps -e | grep netscape ; then
+     ${NETSCAPE} -remote openURL\("$URL"\,new_window\) ;
+  else
+     rm ${HOME}/.netscape/lock ;
+     ${NETSCAPE} "$URL"
+  fi
 else
-  ${NETSCAPE} "$1"
+  ${NETSCAPE} "$URL"
 fi
+
+
