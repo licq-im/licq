372d85dbc1ac8fbea52bfd8902f5099c6933db09
Jon Keating <jon@licq.org>
2000-02-04 04:34:33 +0000

    A little better system messages

--- jons-gtk-gui/src/system_message.cpp
+++ jons-gtk-gui/src/system_message.cpp
@@ -27,6 +27,7 @@
 void system_message_window()
 {
 	GtkWidget *button;
+	GtkWidget *scroll;
 	GtkWidget *v_box;
 	struct system_message *sm = g_new0(struct system_message, 1);
 	const gchar *title = "Incoming System Message";
@@ -40,43 +41,52 @@ void system_message_window()
 	/* The box */
 	v_box = gtk_vbox_new(FALSE, 5);
 
+	/* The scrolling window */
+	scroll = gtk_scrolled_window_new(NULL, NULL);
+	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scroll),
+				       GTK_POLICY_NEVER,
+				       GTK_POLICY_AUTOMATIC);
+
 	/* The text box to display messages */
 	sm->text = gtk_text_new(NULL, NULL);
 	gtk_text_set_editable(GTK_TEXT(sm->text), FALSE);
+	gtk_text_set_word_wrap(GTK_TEXT(sm->text), TRUE);
+	gtk_text_set_line_wrap(GTK_TEXT(sm->text), TRUE);
 	gtk_widget_set_usize(sm->text, 300, 100);
+	gtk_container_add(GTK_CONTAINER(scroll), sm->text);
 
 	/* Dispaly what type of system message it is */
 	switch(event->SubCommand())
 	{
 	case ICQ_CMDxSUB_REQxAUTH:
 		gtk_text_insert(GTK_TEXT(sm->text), 0, 0, 0,
-			        "Authorization Request\n", -1);
+			        "Authorization Request\n-------------------\n\n", -1);
 		menu_system_auth_user(NULL, ((CEventAuthReq *)event)->Uin());
 		break;
 	
 	case ICQ_CMDxSUB_AUTHORIZED:
 		gtk_text_insert(GTK_TEXT(sm->text), 0, 0, 0,
-				"Authorization Granted\n", -1);
+				"Authorization Granted\n--------------------\n\n", -1);
 		break;
 
 	case ICQ_CMDxSUB_ADDEDxTOxLIST:
 		gtk_text_insert(GTK_TEXT(sm->text), 0, 0, 0,
-				"Added To Contact List\n", -1);
+				"Added To Contact List\n-------------------\n\n", -1);
 		break;
 
 	case ICQ_CMDxSUB_WEBxPANEL:
 		gtk_text_insert(GTK_TEXT(sm->text), 0, 0, 0,
-				"Web Panel\n", -1);
+				"Web Panel\n--------\n\n", -1);
 		break;
 
 	case ICQ_CMDxSUB_EMAILxPAGER:
 		gtk_text_insert(GTK_TEXT(sm->text), 0, 0, 0,
-				"E-mail Pager\n", -1);
+				"E-mail Pager\n-----------\n\n", -1);
 		break;
 
 	case ICQ_CMDxSUB_CONTACTxLIST:
 		gtk_text_insert(GTK_TEXT(sm->text), 0, 0, 0,
-				"Contact\n", -1);
+				"Contact\n-------\n\n", -1);
 		break;
 
 	default:
@@ -90,7 +100,7 @@ void system_message_window()
 	gUserManager.DropOwner();
 
 	/* Pack it */
-	gtk_box_pack_start(GTK_BOX(v_box), sm->text, FALSE, FALSE, 0);
+	gtk_box_pack_start(GTK_BOX(v_box), scroll, FALSE, FALSE, 0);
 
 	/* The close button */
 	button = gtk_button_new_with_label("Close");
@@ -107,6 +117,7 @@ void system_message_window()
 	/* Add the v_box to the window and show the widgets */
 	gtk_container_add(GTK_CONTAINER(sm->window), v_box);
 	gtk_widget_show_all(sm->window);
+	gtk_window_set_position(GTK_WINDOW(sm->window), GTK_WIN_POS_CENTER);
 
 	contact_list_refresh();
 	system_status_refresh();
