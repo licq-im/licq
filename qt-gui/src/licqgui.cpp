a8d00f89a4902d7ffce2c344171b876d78b6c485
Dirk Mueller <daywalker@unknown>
2001-03-06 19:20:13 +0000

    tooltip fading causes crashes on NVIDIA crap

--- qt-gui/src/licqgui.cpp
+++ qt-gui/src/licqgui.cpp
@@ -246,10 +246,6 @@ CLicqGui::CLicqGui(int argc, char **argv)
   m_szIcons = strdup(iconsName);
   m_bStartHidden = bStartHidden;
 
-#if QT_VERSION >= 220
-  QApplication::setEffectEnabled(Qt::UI_FadeTooltip);
-#endif
-
   // Try and load a translation
   char *p;
 #ifndef HAVE_LOCALE_H
