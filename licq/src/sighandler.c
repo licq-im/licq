6246b9d6052300f098e10142fc91637dc0b1e3cb
Dirk Mueller <daywalker@unknown>
2001-03-14 19:41:58 +0000

    include the right header files so it has a chance to be portable

--- licq/src/sighandler.c
+++ licq/src/sighandler.c
@@ -7,7 +7,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
-#include <wait.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #if __GLIBC__ == 2 && __GLIBC_MINOR__ >= 1
 #define HAVE_BACKTRACE
