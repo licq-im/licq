a1f487e261d90d4a9fb44cd4ee11d87e963897d8
Dirk Mueller <daywalker@unknown>
2002-05-04 15:50:06 +0000

    more fixes from Toastie

--- qt-gui/src/userbox.cpp
+++ qt-gui/src/userbox.cpp
@@ -1278,7 +1278,7 @@ void CUserView::maybeTip(const QPoint& c)
       if (u->AutoResponse() && *u->AutoResponse() &&
           item->m_nStatus != ICQ_STATUS_OFFLINE &&
           item->m_nStatus != ICQ_STATUS_ONLINE)
-        s += tr("<br><u>Auto Response:</u>") + QStyleSheet::convertFromPlainText(codec->toUnicode(u->AutoResponse()));
+        s += tr("<br><u>Auto Response:</u>") + QStyleSheet::convertFromPlainText(codec->toUnicode(u->AutoResponse()), QStyleSheetItem::WhiteSpaceNormal);
 
       gUserManager.DropUser(u);
     }
