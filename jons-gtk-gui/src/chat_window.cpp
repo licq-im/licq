ee8c235bb84f071459313471b5394db801b37997
Jon Keating <jon@licq.org>
2003-01-07 18:50:17 +0000

    New ChangeFontFamily parameters.

--- jons-gtk-gui/src/chat_window.cpp
+++ jons-gtk-gui/src/chat_window.cpp
@@ -1404,7 +1404,7 @@ void font_dlg_ok(GtkWidget *widget, gpointer _cw)
 
 		g_print("Final: %s\n", cw->l_font_name);
 
-		cw->chatman->ChangeFontFamily(cw->l_font_name);
+		cw->chatman->ChangeFontFamily(cw->l_font_name, ENCODING_DEFAULT, STYLE_DONTCARE);
 	}
 
 	GdkFont *font_tmp = gtk_font_selection_dialog_get_font(
