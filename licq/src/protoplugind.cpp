4d803458bd28f30eaf66db9f14a261645188cbed
Jon Keating <jon@licq.org>
2003-04-08 05:49:45 +0000

    Add corresponding .cpp file for CProtoPlugin

---
+++ licq/src/protoplugind.cpp
@@ -0,0 +1,45 @@
+#include "licq_protoplugind.h"
+#include "licq_constants.h"
+
+#include <unistd.h>
+
+class CSignal;
+
+CProtoPlugin::CProtoPlugin(const char *_szLibName)
+{
+  m_szLibName = _szLibName ? strdup(_szLibName) : 0;
+  pthread_mutex_init(&mutex_signals, NULL);
+  pipe(pipe_plugin);
+}
+
+CProtoPlugin::~CProtoPlugin()
+{
+  if (m_szLibName)
+    free(m_szLibName);
+}
+
+bool CProtoPlugin::CompareThread(pthread_t t)
+{
+  return pthread_equal(t, thread_plugin);
+}
+
+void CProtoPlugin::PushSignal(CSignal *s)
+{
+  pthread_mutex_lock(&mutex_signals);
+  list_signals.push_back(s);
+  pthread_mutex_unlock(&mutex_signals);
+  write(pipe_plugin[PIPE_WRITE], "S", 1);
+}
+
+CSignal *CProtoPlugin::PopSignal()
+{
+  CSignal *s = NULL;
+  pthread_mutex_lock(&mutex_signals);
+  if (list_signals.size() != 0)
+  {
+    s = list_signals.front();
+    list_signals.pop_front();
+  }
+  pthread_mutex_unlock(&mutex_signals);
+  return s;
+}
