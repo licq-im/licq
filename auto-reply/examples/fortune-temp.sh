b24bafce627a85d6c0c8cde57f5f2c5e7869c657
Thomas Reitelbach <dreamforce2@unknown>
2004-01-06 14:13:45 +0000

    a nice example script for the auto-reply plugin. Thanks to Eli Hayoon #124650632

---
+++ auto-reply/examples/fortune-temp.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# This requires
+# Arguments = '$a' $i
+# PassMessage = 1
+# PassMessage is 1 so we could read the user message.
+# Arguments = '$a' (getting user Alias, make sure you use ' ') $i ( getting user IP address)
+# Writen by Elius - www.buga-designs.com - ICQ: 124650632
+
+
+
+# This line store the message the user sent to var A.
+A=`tee`
+
+# Checks for the user input and display the data according to it.
+if [ $A = "1" ]; then
+fortune SP
+else
+        if [ $A = "2" ]; then
+        fortune
+        else
+                if [ $A = "3" ]; then
+                cpu_temp=$(sensors | tail -n 7 | head -1 | cut -d "+" -f 2 | cut -d " " -f 1)
+                mb_temp=$(sensors | tail -n 8 | head -1 | cut -d "+" -f 2 | cut -d " " -f 1)
+                echo "CPU : $cpu_temp"
+                echo "Chipset : $mb_temp"
+                else
+                        if [ $A = "4" ]; then
+                        ping_time=$(ping -c 1 $2 | head -2 | cut -d "=" -f 4,9 | tail -n 1)
+                        echo "Ping takes $ping_time"
+                        else
+                                echo " Hi $1"
+                                echo " 1 - south park fortune"
+                                echo " 2 - fortune"
+                                echo " 3 - System temperature"
+                        echo " 4 - Ping"
+                        echo " Any other char - menu"
+                        fi
+                fi
+        fi
+fi
