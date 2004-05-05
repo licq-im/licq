c5ab3b8b17d3f0ea4123a340ec26d812507e8318
Jon Keating <jon@licq.org>
2004-05-05 10:37:16 +0000

    Oops, get AIM working again

--- licq/src/icqpacket.cpp
+++ licq/src/icqpacket.cpp
@@ -852,7 +852,7 @@ CPU_CapabilitySettings::CPU_CapabilitySettings()
   buffer->PackTLV(0x05, 0x40, data);
 #else
 
-  char data[4][CAP_LENGTH];
+  char data[5][CAP_LENGTH];
   m_nSize += 4 + sizeof(data);
   InitBuffer();
 
@@ -860,6 +860,7 @@ CPU_CapabilitySettings::CPU_CapabilitySettings()
   memcpy(data[1], ICQ_CAPABILITY_SRVxRELAY, CAP_LENGTH);
   memcpy(data[2], ICQ_CAPABILITY_TYPING, CAP_LENGTH);
   memcpy(data[3], ICQ_CAPABILITY_LICQxVER, CAP_LENGTH);
+  memcpy(data[4], ICQ_CAPABILITY_AIMxINTER, CAP_LENGTH);
   data[3][12] = INT_VERSION / 1000;
   data[3][13] = (INT_VERSION / 10) % 1000;
   data[3][14] = INT_VERSION % 10;
