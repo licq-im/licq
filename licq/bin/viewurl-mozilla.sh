e010917cb1becdbc9a690ce806f7ddf6a7277b88
Jon Keating <jon@licq.org>
2004-08-11 01:28:09 +0000

    Fix for URLs with spaces.

--- licq/bin/viewurl-mozilla.sh
+++ licq/bin/viewurl-mozilla.sh
@@ -55,9 +55,9 @@ fi
 # check if mozilla is running
 if [ -z "`${MOZILLA} -remote ping\(\) >& /dev/stdout`" ]; then
   # mozilla is running, open the URL there
-  ${MOZILLA} -remote openurl\(${OPENSTRING}\) &
+  ${MOZILLA} -remote "openurl\(${OPENSTRING}\)" &
 else
   # not running, open a new browser
-  ${MOZILLA} ${URL} &
+  ${MOZILLA} "${URL}" &
 fi
 
