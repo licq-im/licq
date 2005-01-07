35905510119193d2925e3fdf23d01409107601d3
Jon Keating <jon@licq.org>
2005-01-07 02:35:15 +0000

    patches for the new GPGME API. Now we reuquire GPGME 0.4.2 or higher. Add a configure check for this instead of our old hack. Thanks to Richard and Robert for their patches to the licq-devel ML.

--- licq/src/gpg.cpp
+++ licq/src/gpg.cpp
@@ -41,24 +41,25 @@ char *CGPGHelper::Decrypt(const char *szCipher)
   if (!mCtx) return 0;
 
   size_t nRead = 0;
-  GpgmeData cipher, plain;
+  gpgme_data_t cipher, plain;
 
   CGPGMEMutex mutex;
   if (!mutex.Lock()) return 0;
-  if (gpgme_data_new(&cipher) != GPGME_No_Error) return 0;
+  if (gpgme_data_new(&cipher) != GPG_ERR_NO_ERROR) return 0;
   char *buf = strdup(szCipher);
-  GpgmeError err;
+  gpgme_error_t err;
   gpgme_data_write(cipher, buf, strlen(buf));
   free(buf);
 
-  if (gpgme_data_new(&plain) != GPGME_No_Error)
+  if (gpgme_data_new(&plain) != GPG_ERR_NO_ERROR)
   {
     gpgme_data_release(cipher);
     return 0;
   }
 
-  if ((err = gpgme_op_decrypt(mCtx, cipher, plain)) != GPGME_No_Error)
-    gLog.Warn("%sgpgme message decryption failed: %s\n", L_WARNxSTR, gpgme_strerror(err));
+  gpgme_data_seek(cipher, 0, SEEK_SET);
+  if ((err = gpgme_op_decrypt(mCtx, cipher, plain)) != GPG_ERR_NO_ERROR)
+    gLog.Warn("%s[GPG] gpgme message decryption failed: %s\n", L_WARNxSTR, gpgme_strerror(err));
   
   gpgme_data_release(cipher);
   buf = gpgme_data_release_and_get_mem(plain, &nRead);
@@ -88,38 +89,43 @@ char *CGPGHelper::Encrypt(const char *szPlain, const char *szId,
 
   CGPGMEMutex mutex;
   if (!mutex.Lock()) return 0;
-  GpgmeRecipients rcps;
-  GpgmeData plain = 0, cipher = 0;
-  GpgmeError err;
+  gpgme_key_t rcps[2];
+  gpgme_data_t plain = 0, cipher = 0;
+  gpgme_error_t err;
   char *szCipher = 0;
+  size_t nRead = 0;
 
-  if (gpgme_recipients_new(&rcps) != GPGME_No_Error) return 0;
-  if (gpgme_recipients_add_name_with_validity(rcps, buf, GPGME_VALIDITY_FULL) != GPGME_No_Error)
-    gLog.Error("%sCouldn't use gpgme recipient: %s\n", L_ERRORxSTR, buf);
+  rcps[1] = 0;
+  // Still use the old method, gpgme_get_key requires the fingerprint, which
+  // actually isn't very helpful.
+  if (gpgme_op_keylist_start (mCtx, buf, 0) != GPG_ERR_NO_ERROR)
+    gLog.Error("%s[GPG] Couldn't use gpgme recipient: %s\n", L_ERRORxSTR, buf);
   else
   {
-    if (gpgme_data_new_from_mem(&plain, szPlain, strlen(szPlain), 1) == GPGME_No_Error &&
-        gpgme_data_new(&cipher) == GPGME_No_Error)
+    if (gpgme_op_keylist_next(mCtx, rcps) != GPG_ERR_NO_ERROR)
+      gLog.Error("%s[GPG] Couldn't get key: %s\n", L_ERRORxSTR, buf);
+    else
     {
-      if ((err = gpgme_op_encrypt(mCtx, rcps, plain, cipher)) == GPGME_No_Error) 
+      if (gpgme_data_new_from_mem(&plain, szPlain, strlen(szPlain), 0) == GPG_ERR_NO_ERROR &&
+          gpgme_data_new(&cipher) == GPG_ERR_NO_ERROR)
       {
-        size_t nRead;
-        if (gpgme_data_read(cipher, 0, 0, &nRead) == GPGME_No_Error)
+        if ((err = gpgme_op_encrypt(mCtx, rcps, GPGME_ENCRYPT_ALWAYS_TRUST, plain, cipher)) == GPG_ERR_NO_ERROR)
         {
-          szCipher = (char *)malloc(nRead+1);
-          memset(szCipher, 0, nRead+1);
-          gpgme_data_read(cipher, szCipher, nRead, &nRead);
+          szCipher = gpgme_data_release_and_get_mem(cipher, &nRead);
+          cipher = 0;
+          szCipher = (char *)realloc(szCipher, nRead + 1);
+          szCipher[nRead] = 0;
         }
+        else
+          gLog.Error("%s[GPG] Encryption failed: %s\n", L_ERRORxSTR, gpgme_strerror(err));
       }
-      else
-        gLog.Error("%sEncryption failed: %s\n", L_ERRORxSTR, gpgme_strerror(err));
     }
 
     if (cipher) gpgme_data_release(cipher);
     if (plain) gpgme_data_release(plain);
   }
 
-  gpgme_recipients_release(rcps);
+  gpgme_key_unref(rcps[0]);
   return szCipher;
 #else
   return 0;
@@ -138,10 +144,10 @@ void CGPGHelper::Start()
   mKeysIni.ReadStr("passphrase", buf); mGPGPassphrase = strdup(buf);
 
   const char *gpgme_ver = gpgme_check_version(0);
-  gLog.Info("%sgpgme library found: %s\n", L_INITxSTR, gpgme_ver);
+  gLog.Info("%s[GPG] gpgme library found: %s\n", L_INITxSTR, gpgme_ver);
 	
-  if (gpgme_engine_check_version(GPGME_PROTOCOL_OpenPGP) != GPGME_No_Error)
-    gLog.Error("%sgpgme engine OpenPGP not found!\n", L_ERRORxSTR);
+  if (gpgme_engine_check_version(GPGME_PROTOCOL_OpenPGP) != GPG_ERR_NO_ERROR)
+    gLog.Error("%s[GPG] gpgme engine OpenPGP not found!\n", L_ERRORxSTR);
 
   gpgme_new(&mCtx);
   gpgme_set_protocol(mCtx, GPGME_PROTOCOL_OpenPGP);
@@ -151,9 +157,18 @@ void CGPGHelper::Start()
 }
 
 
-const char *CGPGHelper::PassphraseCallback(void *, const char *, void **)
+gpgme_error_t CGPGHelper::PassphraseCallback(void *, const char *, const char *, int, int fd)
 {
-  return gGPGHelper.mGPGPassphrase;
+  const char nl = '\n';
+  const char* const pf = gGPGHelper.mGPGPassphrase;
+  if (pf == 0)
+  {
+    write(fd, &nl, 1);
+    return GPG_ERR_CANCELED;
+  }
+  write(fd, pf, strlen(pf));
+  write(fd, &nl, 1);
+  return GPG_ERR_NO_ERROR;
 }
 
 
