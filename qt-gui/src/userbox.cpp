c95a717e2c3ce964236f78b99bcf4d42b46c3959
Jon Keating <jon@licq.org>
2005-01-28 14:08:35 +0000

    Support HTML in the tooltip. This makes it show AIM user's away messages without the HTML code.

--- qt-gui/src/userbox.cpp
+++ qt-gui/src/userbox.cpp
@@ -1462,7 +1462,7 @@ void CUserView::maybeTip(const QPoint& c)
       if (u->AutoResponse() && *u->AutoResponse() &&
           item->m_nStatus != ICQ_STATUS_OFFLINE &&
           item->m_nStatus != ICQ_STATUS_ONLINE)
-        s += tr("<br><u>Auto Response:</u>") + QStyleSheet::convertFromPlainText(codec->toUnicode(u->AutoResponse()), QStyleSheetItem::WhiteSpaceNormal);
+        s += tr("<br><u>Auto Response:</u>") + codec->toUnicode(u->AutoResponse());
       gUserManager.DropUser(u);
     }
 
