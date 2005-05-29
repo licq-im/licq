eb7ff68a25e624eec1b83c48828b5672ced5fb6f
Jon Keating <jon@licq.org>
2005-05-29 12:56:58 +0000

    Initial revision

---
+++ website/shots.php
@@ -0,0 +1,63 @@
+<?
+  include "header.php";
+
+  if (file_exists("/home/groups/l/li/licq/htdocs/zorbstats/zorblogs.php"))
+    include("/home/groups/l/li/licq/htdocs/zorbstats/zorblogs.php");
+  if (file_exists("/home/groups/l/li/licq/htdocs/zorbstats/zorblogspages.php"))
+  {
+    include ("/home/groups/l/li/licq/htdocs/zorbstats/zorblogspages.php");
+    $pagename = substr($PHP_SELF, 1);
+    $dispname = substr($PHP_SELF, 1, -4);
+    countpage($pagename, $dispname);
+  }
+?>
+      
+      <div id="content" > 
+
+				<div id="main"><img src="images/main_title_screenshot.gif" alt="Screenshots" /></div>
+        <div id="maincontent">
+          <div id="subcontent">
+
+          <p style="color:white;">.</p> <!-- Cheat when there is no text at the top of the page -->
+            
+            <!-- Screenshot Item Begin-->
+            <div class="news">
+              <h2>Screenshot #1</h2>
+              <p><a href="Gifs/Screenshot-1.jpg"><img style="width: 113px; height: 154px; border: 0;" src="Gifs/Screenshot-1.jpg" alt="Screenshot_1" /></a></p>
+              <ul>
+                <li>KDE Keramic style</li>
+                <li>Main window with thomas skin and thomas icons</li>
+                <li>User select dialog</li>
+              </ul>
+              <div class="footer">
+                <div class="footer_right"></div>
+              </div>
+            </div>
+            <!-- Screenshot Item End-->
+
+            <!-- Screenshot Item Begin-->
+            <div class="news">
+              <h2>Screenshot #2</h2>
+              <p><a href="Gifs/Screenshot-2.jpg"><img style="width: 177px; height: 154px; border: 0;" src="Gifs/Screenshot-2.jpg" alt="Screenshot_2" /></a></p>
+              <ul>
+                <li>Requesting SSL encrypted channel to other Licq user</li>
+                <li>User information dialog</li>
+                <li>Network log window</li>
+                <li>Message window with tabbed chatting and chatview enabled</li>
+                <li>KDE Dock icon (docked into the KDE notification area)</li>
+              </ul>
+              <div class="footer">
+                <div class="footer_right"></div>
+              </div>
+            </div>
+            <!-- Screenshot Item End-->
+
+
+          </div>
+        </div>
+
+        <div id="mainfooter"><p id="mainfooter_left"></p><!-- <img src="images/main_bg_bottomleft.gif" border="0" alt="Main" /> --></div>
+
+			</div> 
+
+<? include "footer.php" ?>
---
+++ website/zoom.php
@@ -0,0 +1,38 @@
+<?
+  include "header.php";
+
+  if (file_exists("/home/groups/l/li/licq/htdocs/zorbstats/zorblogs.php"))
+    include("/home/groups/l/li/licq/htdocs/zorbstats/zorblogs.php");
+  if (file_exists("/home/groups/l/li/licq/htdocs/zorbstats/zorblogspages.php"))
+  {
+    include ("/home/groups/l/li/licq/htdocs/zorbstats/zorblogspages.php");
+    $pagename = substr($PHP_SELF, 1);
+    $dispname = substr($PHP_SELF, 1, -4);
+    countpage($pagename, $dispname);
+  }
+  
+  $skin = $_GET['skin'];
+
+  $display = '';
+  $display .= '<div style="text-align: center;"><img src="skins/fullsize/'. $skin .'.jpg" border="0" />';
+  
+?>
+
+      <div id="content" > 
+
+				<div id="main"><img src="images/main_title_skins.gif" alt="Skins" /></div>
+        <div id="maincontent">
+          <div id="subcontent">
+          <p>Skin Name: <? echo $skin ?></a> :: <a href="skins/skin_files/skin.<? echo $skin ?>.tar.gz">Download</a></p> 
+          <p><a href="skins.php">Back to Skins</a></p>
+
+          <? echo $display ?>
+
+          </div>
+        </div>
+
+        <div id="mainfooter"><p id="mainfooter_left"></p></div>
+
+			</div> 
+
+<? include "footer.php" ?>
