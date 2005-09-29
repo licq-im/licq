b8e0f71969c36eb09c9dddb0143cdf220d9b326b
Jon Keating <jon@licq.org>
2005-09-29 03:14:23 +0000

    Change the index page to php and add a news file. Look at the source to see where to edit the file when there is new news. All project members should have write access to this file by tonight..

---
+++ licqweb/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+    <head>
+        <title>licq</title>
+		<link rel="StyleSheet" type="text/css" href="licqweb.css" />
+		<link rel="shortcut icon" href="images/favicon.ico">
+    </head>
+    <body onload="checkSession()">
+	<div id="tooltip"></div>
+	<script type="text/javascript" src="licqweb.js" />
+    <h1>licq<i><font color="#5555ff">web</font></i></h1>
+	<div id="login" class="loginwin">
+		<div class="bar">licqweb login</div>
+		<div class="centre">
+			UIN <input id="uin" type="text"/><p>
+			Password <input id="password" type="password"/><p>
+			<input type="submit" onclick="login(); return false;" value="Login" />
+		</div>
+	</div>
+	<div id="contactList" class="box" style="left:10px;top:150px;display:none;">
+	  <div class="bar" onmousedown="init_drag(event, 'contactList')" id="nick">licqweb</div>
+	  <div id="contacts"></div>
+	  <div id="ownerStatus"></div>
+	</div>
+	<div id="statusMenu"></div>
+	<div id="windows"></div>
+	<div class="logWindow" id="log"></div>
+	<div id="debug"></div>
+<?
+	$fd = fopen("http://www.licq.org/licqweb-news.txt", "r");
+	if (!$fd)
+	  $fd = fopen("http://www.thejon.org/licqweb-news.txt", "r");
+        if ($fd)
+	{
+          $str = "";
+          while(!feof($fd))
+            $str .= fread($fd, 4096);
+          fclose($fd);
+	}
+
+        echo "<div class=\"news\"><div class=\"bar\">News</div>".$str."</div>\n";
+?>
+    </body>
+</html>
