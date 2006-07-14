b5d2faaf8c7848fe0decf780f24ca9f98aaa6f7b
Erik Johansson <erijo@licq.org>
2006-07-14 21:16:32 +0000

    Change CVS to SVN and updated links to point to trac instead of sf.net

--- website/daily.php
+++ website/daily.php
@@ -1,17 +1,17 @@
 <? include('header.php'); ?>
   <div id="content" > 
 
-      <div id="main"><img src="images/main_title_dailies.gif" alt="CVS Dailies" /></div>
+      <div id="main"><img src="images/main_title_dailies.gif" alt="SVN Dailies" /></div>
         <div id="maincontent">
           <div id="subcontent">
 
           <p style="color:white;">.</p> <!-- Cheat when there is no text at the top of the page -->
-            
+
             <!-- Screenshot Item Begin-->
             <div class="news">
-             <h2>CVS snapshots</h2>
-             <p>CVS snapshots, in addition to anonymous CVS (see below), are available daily. If you want to participate in development and get full CVS access, please ask <a href="mailto:jon@licq.org">Jon</a>.</p>
-             <p>If you want to know what has changed in every detail, you should subscribe to the Licq CVS commit mailinglist.To do so, write an email to <a href=" mailto:licq-cvs-request@lists.sourceforge.net">licq-cvs-request@lists.sourceforge.net</a> and put 'subscribe' in the subject.</p>
+             <h2>SVN snapshots</h2>
+             <p>SVN snapshots, in addition to anonymous SVN (see below), are available daily. If you want to participate in development and get full SVN access, please ask <a href="mailto:jon@licq.org">Jon</a>.</p>
+             <p>If you want to know what has changed in every detail, you should subscribe to the <a href="https://lists.sourceforge.net/lists/listinfo/licq-cvs">Licq CVS commit mailinglist</a>. To do so, write an email to <a href=" mailto:licq-cvs-request@lists.sourceforge.net?subject=subscribe">licq-cvs-request@lists.sourceforge.net</a> and put 'subscribe' in the subject.</p>
              <p>Brief summary of changes for the last week:<br>
              <?php require("xlog.html"); ?>
              </p>
@@ -21,7 +21,7 @@
              <p>Remember, these are development versions, so they might have new and untested features or contain bugs. They might not even compile, so use them at your own risk.
              <p>Please join the Licq development mailinglist when you use the snapshots regularly.
              You should report problems with the snapshots or
-             discuss feature requests/patches there. To subscribe, write an email to <a href="mailto:licq-devel-request@lists.sourceforge.net">licq-devel-request@lists.sourceforge.net</a>.
+             discuss feature requests/patches there. To subscribe, write an email to <a href="mailto:licq-devel-request@lists.sourceforge.net?subject=subscribe">licq-devel-request@lists.sourceforge.net</a> and put 'subscribe' in the subject.
              <p>If there is a problem with the snapshot itself, please tell <a href="mailto:jon@licq.org">me</a>.
               <div class="footer">
                 <div class="footer_right"></div>
@@ -31,35 +31,20 @@
 
             <!-- Screenshot Item Begin-->
             <div class="news">
-            <h2>Anonymous CVS</h2>
-            <p>Thanks to Billy Biggs, there is now an anonymous CVS server available which is now being kept on SourceForge.
-            The anonymous CVS server is read-only. If you have installed the cvs binary,
+            <h2>Anonymous SVN</h2>
+            <p>The anonymous SVN server is read-only. If you have installed the svn binary,
             you can use it to easily keep up to date with bleeding edge Licq development. This is 
-            <b>only</b> intended for those who have at least basic knowledge in CVS usage
+            <b>only</b> intended for those who have at least basic knowledge in <a href="http://svnbook.org">SVN usage</a>
             or those who want to figure it out on their own. All others are strongly recommended to use
-            the regularly generated CVS snapshots. See above for details.</p>
-            <p>Login to the anonymous CVS server by typing</p>
-            <pre>
-   cvs -d:pserver:anonymous@cvs.sourceforge.net:/cvsroot/licq login
-   cvs -d:pserver:anonymous@cvs.sourceforge.net:/cvsroot/licq co &lt;modulename&gt;
-            </pre>
-            <p>the requested password is empty, just type return.</p>
-            <p>You can checkout the following modules:
-            <ul>
-              <li>licq</li>
-              <li>licq-data</li>
-              <li>licq-plugins</li>
-            </ul>
-            <p>You have to run
-            <pre>   make -f Makefile.cvs</pre><p>to generate the ./configure script needed for compiling the source. You need automake 1.4 or later installed to succeed.<br>
-            The recommended ~/.cvsrc for CVS usage is:
-            <pre>
-   cvs -l -z4 -q
-   diff -u1 -b -p
-   update -dP
-   checkout -P
-            </pre></p>
-            <p>If you experience any unusual problems, please inform me.
+            the regularly generated SVN snapshots (see above for details).</p>
+
+            <p>Checkout the latest version of Licq and the most common plugins with:</p>
+            <pre>   # svn co http://svn.licq.org/svn/trunk/licq</pre>
+
+            <p>You have to run</p>
+            <pre>   # make -f Makefile.cvs</pre>
+            <p>to generate the ./configure script needed for compiling the source. You need automake 1.4 or later installed to succeed.</p>
+            <p>If you experience any unusual problems, please inform me.</p>
               <div class="footer">
                 <div class="footer_right"></div>
               </div>
