38f619a3edee7ff3e47cfdf67c972faa2f173cae
Erik Johansson <erijo@licq.org>
2006-08-15 17:27:04 +0000

    Removed rms-web since it has been replaced by licqweb.

--- rms-web/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-include "check_session.php";
-session_destroy();
-header("Location: index.php");
-?>
--- rms-web/lougout.php
+++
@@ -1,5 +0,0 @@
-<?php
-include "check_session.php";
-session_destroy();
-header("location:index.php");
-?>
