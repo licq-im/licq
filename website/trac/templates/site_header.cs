05d37fa5b8295619f388c3185d69cb803750384f
Erik Johansson <erijo@licq.org>
2006-10-14 20:46:35 +0000

    A few more entries to the menu and a layout fix.

--- website/trac/templates/site_header.cs
+++ website/trac/templates/site_header.cs
@@ -9,10 +9,14 @@
 <div id="licqnav">
 <ul>
  <li><a href="/wiki/WikiStart">Main</a></li>
+ <li><a href="/tags/news">News</a></li>
+ <li><a href="/blog">Blog</a></li>
  <li><a href="/wiki/LicqFeatures">Features</a></li>
+ <li><a href="/wiki/LicqScreenshots">Screenshots</a></li>
  <li><a href="/wiki/LicqDownload">Download</a></li>
  <li><a href="/wiki/LicqFaq">FAQ</a></li>
  <li><a href="/wiki/LicqTips">Tips</a></li>
  <li><a href="/wiki/PluginList">Plugins</a></li>
+ <li><a href="/wiki/DeveloperArea">Developers</a></li>
 </ul>
 </div>
