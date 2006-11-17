ff981056ecba227bcb38b616df3b0b1ab64d1b07
Jon Keating <jon@licq.org>
2006-11-17 16:25:11 +0000

    We arent ready for v9 yet.

--- licq/include/licq_icq.h
+++ licq/include/licq_icq.h
@@ -7,8 +7,8 @@
 #define MODE_INDIRECT 0x02
 #define MODE_DENIED   0x01  // user denies direct connection from "any" user
 
-#define ICQ_VERSION 9
-const unsigned short ICQ_VERSION_TCP               = 0x0009;
+#define ICQ_VERSION 8
+const unsigned short ICQ_VERSION_TCP               = 0x0008;
 
 #define LICQ_WITHSSL     0x7D800000
 #define LICQ_WITHOUTSSL  0x7D000000
