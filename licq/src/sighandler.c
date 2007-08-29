724ae1e130b534ea20415120d77256975649f8a7
Erik Johansson <erijo@licq.org>
2007-08-29 22:06:28 +0000

    Print backtrace on SIGABRT

--- licq/src/sighandler.c
+++ licq/src/sighandler.c
@@ -30,6 +30,7 @@
 #include "gettext.h"
 
 void licq_handle_sigsegv(int);
+void licq_handle_sigabrt(int);
 void licq_handle_sigchld(int);
 
 /*
@@ -46,6 +47,7 @@ void licq_segv_handler(void (*f)(int, siginfo_t *, void *))
 void licq_signal_handler()
 {
   signal(SIGSEGV, &licq_handle_sigsegv);
+  signal(SIGABRT, &licq_handle_sigabrt);
   /* writing to socket after remote disconnected causes this, ignore it, the
      disconnection will be handled, eventually */
   signal(SIGPIPE, SIG_IGN);
@@ -70,16 +72,32 @@ void licq_handle_sigsegv(int s)
   fprintf(stderr, tr("Licq Segmentation Violation Detected.\n"));
   /*fprintf(stderr, "Fault Address: [0x%08lX]\n", (unsigned long)si->si_addr); */
 
+  // SIGABRT handler will print backtrace
+  abort();
+}
+
+
+
+void licq_handle_sigabrt(int s)
+{
+  if (s != SIGABRT)
+  {
+    fprintf(stderr, tr("Unknown signal.\n"));
+    return;
+  }
+
 #ifdef HAVE_BACKTRACE
   fprintf(stderr, tr("Backtrace:\n"));
   {
-    void *array[32];
-    unsigned short i;
-    int n = backtrace(array, 32);
+    const int size = 100;
+    int i;
+    void *array[size];
+    int n = backtrace(array, size);
     char **res = backtrace_symbols(array, n);
     for (i = 0; i < n; i++)
       fprintf(stderr, "%s\n", res[i]);
 
+    free(res);
     /*array[0] = si->si_addr;
     res = backtrace_symbols(array, 1);
     fprintf(stderr, "%s\n", res[0]);*/
@@ -87,12 +105,10 @@ void licq_handle_sigsegv(int s)
   fprintf(stderr, tr("Attempting to generate core file.\n"));
 
   #ifdef HAVE_PTHREAD_KILL_OTHER_THREADS_NP
-	// only available in LinuxThreads, not in NPTL
+  // only available in LinuxThreads, not in NPTL
   pthread_kill_other_threads_np();
   #endif
 #endif
-
-  abort();
 }
 
 
