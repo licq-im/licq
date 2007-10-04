a630a15db2294ec7e4b6275e0aee7f7ec3783912
Arne Schmitz <arne.schmitz@gmx.net>
2007-10-04 15:40:39 +0000

    - Updated German translation

--- qt-gui/po/de.ts
+++ qt-gui/po/de.ts
@@ -1174,7 +1174,7 @@ Sie müssen den Browser selbst starten und die URL in die Adressleiste kopieren.
     </message>
     <message>
         <source>Online Notify</source>
-        <translation>Online-Benachrichtigung</translation>
+        <translation type="obsolete">Online-Benachrichtigung</translation>
     </message>
     <message>
         <source>Set &amp;Auto Response...</source>
@@ -1390,11 +1390,11 @@ Siehe Netzwerkfenster für Details.</translation>
     </message>
     <message>
         <source>Invisible List</source>
-        <translation>Unsichtbar</translation>
+        <translation type="obsolete">Unsichtbar</translation>
     </message>
     <message>
         <source>Visible List</source>
-        <translation>Sichtbar</translation>
+        <translation type="obsolete">Sichtbar</translation>
     </message>
     <message>
         <source>Send &amp;Chat Request</source>
@@ -1414,7 +1414,7 @@ Siehe Netzwerkfenster für Details.</translation>
     </message>
     <message>
         <source>Ignore List</source>
-        <translation>Ignorieren</translation>
+        <translation type="obsolete">Ignorieren</translation>
     </message>
     <message>
         <source>%1: %2
@@ -1440,7 +1440,7 @@ Siehe Netzwerkfenster für Details.</translation>
     </message>
     <message>
         <source>New Users</source>
-        <translation>Neue Benutzer</translation>
+        <translation type="obsolete">Neue Benutzer</translation>
     </message>
     <message>
         <source>&amp;Network Window</source>
@@ -1636,7 +1636,7 @@ Doppelklick - Nächste Nachricht</translation>
     </message>
     <message>
         <source>All Users</source>
-        <translation>Alle Benutzer</translation>
+        <translation type="obsolete">Alle Benutzer</translation>
     </message>
     <message>
         <source>Don&apos;t Show</source>
@@ -1688,7 +1688,7 @@ Doppelklick - Nächste Nachricht</translation>
     </message>
     <message>
         <source>&lt;ul&gt;&lt;li&gt;&lt;tt&gt;%a - &lt;/tt&gt;user alias&lt;/li&gt;&lt;li&gt;&lt;tt&gt;%e - &lt;/tt&gt;email&lt;/li&gt;&lt;li&gt;&lt;tt&gt;%f - &lt;/tt&gt;first name&lt;/li&gt;&lt;li&gt;&lt;tt&gt;%h - &lt;/tt&gt;phone number&lt;/li&gt;&lt;li&gt;&lt;tt&gt;%i - &lt;/tt&gt;user ip&lt;/li&gt;&lt;li&gt;&lt;tt&gt;%l - &lt;/tt&gt;last name&lt;/li&gt;&lt;li&gt;&lt;tt&gt;%L - &lt;/tt&gt;local time&lt;/li&gt;&lt;li&gt;&lt;tt&gt;%m - &lt;/tt&gt;# pending messages&lt;/li&gt;&lt;li&gt;&lt;tt&gt;%M - &lt;/tt&gt;# pending messages (if any)&lt;/li&gt;&lt;li&gt;&lt;tt&gt;%n - &lt;/tt&gt;full name&lt;/li&gt;&lt;li&gt;&lt;tt&gt;%o - &lt;/tt&gt;last seen online&lt;/li&gt;&lt;li&gt;&lt;tt&gt;%O - &lt;/tt&gt;online since&lt;/li&gt;&lt;li&gt;&lt;tt&gt;%p - &lt;/tt&gt;user port&lt;/li&gt;&lt;li&gt;&lt;tt&gt;%s - &lt;/tt&gt;full status&lt;/li&gt;&lt;li&gt;&lt;tt&gt;%S - &lt;/tt&gt;abbreviated status&lt;/li&gt;&lt;li&gt;&lt;tt&gt;%u - &lt;/tt&gt;uin&lt;/li&gt;&lt;li&gt;&lt;tt&gt;%w - &lt;/tt&gt;webpage&lt;/li&gt;&lt;/ul&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;ul&gt;&lt;li&gt;&lt;tt&gt;%a - &lt;/tt&gt;Benutzeralias&lt;/li&gt;&lt;li&gt;&lt;tt&gt;%e - &lt;/tt&gt;Email&lt;/li&gt;&lt;li&gt;&lt;tt&gt;%f - &lt;/tt&gt;Vorname&lt;/li&gt;&lt;li&gt;&lt;tt&gt;%h - &lt;/tt&gt;Telefonnummer&lt;/li&gt;&lt;li&gt;&lt;tt&gt;%i - &lt;/tt&gt;Benutzer IP&lt;/li&gt;&lt;li&gt;&lt;tt&gt;%l - &lt;/tt&gt;Nachname&lt;/li&gt;&lt;li&gt;&lt;tt&gt;%L - &lt;/tt&gt;Ortszeit&lt;/li&gt;&lt;li&gt;&lt;tt&gt;%m - &lt;/tt&gt;# wartende Nachrichten&lt;/li&gt;&lt;li&gt;&lt;tt&gt;%M - &lt;/tt&gt;# wartende Nachrichten (wenn vorhanden)&lt;/li&gt;&lt;li&gt;&lt;tt&gt;%n - &lt;/tt&gt;Voller Name&lt;/li&gt;&lt;li&gt;&lt;tt&gt;%o - &lt;/tt&gt;Letztes mal Online&lt;/li&gt;&lt;li&gt;&lt;tt&gt;%O - &lt;/tt&gt;Online seit&lt;/li&gt;&lt;li&gt;&lt;tt&gt;%p - &lt;/tt&gt;Benutzerport&lt;/li&gt;&lt;li&gt;&lt;tt&gt;%s - &lt;/tt&gt;Voller Status&lt;/li&gt;&lt;li&gt;&lt;tt&gt;%S - &lt;/tt&gt;Abgekürzter Status&lt;/li&gt;&lt;li&gt;&lt;tt&gt;%u - &lt;/tt&gt;UIN&lt;/li&gt;&lt;li&gt;&lt;tt&gt;%w - &lt;/tt&gt;Webseite&lt;/li&gt;&lt;/ul&gt;</translation>
     </message>
     <message>
         <source>&lt;h2&gt;Hints for Using&lt;br&gt;the Licq Qt-GUI Plugin&lt;/h2&gt;&lt;br&gt;&lt;hr&gt;&lt;br&gt;&lt;ul&gt;&lt;li&gt;Change your status by right clicking on the status label.&lt;/li&gt;&lt;li&gt;Change your auto response by double-clicking on the status label.&lt;/li&gt;&lt;li&gt;View system messages by double clicking on the message label.&lt;/li&gt;&lt;li&gt;Change groups by right clicking on the message label.&lt;/li&gt;&lt;li&gt;Use the following shortcuts from the contact list:&lt;ul&gt;&lt;li&gt;&lt;tt&gt;Ctrl-M : &lt;/tt&gt;Toggle mini-mode&lt;/li&gt;&lt;li&gt;&lt;tt&gt;Ctrl-O : &lt;/tt&gt;Toggle show offline users&lt;/li&gt;&lt;li&gt;&lt;tt&gt;Ctrl-X : &lt;/tt&gt;Exit&lt;/li&gt;&lt;li&gt;&lt;tt&gt;Ctrl-H : &lt;/tt&gt;Hide&lt;/li&gt;&lt;li&gt;&lt;tt&gt;Ctrl-I : &lt;/tt&gt;View the next message&lt;/li&gt;&lt;li&gt;&lt;tt&gt;Ctrl-V : &lt;/tt&gt;View message&lt;/li&gt;&lt;li&gt;&lt;tt&gt;Ctrl-S : &lt;/tt&gt;Send message&lt;/li&gt;&lt;li&gt;&lt;tt&gt;Ctrl-U : &lt;/tt&gt;Send Url&lt;/li&gt;&lt;li&gt;&lt;tt&gt;Ctrl-C : &lt;/tt&gt;Send chat request&lt;/li&gt;&lt;li&gt;&lt;tt&gt;Ctrl-F : &lt;/tt&gt;Send File&lt;/li&gt;&lt;li&gt;&lt;tt&gt;Ctrl-A : &lt;/tt&gt;Check Auto response&lt;/li&gt;&lt;li&gt;&lt;tt&gt;Ctrl-P : &lt;/tt&gt;Popup all messages&lt;/li&gt;&lt;li&gt;&lt;tt&gt;Ctrl-L : &lt;/tt&gt;Redraw user window&lt;/li&gt;&lt;li&gt;&lt;tt&gt;Delete : &lt;/tt&gt;Delete user from current group&lt;/li&gt;&lt;li&gt;&lt;tt&gt;Ctrl-Delete : &lt;/tt&gt;Delete user from contact list&lt;/li&gt;&lt;/ul&gt;&lt;li&gt;Hold control while clicking on close in the function window to remove   the user from your contact list.&lt;/li&gt;&lt;li&gt;Hit Ctrl-Enter from most text entry fields to select &quot;Ok&quot; or &quot;Accept&quot;.   For example in the send tab of the user function window.&lt;/li&gt;&lt;li&gt;Here is the complete list of user % options, which can be used in &lt;b&gt;OnEvent&lt;/b&gt;   parameters, &lt;b&gt;auto responses&lt;/b&gt;, and &lt;b&gt;utilities&lt;/b&gt;:</source>
@@ -2155,83 +2155,83 @@ Doppelklick - Nächste Nachricht</translation>
     </message>
     <message>
         <source>Birthday Today!</source>
-        <translation type="unfinished"></translation>
+        <translation>Heute Geburtstag!</translation>
     </message>
     <message>
         <source>Typing a message</source>
-        <translation type="unfinished"></translation>
+        <translation>Tippt gerade eine Nachricht</translation>
     </message>
     <message>
         <source>Phone &amp;quot;Follow Me&amp;quot;: Available</source>
-        <translation type="unfinished"></translation>
+        <translation>Telefon &amp;quot;folgen&amp;quot;: Verfügbar</translation>
     </message>
     <message>
         <source>Phone &amp;quot;Follow Me&amp;quot;: Busy</source>
-        <translation type="unfinished"></translation>
+        <translation>Telefon &amp;quot;folgen&amp;quot;: Beschäftigt</translation>
     </message>
     <message>
         <source>ICQphone: Available</source>
-        <translation type="unfinished"></translation>
+        <translation>ICQphone: Verfügbar</translation>
     </message>
     <message>
         <source>ICQphone: Busy</source>
-        <translation type="unfinished"></translation>
+        <translation>ICQphone: Beschäftigt</translation>
     </message>
     <message>
         <source>File Server: Enabled</source>
-        <translation type="unfinished"></translation>
+        <translation>Dateiserver: aktiviert</translation>
     </message>
     <message>
         <source>Secure connection</source>
-        <translation type="unfinished"></translation>
+        <translation>Sichere Verbindung</translation>
     </message>
     <message>
         <source>Custom Auto Response</source>
-        <translation type="unfinished"></translation>
+        <translation>Eigene Auto-Antwort</translation>
     </message>
     <message>
         <source>Auto Response:</source>
-        <translation type="unfinished"></translation>
+        <translation>Auto-Antwort:</translation>
     </message>
     <message>
         <source>E: </source>
-        <translation type="unfinished"></translation>
+        <translation>E: </translation>
     </message>
     <message>
         <source>P: </source>
-        <translation type="unfinished"></translation>
+        <translation>P: </translation>
     </message>
     <message>
         <source>C: </source>
-        <translation type="unfinished"></translation>
+        <translation>C: </translation>
     </message>
     <message>
         <source>F: </source>
-        <translation type="unfinished"></translation>
+        <translation>F: </translation>
     </message>
     <message>
         <source>Ip: </source>
-        <translation type="unfinished"></translation>
+        <translation>lp: </translation>
     </message>
     <message>
         <source>O: </source>
-        <translation type="unfinished"></translation>
+        <translation>O: </translation>
     </message>
     <message>
         <source>Logged In: </source>
-        <translation type="unfinished"></translation>
+        <translation>Eingelogged: </translation>
     </message>
     <message>
         <source>Idle: </source>
-        <translation type="unfinished"></translation>
+        <translation>Untätig: </translation>
     </message>
     <message>
         <source>Local time: </source>
-        <translation type="unfinished"></translation>
+        <translation>Ortszeit: </translation>
     </message>
     <message>
         <source>ID: </source>
-        <translation type="unfinished"></translation>
+        <translation>ID: </translation>
     </message>
 </context>
 <context>
@@ -4400,7 +4400,8 @@ Bei einer Änderung ist ein Neustart erforderlich!
     <message>
         <source>The format string used to define what will appear in each column.
 The following parameters can be used:</source>
-        <translation type="unfinished"></translation>
+        <translation>Der Formatstring, der definiert was in jeder Spalte erscheint.
+Die folgenden Parameter können benutzt werden:</translation>
     </message>
 </context>
 <context>
