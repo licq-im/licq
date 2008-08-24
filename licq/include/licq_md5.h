8bc8cf62a9756c64c70c83bd40ac7024c0430ca1
Anders Olofsson <flynd@licq.org>
2008-08-24 14:56:35 +0000

    Fixed indentation and some other coding style for the MD5 functions.

--- licq/include/licq_md5.h
+++ licq/include/licq_md5.h
@@ -2,7 +2,7 @@
  * md5.h:  Header file for Colin Plumb's MD5 implementation.
  *         Modified by Ian Jackson so as not to use Colin Plumb's
  *         'usuals.h'.
- * 
+ *
  *         This file is in the public domain.
  */
 
@@ -16,18 +16,16 @@
 
 void md5(const uint8_t* buf, size_t len, uint8_t* digest);
 
-struct MD5Context
+struct Md5Context
 {
   uint32_t buf[4];
   uint64_t bytes;
   uint32_t in[16];
 };
 
-void MD5Init(struct MD5Context *context);
-void MD5Update(struct MD5Context* context, uint8_t const* buf, size_t len);
-void MD5Final(unsigned char digest[16], struct MD5Context *context);
-void MD5Transform(uint32_t buf[4], const uint32_t in[16]);
+void md5Init(struct Md5Context* context);
+void md5Update(struct Md5Context* context, uint8_t const* buf, size_t len);
+void md5Final(struct Md5Context* context, unsigned char digest[16]);
+void md5Transform(uint32_t buf[4], const uint32_t in[16]);
 
-void byteSwap(uint32_t* buf, unsigned words);
-
-#endif				/* !MD5_H */
+#endif
