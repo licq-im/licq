b83c0cab87ee1c867f1992b00fb808c88ae0962d
Anders Olofsson <flynd@licq.org>
2009-10-08 19:08:26 +0000

    Disable mutex debugging before releasing Licq 1.3.7.

--- licq/include/licq_mutex.h
+++ licq/include/licq_mutex.h
@@ -10,7 +10,7 @@
  * daemon will print a message to stderr (and to the file
  * <basedir>/licq.debug_rw_mutex) and then abort.
  */
-#define DEBUG_RW_MUTEX
+//#define DEBUG_RW_MUTEX
 
 
 #ifdef DEBUG_RW_MUTEX
@@ -64,7 +64,7 @@ public:
 #ifdef DEBUG_RW_MUTEX
   { myName = name; }
 #else
-  { }
+  { (void)name; }
 #endif
 
 private:
