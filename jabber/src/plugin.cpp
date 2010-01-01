fbce40e8d1752a856fe9e163366a8eaae5749954
Erik Johansson <erijo@licq.org>
2010-01-01 22:19:41 +0000

    Skeleton for a jabber plugin. Doesn't do much except load at the moment. #759 work.

---
+++ jabber/src/plugin.cpp
@@ -0,0 +1,58 @@
+/*
+ * This file is part of Licq, an instant messaging client for UNIX.
+ * Copyright (C) 2010 Erik Johansson <erijo@licq.org>
+ *
+ * Licq is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Licq is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Licq; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "pluginversion.h"
+
+#include <licq_log.h>
+#include <licq_protoplugin.h>
+
+char* LProto_Name()
+{
+  static char name[] = "Jabber";
+  return name;
+}
+
+char* LProto_Version()
+{
+  static char version[] = PLUGIN_VERSION_STRING;
+  return version;
+}
+
+char* LProto_PPID()
+{
+  static char ppid[] = "XMPP";
+  return ppid;
+}
+
+bool LProto_Init()
+{
+  gLog.Info("Hello World!\n");
+
+  return true;
+}
+
+unsigned long LProto_SendFuncs()
+{
+  return 0;
+}
+
+int LProto_Main(CICQDaemon* daemon)
+{
+  return 0;
+}
