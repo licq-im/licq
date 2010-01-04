4f88197a38c9d82b500307b9143a525c594b4737
Erik Johansson <erijo@licq.org>
2010-01-04 22:18:08 +0000

    Moving up a level since it is not part of the core

--- website/trac/templates/site_css.cs
+++
@@ -1,5 +0,0 @@
-<?cs
-##################################################################
-# Site CSS - Place custom CSS, including overriding styles here.
-?>
-@import url(<?cs var:chrome.href ?>/site/style.css);
