ca92aef70a75c3f9d9a2c44f259973f87d5d57a8
Eugene Paskevich <eugene@licq.org>
2010-01-09 10:08:43 +0000

    There is no need in calling a handler for each ReadyState change if we are interested in only one state, loaded.

--- licqweb/licqweb.js
+++ licqweb/licqweb.js
@@ -60,28 +60,23 @@ function Message(id, pp, uid, message) {
 
 //callback for server push reponses
 function acceptResponse() {
-  if (xmlhttp.readyState == 4 && xmlhttp.responseXML != null)
-  {
-		if (xmlhttp.status == 200) {
-			response = xmlhttp.responseXML.documentElement;
-			method = response.getElementsByTagName('method')[0].firstChild.data;
-			eval(method + '(response)');
-		} else {
-			alert("There was a problem retrieving the XML data:\n" + xmlhttp.statusText);
-		}
+	if (xmlhttp.responseXML != null) {
+		response = xmlhttp.responseXML.documentElement;
+		method = response.getElementsByTagName('method')[0].firstChild.data;
+		eval(method + '(response)');
+	} else {
+		alert("There was a problem retrieving the XML data:\n" + xmlhttp.statusText);
 	}
 }
 
 //callback for single events
 function acceptResponse2() {
-	if (xmlhttp2.readyState == 4) {
-		if (xmlhttp2.status == 200) {
-			response = xmlhttp2.responseXML.documentElement;
-			method = response.getElementsByTagName('method')[0].firstChild.data;
-			eval(method + '(response)');
-		} else {
-			alert("There was a problem retrieving the XML data:\n" + xmlhttp2.statusText);
-		}
+	if (xmlhttp2.responseXML != null) {
+		response = xmlhttp2.responseXML.documentElement;
+		method = response.getElementsByTagName('method')[0].firstChild.data;
+		eval(method + '(response)');
+	} else {
+		alert("There was a problem retrieving the XML data:\n" + xmlhttp2.statusText);
 	}
 }
 
@@ -127,7 +122,7 @@ function viewEvent(response) {
 
 function requestViewEvent(id, pp) {
 	xmlhttp2 = new XMLHttpRequest();
-	xmlhttp2.onreadystatechange = acceptResponse2;
+	xmlhttp2.onload = acceptResponse2;
 	xmlhttp2.open("GET", baseurl + "/viewEvent.php?id=" + id + "&pp=" + pp, true);
 	xmlhttp2.send(null);
 }
@@ -239,7 +234,7 @@ function sendMessage(id, pp) {
 	var message = document.getElementById(id + '-' + pp + '-input').value;
 	ackMessages[uid] = new Message(id, pp, uid, message);
 	xmlhttp2 = new XMLHttpRequest();
-	xmlhttp2.onreadystatechange = acceptResponse2;
+	xmlhttp2.onload = acceptResponse2;
 	xmlhttp2.open("POST", baseurl + "/sendMessage.php", true);
 	xmlhttp2.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 	xmlhttp2.send('uid=' + uid + '&id=' + id + '&pp=' + pp + '&msg=' + escape(message));
@@ -374,7 +369,7 @@ function showSelectStatus(e, id, pp) {
 
 function changeStatus(pp, status) {
 	xmlhttp2 = new XMLHttpRequest();
-	xmlhttp2.onreadystatechange = acceptResponse2;
+	xmlhttp2.onload = acceptResponse2;
 	xmlhttp2.open("POST", baseurl + "/changeStatus.php", true);
 	xmlhttp2.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
   xmlhttp2.send('pp=' + pp + '&status=' + escape(status.replace(/ /g,"")));
@@ -411,7 +406,7 @@ function doLogin(uin, password) {
 		login = "&uin=" + uin + "&password=" + password;
 	}
 	xmlhttp.open("GET", baseurl + "/push.php?listtype=" + listtype + login, true);
-	xmlhttp.onreadystatechange = acceptResponse;
+	xmlhttp.onload = acceptResponse;
 	xmlhttp.send(null);
 	document.getElementById('login').style.display = 'none';
 	document.getElementById('contactList').style.display = 'block';
@@ -576,7 +571,7 @@ function stop_resize(event)
 function requestViewHistory(id, pp)
 {
   xmlhttp2 = new XMLHttpRequest();
-  xmlhttp2.onreadystatechange = acceptResponse2;
+  xmlhttp2.onload = acceptResponse2;
   xmlhttp2.open("GET", baseurl + "/viewHistory.php?id=" + id + "&pp=" + pp + "&lenght=" + loadHistory + "&offset=0", true);
   xmlhttp2.send(null);
 }
