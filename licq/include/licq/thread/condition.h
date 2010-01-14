f650f50b49af62cf82e1458c31e5017ba2367ee0
Erik Johansson <erijo@licq.org>
2010-01-14 19:26:19 +0000

    Added class Condition as a simple wrapper around pthread_cond

---
+++ licq/include/licq/thread/condition.h
@@ -0,0 +1,69 @@
+/*
+ * This file is part of Licq, an instant messaging client for UNIX.
+ * Copyright (C) 2010 Erik Johansson <erijo@licq.org>
+ *
+ * Licq is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Licq is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Licq; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef LICQ_CONDITION_H
+#define LICQ_CONDITION_H
+
+#include "mutex.h"
+
+#include <boost/noncopyable.hpp>
+#include <pthread.h>
+
+namespace Licq
+{
+
+class Condition : private boost::noncopyable
+{
+public:
+  static const unsigned int WAIT_FOREVER = -1;
+
+  Condition();
+  ~Condition();
+
+  void signal();
+  void broadcast();
+  bool wait(Mutex& mutex, unsigned int msec = WAIT_FOREVER);
+
+private:
+  pthread_cond_t myCondition;
+};
+
+inline Condition::Condition()
+{
+  ::pthread_cond_init(&myCondition, NULL);
+}
+
+inline Condition::~Condition()
+{
+  ::pthread_cond_destroy(&myCondition);
+}
+
+inline void Condition::signal()
+{
+  ::pthread_cond_signal(&myCondition);
+}
+
+inline void Condition::broadcast()
+{
+  ::pthread_cond_broadcast(&myCondition);
+}
+
+} // namespace Licq
+
+#endif
---
+++ licq/src/thread/condition.cpp
@@ -0,0 +1,51 @@
+/*
+ * This file is part of Licq, an instant messaging client for UNIX.
+ * Copyright (C) 2010 Erik Johansson <erijo@licq.org>
+ *
+ * Licq is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Licq is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Licq; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "licq/thread/condition.h"
+
+#include <ctime>
+#include <sys/time.h>
+
+using namespace Licq;
+
+bool Condition::wait(Mutex& mutex, unsigned int msec)
+{
+  if (msec == WAIT_FOREVER)
+  {
+    ::pthread_cond_wait(&myCondition, &mutex.myMutex);
+    return true;
+  }
+
+  timeval now;
+  ::gettimeofday(&now, NULL);
+
+  timespec abstime;
+  abstime.tv_sec = now.tv_sec + (msec / 1000);
+  abstime.tv_nsec = now.tv_usec * 1000 + ((msec % 1000) * 1000 * 1000);
+
+  const long nanoInSec = 1 * 1000 * 1000 * 1000;
+  if (abstime.tv_nsec >= nanoInSec)
+  {
+    abstime.tv_nsec -= nanoInSec;
+    abstime.tv_sec += 1;
+  }
+
+  int ret = ::pthread_cond_timedwait(&myCondition, &mutex.myMutex, &abstime);
+  return (ret == 0);
+}
