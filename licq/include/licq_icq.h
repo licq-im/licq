79afe18daba2cac10c551b68d2dff2397414530c
Eugene Paskevich <eugene@licq.org>
2010-01-18 00:34:04 +0000

    Added new protocol sending capability for non-online status message.

--- licq/include/licq_icq.h
+++ licq/include/licq_icq.h
@@ -623,15 +623,16 @@ const char XtraAwayCaps[XTRA_AWAY_COUNT][0x10] =
 // For protocol plugins
 
 // Sending capabilities
-const unsigned long PP_SEND_MSG      = 0x00000001;
-const unsigned long PP_SEND_URL      = 0x00000002;
-const unsigned long PP_SEND_FILE     = 0x00000004;
-const unsigned long PP_SEND_CHAT     = 0x00000008;
-const unsigned long PP_SEND_CONTACT  = 0x00000010;
-const unsigned long PP_SEND_AUTH     = 0x00000020;
-const unsigned long PP_SEND_AUTHxREQ = 0x00000040;
-const unsigned long PP_SEND_SMS      = 0x00000080;
-const unsigned long PP_SEND_SECURE   = 0x00000100;
-const unsigned long PP_SEND_DIRECT   = 0x00000200;
+const unsigned long PP_SEND_MSG         = 0x00000001;
+const unsigned long PP_SEND_URL         = 0x00000002;
+const unsigned long PP_SEND_FILE        = 0x00000004;
+const unsigned long PP_SEND_CHAT        = 0x00000008;
+const unsigned long PP_SEND_CONTACT     = 0x00000010;
+const unsigned long PP_SEND_AUTH        = 0x00000020;
+const unsigned long PP_SEND_AUTHxREQ    = 0x00000040;
+const unsigned long PP_SEND_SMS         = 0x00000080;
+const unsigned long PP_SEND_SECURE      = 0x00000100;
+const unsigned long PP_SEND_DIRECT      = 0x00000200;
+const unsigned long PP_SEND_STATUSxMSG  = 0x00000400;
 
 #endif
