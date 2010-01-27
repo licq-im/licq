21ce56ad28495d09c9668a752afa91016194f5d9
Erik Johansson <erijo@licq.org>
2010-01-27 19:52:38 +0000

    Added new class ThreadSpecificData.

---
+++ licq/include/licq/thread/threadspecificdata.h
@@ -0,0 +1,89 @@
+/*
+ * This file is part of Licq, an instant messaging client for UNIX.
+ * Copyright (C) 2010 Erik Johansson <erijo@licq.org>
+ *
+ * Licq is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Licq is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Licq; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef LICQ_THREADSPECIFICDATA_H
+#define LICQ_THREADSPECIFICDATA_H
+
+#include <pthread.h>
+
+namespace Licq
+{
+
+/**
+ * ThreadSpecificData stores data specific to a thread.
+ * @ingroup thread
+ */
+template<typename T>
+class ThreadSpecificData
+{
+public:
+  /**
+   * Creates a new thread specific data object.
+   */
+  ThreadSpecificData() { ::pthread_key_create(&myKey, &deleter); }
+
+  /**
+   * Destroys the thread specific data object.
+   */
+  ~ThreadSpecificData() { ::pthread_key_delete(myKey); }
+
+  /**
+   * Sets new data. ThreadSpecificData takes ownership of the data, which must
+   * have been allocated on the heap with new. The data will be deleted when
+   * the thread terminates or when set is called the next time.
+   */
+  inline void set(const T* data);
+
+  /**
+   * Returns the data previously set(); or 0 if no data is set.
+   */
+  inline T* get() const;
+
+  /**
+   * @return True if data is set; otherwise false.
+   */
+  bool isSet() const { return get() != 0; }
+
+private:
+  static inline void deleter(void* data);
+  pthread_key_t myKey;
+};
+
+template<typename T>
+void ThreadSpecificData<T>::set(const T* data)
+{
+  delete get();
+  ::pthread_setspecific(myKey, data);
+}
+
+template<typename T>
+T* ThreadSpecificData<T>::get() const
+{
+  return static_cast<T*>(::pthread_getspecific(myKey));
+}
+
+template<typename T>
+void ThreadSpecificData<T>::deleter(void* data)
+{
+  delete static_cast<T*>(data);
+}
+
+} // namespace Licq
+
+#endif
---
+++ licq/src/thread/tests/threadspecificdatatest.cpp
@@ -0,0 +1,78 @@
+/*
+ * This file is part of Licq, an instant messaging client for UNIX.
+ * Copyright (C) 2010 Erik Johansson <erijo@licq.org>
+ *
+ * Licq is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Licq is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Licq; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "licq/thread/threadspecificdata.h"
+
+#include <gtest/gtest.h>
+
+using namespace Licq;
+
+TEST(ThreadSpecificData, create)
+{
+  ThreadSpecificData<int> data;
+}
+
+TEST(ThreadSpecificData, setAndGet)
+{
+  ThreadSpecificData<int> data;
+  data.set(new int(5));
+  EXPECT_EQ(5, *data.get());
+
+  // Cleanup
+  data.set(0);
+}
+
+TEST(ThreadSpecificData, isSet)
+{
+  ThreadSpecificData<int> data;
+  EXPECT_FALSE(data.isSet());
+
+  data.set(new int());
+  EXPECT_TRUE(data.isSet());
+
+  data.set(0);
+  EXPECT_FALSE(data.isSet());
+}
+
+struct DeleteLogger
+{
+  static int myCount;
+  ~DeleteLogger() { myCount += 1; }
+};
+
+int DeleteLogger::myCount = 0;
+
+static void* threadEntry(void* data)
+{
+  static_cast<ThreadSpecificData<DeleteLogger>*>(data)->
+      set(new DeleteLogger());
+  return 0;
+}
+
+TEST(ThreadSpecificData, dataDeletedWhenThreadExits)
+{
+  ThreadSpecificData<DeleteLogger> logger;
+  DeleteLogger::myCount = 0;
+
+  pthread_t thread;
+  ::pthread_create(&thread, 0, &threadEntry, &logger);
+  ::pthread_join(thread, 0);
+
+  EXPECT_EQ(1, DeleteLogger::myCount);
+}
