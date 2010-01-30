d1fb997056c12615a12054c7d69126dd4ce23022
Erik Johansson <erijo@licq.org>
2010-01-30 21:37:20 +0000

    Don't cache install paths. This way CMAKE_INSTALL_PREFIX can be changed in e.g. ccmake after cmake has been run for the first time. Licq_*_DIR can still be given on the command line to override the default path.

--- licq/cmake/LicqDefaultPaths.cmake
+++ licq/cmake/LicqDefaultPaths.cmake
@@ -2,7 +2,7 @@
 # also used by plugins when configuring the plugin to build using headers in
 # the source directory.
 
-set(Licq_INCLUDE_DIR "${CMAKE_INSTALL_PREFIX}/include" CACHE PATH "Licq include directory")
-set(Licq_LOCALE_DIR "${CMAKE_INSTALL_PREFIX}/share/locale" CACHE PATH "Licq locale directory")
-set(Licq_PLUGIN_DIR "${CMAKE_INSTALL_PREFIX}/lib${LIB_SUFFIX}/licq" CACHE PATH "Licq plugin directory")
-set(Licq_SHARE_DIR "${CMAKE_INSTALL_PREFIX}/share/licq" CACHE PATH "Licq share directory")
+set(Licq_INCLUDE_DIR "${CMAKE_INSTALL_PREFIX}/include")
+set(Licq_LOCALE_DIR "${CMAKE_INSTALL_PREFIX}/share/locale")
+set(Licq_PLUGIN_DIR "${CMAKE_INSTALL_PREFIX}/lib${LIB_SUFFIX}/licq")
+set(Licq_SHARE_DIR "${CMAKE_INSTALL_PREFIX}/share/licq")
