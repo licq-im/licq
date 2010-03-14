4507ca818f5987f7c18d3fe794fbe3ef9abbd828
Anders Olofsson <flynd@licq.org>
2010-03-14 17:15:46 +0000

    Replaced old ini file handler by deriving new class instead. This will cause some messages about missing parameter in files not to be logged. It will also cause a format change in the config files on next write.

--- licq/include/licq_file.h
+++ licq/include/licq_file.h
@@ -1,9 +1,15 @@
 #ifndef INIFILE_H
 #define INIFILE_H
 
+#include "licq/inifile.h"
+
+#include <cerrno>
+#include <cstring>
 #include <list>
 #include <string>
 #include <boost/any.hpp>
+
+#include "licq_log.h"
 
 #define MAX_SECTIONxNAME_LEN 160
 #define MAX_KEYxNAME_LEN 160
@@ -23,17 +29,29 @@ const unsigned short INI_ExIOWRITE        = 0x0010;
 void AddNewLines(char *_szDest, const char *_szSource);
 
 //=====CIniFile================================================================
-class CIniFile
+class CIniFile : public Licq::IniFile
 {
 public:
-  CIniFile(unsigned short _nFlags = 0);
-  ~CIniFile();
+  CIniFile(unsigned short _nFlags = 0)
+    : Licq::IniFile(""), m_nFlags(_nFlags)
+  { /* Empty */ }
 
-  bool LoadFile(const char *_szFilename);
-  bool ReloadFile();
-  bool FlushFile();
-  void CloseFile();
-  void SetFileName(const char *_szFilename);
+  ~CIniFile() {}
+
+  bool LoadFile(const char *_szFilename)
+  { setFilename(_szFilename); return loadFile(); }
+
+  bool ReloadFile()
+  { return loadFile(); }
+
+  bool FlushFile()
+  { return writeFile(GetFlag(INI_FxALLOWxCREATE)); }
+
+  void CloseFile()
+  { /* Empty */ }
+
+  void SetFileName(const char *_szFilename)
+  { setFilename(_szFilename); }
 
   // Flag setting and clearing
   void SetFlags(unsigned short _nFlags)
@@ -47,16 +65,11 @@ public:
   bool GetFlag(unsigned short _nFlag)
     { return (m_nFlags & _nFlag); };
 
-  bool SetSection(const char *_szSectionName);
-  bool CreateSection(const char *_szSectionName);
+  bool SetSection(const char *_szSectionName)
+  { return setSection(_szSectionName, GetFlag(INI_FxALLOWxCREATE)); }
 
-  /**
-   * Get a list of keys from the current section
-   *
-   * @param ret List to return keys in
-   * @param prefix Prefix that keys must match to be returned
-   */
-  void getKeyList(std::list<std::string>& ret, const std::string& prefix);
+  bool CreateSection(const char *_szSectionName)
+  { return setSection(_szSectionName, true); }
 
   /**
    * Read a value from the configuration file
@@ -65,7 +78,8 @@ public:
    * @param data variable to put value in
    * @return True if value was found or false if default value was used
    */
-  bool readVar(const std::string& key, boost::any& data);
+  bool readVar(const std::string& key, boost::any& data)
+  { return get(key, data); }
 
   /**
    * Read a string value from the configuration file
@@ -77,17 +91,80 @@ public:
    * @return True if value was found or false if default value was used
    */
   bool readString(const std::string& key, std::string& data,
-      const std::string& defValue = "", bool trim = true);
+      const std::string& defValue = "", bool trim = true)
+  {
+    bool ret = get(key, data, defValue);
+    if (ret && !trim)
+      data = ' ' + data;
+    return ret;
+  }
 
-  bool ReadStr(const std::string& Key, char* data, const char* defValue = NULL, bool trim = true, int maxLength = 0);
-  bool ReadNum(const std::string& key, unsigned long &data, unsigned long defValue = 0);
-  bool ReadNum(const std::string& key, signed long &data, signed long defValue = 0);
-  bool ReadNum(const std::string& key, unsigned int &data, unsigned int defValue = 0);
-  bool ReadNum(const std::string& key, signed int &data, signed int defValue = 0);
-  bool ReadNum(const std::string& key, unsigned short &data, unsigned short defValue = 0);
-  bool ReadNum(const std::string& key, signed short &data, signed short defValue = 0);
-  bool ReadNum(const std::string& key, char &data, char defValue = 0);
-  bool ReadBool(const std::string& key, bool &data, const bool defValue = false);
+  bool ReadStr(const std::string& Key, char* data, const char* defValue = NULL, bool trim = true, int maxLength = 0)
+  {
+    (void)trim; (void)maxLength;
+    std::string retStr;
+    bool ret = get(Key, retStr);
+    if (ret) {
+      if (trim) {
+        strcpy(data, retStr.c_str());
+      } else {
+        data[0] = ' ';
+        strcpy(data+1, retStr.c_str());
+      }
+    } else if (defValue != NULL) {
+      strcpy(data, defValue);
+    }
+    return ret;
+  }
+
+  bool ReadNum(const std::string& key, unsigned long &data, unsigned long defValue = 0)
+  {
+    unsigned retInt;
+    bool ret = get(key, retInt, defValue);
+    data = retInt;
+    return ret;
+  }
+
+  bool ReadNum(const std::string& key, signed long &data, signed long defValue = 0)
+  {
+    int retInt;
+    bool ret = get(key, retInt, defValue);
+    data = retInt;
+    return ret;
+  }
+
+  bool ReadNum(const std::string& key, unsigned int &data, unsigned int defValue = 0)
+  { return get(key, data, defValue); }
+
+  bool ReadNum(const std::string& key, signed int &data, signed int defValue = 0)
+  { return get(key, data, defValue); }
+
+  bool ReadNum(const std::string& key, unsigned short &data, unsigned short defValue = 0)
+  {
+    unsigned retInt;
+    bool ret = get(key, retInt, defValue);
+    data = retInt;
+    return ret;
+  }
+
+  bool ReadNum(const std::string& key, signed short &data, signed short defValue = 0)
+  {
+    int retInt;
+    bool ret = get(key, retInt, defValue);
+    data = retInt;
+    return ret;
+  }
+
+  bool ReadNum(const std::string& key, char &data, char defValue = 0)
+  {
+    int retInt;
+    bool ret = get(key, retInt, defValue);
+    data = retInt;
+    return ret;
+  }
+
+  bool ReadBool(const std::string& key, bool &data, const bool defValue = false)
+  { return get(key, data, defValue); }
 
   /**
    * Write a value to the configuration file
@@ -95,7 +172,8 @@ public:
    * @param key Key of value to write
    * @param data Value to write
    */
-  void writeVar(const std::string& key, const boost::any& data);
+  void writeVar(const std::string& key, const boost::any& data)
+  { set(key, data); }
 
   /**
    * Write a string value to the configuration file
@@ -103,51 +181,34 @@ public:
    * @param key Key of value to write
    * @param data Value to write
    */
-  void writeString(const std::string& key, const std::string& data);
+  void writeString(const std::string& key, const std::string& data)
+  { set(key, data); }
 
-  bool WriteStr(const std::string& key, const char* data);
-  bool WriteNum(const std::string& key, unsigned long data);
-  bool WriteNum(const std::string& key, signed long data);
-  bool WriteNum(const std::string& key, unsigned int data);
-  bool WriteNum(const std::string& key, signed int data);
-  bool WriteNum(const std::string& key, unsigned short data);
-  bool WriteNum(const std::string& key, signed short data);
-  bool WriteNum(const std::string& key, char data);
-  bool WriteBool(const std::string& key, bool data);
+  bool WriteStr(const std::string& key, const char* data)
+  { return set(key, std::string(data)); }
 
-  int Error()  { return (m_nError); }
-  const char *FileName()  { return m_szFilename; }
+  bool WriteNum(const std::string& key, unsigned long data)
+  { return set(key, static_cast<unsigned>(data)); }
+  bool WriteNum(const std::string& key, signed long data)
+  { return set(key, static_cast<int>(data)); }
+  bool WriteNum(const std::string& key, unsigned int data)
+  { return set(key, data); }
+  bool WriteNum(const std::string& key, signed int data)
+  { return set(key, data); }
+  bool WriteNum(const std::string& key, unsigned short data)
+  { return set(key, data); }
+  bool WriteNum(const std::string& key, signed short data)
+  { return set(key, data); }
+  bool WriteNum(const std::string& key, char data)
+  { return set(key, data); }
+  bool WriteBool(const std::string& key, bool data)
+  { return set(key, data); }
+
+  int Error()  { return errno; }
+  const char *FileName()  { return filename().c_str(); }
 
 protected:
-  // Data members
-  char *m_szFilename,
-       *m_szBuffer;
-  int m_nBufPos,
-      m_nBufSize,
-      m_nError;
-  char *m_szSectionName;
-  int m_nSectionStart,
-      m_nSectionEnd;
   unsigned short m_nFlags;
-  bool //m_bEof,
-       //m_bEos,
-       m_bChanged;
-
-  // Functions
-  char *ReadLine(char *_szDest, int nDestSize);
-  char *GetSectionFromLine(char *_szBuffer, const char *_szLine);
-  char *GetKeyFromLine(char *_szBuffer, const char *_szLine);
-  char *GetDataFromLine(char *_szBuffer, const char *_szLine,
-                        bool bTrim = true, int _nMax = 0);
-  void Warn(int nError, const char *_sz = NULL);
-  void InsertStr(const char *_szNewStr, int _nCutStart, int _nCutEnd);
-
-  /*bool Eof()
-    { return (m_bEof); };
-  bool Eos()
-    { return (m_bEos); };*/
-  void ResetFile();
-  void ResetSection();
 };
 
 
