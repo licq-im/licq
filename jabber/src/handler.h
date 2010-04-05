869f61df6d3424041ca83fead176fe75729b34fd
Erik Johansson <erijo@licq.org>
2010-04-05 18:23:56 +0000

    Updated copyright in headers to refer to a single file where all contributors are listed. Added copy of the GPL v2 license.

--- jabber/src/handler.h
+++ jabber/src/handler.h
@@ -1,6 +1,9 @@
 /*
  * This file is part of Licq, an instant messaging client for UNIX.
- * Copyright (C) 2010 Erik Johansson <erijo@licq.org>
+ * Copyright (C) 2010 Licq Developers <licq-dev@googlegroups.com>
+ *
+ * Please refer to the COPYRIGHT file distributed with this source
+ * distribution for the names of the individual contributors.
  *
  * Licq is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
