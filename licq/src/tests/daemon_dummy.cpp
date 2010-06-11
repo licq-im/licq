6428407d9b7e2f9774f9ae74f73a7dfe172133a2
Anders Olofsson <flynd@licq.org>
2010-06-11 20:34:21 +0000

    Added a dummy daemon implementation to allow classes in unittest that depend on gDaemon (although not use it) without having to include entire daemon.cpp and its dependancies.

---
+++ licq/src/tests/daemon_dummy.cpp
@@ -0,0 +1,42 @@
+/*
+ * This file is part of Licq, an instant messaging client for UNIX.
+ * Copyright (C) 2010 Licq developers
+ *
+ * Licq is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Licq is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Licq; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include <licq/daemon.h>
+
+/**
+ * This is a dummy implementation of the daemon class
+ * The purpose of this class is to make sure gDaemon is defined so unittest
+ * can be run on classes that need to link against it
+ */
+class DaemonDummy : public Licq::Daemon
+{
+public:
+  DaemonDummy() { }
+
+  // From Licq::Daemon
+  pthread_t* Shutdown() { return NULL; }
+  const char* Version() const { return NULL; }
+  void SaveConf() { }
+  Licq::LogService& getLogService() { return getLogService(); }
+  bool addUserEvent(Licq::User*, CUserEvent*) { return false; }
+  void rejectEvent(const Licq::UserId&, CUserEvent*) { }
+};
+
+DaemonDummy gDaemonDummy;
+Licq::Daemon& Licq::gDaemon(gDaemonDummy);
