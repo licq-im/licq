e2160d1cbf6d75e59916daa3527291a269d219b2
Eugene Paskevich <eugene@licq.org>
2010-07-21 15:42:55 +0000

    Got rid of reinterpret_cast in favor of static_cast and removed a FIX macro.

--- newosd/src/conf.cpp
+++ newosd/src/conf.cpp
@@ -5,12 +5,23 @@
 
 void Conf::loadConfig()
 {
+  unsigned int tmp;
   Licq::IniFile conf("licq_newosd.ini");
   conf.loadFile();
 
 #define GET(key, dst, def); \
   if (!conf.get((key), (dst), (def))) \
     conf.set((key), (dst));
+
+#define GETTMP(key, dst, def, limit) \
+  GET((key), tmp, static_cast<unsigned int>((def))); \
+  if (tmp > static_cast<unsigned int>((limit))) \
+  { \
+    tmp = (dst) = (def); \
+    conf.set((key), tmp); \
+  } \
+  else \
+    (dst) = static_cast<typeof((dst))>(tmp);
 
   conf.setSection("Appearance");
 
@@ -23,37 +34,14 @@ void Conf::loadConfig()
   GET("Message", textColor, "yellow");
   GET("Control", textControlColor, "gray");
   GET("Shadow", shadowColor, "black");
-  GET("BackOpacity", backOpacity, 0);
-  GET("TextOpacity", textOpacity, 255);
-  GET("ShadowOpacity", shadowOpacity, 255);
-
-#define FIX(key, dst); \
-  if ((dst) > 255) \
-  { \
-    (dst) %= 255; \
-    conf.set((key), (dst)); \
-  }
-
-  FIX("BackOpacity", backOpacity);
-  FIX("TextOpacity", textOpacity);
-  FIX("ShadowOpacity", shadowOpacity);
-#undef FIX
+  GETTMP("BackOpacity", backOpacity, 0, 255);
+  GETTMP("TextOpacity", textOpacity, 255, 255);
+  GETTMP("ShadowOpacity", shadowOpacity, 255, 255);
 
   conf.setSection("Geometry");
 
-  GET("HPosition", reinterpret_cast<unsigned&>(posHorizontal), 0);
-  GET("VPosition", reinterpret_cast<unsigned&>(posVertical), 0);
-
-#define FIX(key, dst); \
-  if ((dst) > COORDINATE_MAXIMUM) \
-  { \
-    (dst) = COORDINATE_MINIMUM; \
-    conf.set((key), reinterpret_cast<unsigned&>((dst))); \
-  }
-
-  FIX("HPosition", posHorizontal);
-  FIX("VPosition", posVertical);
-#undef FIX
+  GETTMP("HPosition", posHorizontal, COORDINATE_MINIMUM, COORDINATE_MAXIMUM);
+  GETTMP("VPosition", posVertical, COORDINATE_MINIMUM, COORDINATE_MAXIMUM);
 
   GET("HOffset", offsetHorizontal, 0);
   GET("VOffset", offsetVertical, 0);
@@ -80,29 +68,17 @@ void Conf::loadConfig()
 
   conf.setSection("Filtering");
 
-  GET("ShowLogonLogoff", reinterpret_cast<unsigned&>(logonLogoff), 2);
-  GET("ShowStatusChange", reinterpret_cast<unsigned&>(statusChange), 2);
-  GET("ShowAutoResponseCheck", reinterpret_cast<unsigned&>(autoResponse), 2);
-  GET("ShowMessages", reinterpret_cast<unsigned&>(showMessage), 2);
-
-#define FIX(key, dst); \
-  if ((dst) > GROUP_TYPE_ALL) \
-  { \
-    (dst) = GROUP_TYPE_ALL; \
-    conf.set((key), reinterpret_cast<unsigned&>((dst))); \
-  }
-
-  FIX("ShowLogonLogoff", logonLogoff);
-  FIX("ShowStatusChange", statusChange);
-  FIX("ShowAutoResponseCheck", autoResponse);
-  FIX("ShowMessages", showMessage);
-#undef FIX
+  GETTMP("ShowLogonLogoff", logonLogoff, GROUP_TYPE_ALL, GROUP_TYPE_ALL);
+  GETTMP("ShowStatusChange", statusChange, GROUP_TYPE_ALL, GROUP_TYPE_ALL);
+  GETTMP("ShowAutoResponseCheck", autoResponse, GROUP_TYPE_ALL, GROUP_TYPE_ALL);
+  GETTMP("ShowMessages", showMessage, GROUP_TYPE_ALL, GROUP_TYPE_ALL);
 
   GET("ShowMessagesNotification", notifyOnly, false);
   GET("ShowInModes", ownerModes, "");
   GET("ShowMsgsInModes", ownerModesMsg, "");
 
 #undef GET
+#undef GETTMP
 
   conf.writeFile();
 }
