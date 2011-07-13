licq-im/licq

commit 3bca380290d39c45b975ec19705bd15dce332793

api: Use owner instead of protocol in SignalLogon

jabber/src/handler.cpp
@@ -67,7 +67,7 @@ void Handler::onConnect(const string& ip, int port, unsigned status)
 
   Licq::gPluginManager.pushPluginSignal(
       new Licq::PluginSignal(Licq::PluginSignal::SignalLogon,
-                             0, UserId(), JABBER_PPID));
+      0, gUserManager.ownerUserId(JABBER_PPID)));
 }
 
 void Handler::onChangeStatus(unsigned status)

licq/include/licq/pluginsignal.h
@@ -1,6 +1,6 @@
 /*
  * This file is part of Licq, an instant messaging client for UNIX.
- * Copyright (C) 2010 Licq developers
+ * Copyright (C) 2010-2011 Licq developers
  *
  * Licq is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -40,7 +40,7 @@ public:
   {
     SignalList          = 1<<0,         // Contact list has changed
     SignalUser          = 1<<1,         // A user has changed
-    SignalLogon         = 1<<2,         // Owner logged on, argument is protocolId
+    SignalLogon         = 1<<2,         // Owner logged on, userId is owner
     SignalLogoff        = 1<<3,         // Owner logged off, userId is owner
     SignalOnEvent       = 1<<4,         // ?
     SignalUiViewEvent   = 1<<5,         // UI should popup oldest unread event for userId (if set)

licq/src/icq/icq-srv.cpp
@@ -4123,7 +4123,7 @@ void IcqProtocol::ProcessBOSFam(CBuffer& /* packet */, unsigned short nSubtype)
       Licq::Event* e = DoneExtendedServerEvent(0, Licq::Event::ResultSuccess);
     if (e != NULL) ProcessDoneEvent(e);
       gPluginManager.pushPluginSignal(new Licq::PluginSignal(
-          Licq::PluginSignal::SignalLogon, 0));
+          Licq::PluginSignal::SignalLogon, 0, Licq::gUserManager.ownerUserId(LICQ_PPID)));
 
     //icqSetStatus(m_nDesiredStatus);
       break;

commit c42f4539208f98765717eb374781e3604f762281

jabber: Fix deadlock caused by 3bca380

jabber/src/handler.cpp
@@ -57,12 +57,14 @@ void Handler::onConnect(const string& ip, int port, unsigned status)
 {
   TRACE();
 
-  Licq::OwnerWriteGuard owner(JABBER_PPID);
-  if (owner.isLocked())
   {
-    owner->statusChanged(status);
-    owner->SetIpPort(Licq::INetSocket::ipToInt(ip), port);
-    owner->SetTimezone(Licq::User::SystemTimezone());
+    Licq::OwnerWriteGuard owner(JABBER_PPID);
+    if (owner.isLocked())
+    {
+      owner->statusChanged(status);
+      owner->SetIpPort(Licq::INetSocket::ipToInt(ip), port);
+      owner->SetTimezone(Licq::User::SystemTimezone());
+    }
   }
 
   Licq::gPluginManager.pushPluginSignal(