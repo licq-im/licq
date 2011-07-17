licq-im/licq

commit 4b8780ebaf75a344934af3157d50386e85015e5d

api: Remove ICQ protocol commands from UserEvent

msn/src/msn-sb.cpp
@@ -143,7 +143,7 @@ void CMSN::ProcessSBPacket(char *szUser, CMSNBuffer *packet, int nSock)
           (*packet) >> szMsg[i];
         szMsg[i] = '\0';
 
-        Licq::EventMsg* e = new Licq::EventMsg(Licq::gTranslator.serverToClient(szMsg), ICQ_CMDxRCV_SYSxMSGxOFFLINE,
+        Licq::EventMsg* e = new Licq::EventMsg(Licq::gTranslator.serverToClient(szMsg),
             time(0), 0, SocketToCID(nSock));
         Licq::UserWriteGuard u(UserId(strUser, MSN_PPID));
         if (u.isLocked())
@@ -609,7 +609,6 @@ void CMSN::MSNSendMessage(unsigned long eventId, const UserId& userId, const str
   string msgDos = Licq::gTranslator.returnToDos(message);
   CMSNPacket* pSend = new CPS_MSNMessage(msgDos.c_str());
   Licq::EventMsg* m = new Licq::EventMsg(msgDos, 0, Licq::UserEvent::TimeNow, 0);
-  m->setIsReceiver(false);
   Licq::Event* e = new Licq::Event(eventId, 0, pSend, Licq::Event::ConnectServer, userId, m);
   e->thread_plugin = _tPlugin;  
 

jabber/src/handler.cpp
@@ -29,7 +29,6 @@
 #include <licq/contactlist/user.h>
 #include <licq/contactlist/usermanager.h>
 #include <licq/daemon.h>
-#include <licq/icqdefines.h>
 #include <licq/logging/log.h>
 #include <licq/oneventmanager.h>
 #include <licq/plugin/pluginmanager.h>
@@ -236,7 +235,7 @@ void Handler::onUserAuthorizationRequest(
       string(), string(), // first and last name
       string(), // email
       message,
-      ICQ_CMDxRCV_SYSxMSGxONLINE, time(0), 0);
+      time(0), 0);
 
   Licq::OwnerWriteGuard owner(JABBER_PPID);
   if (Licq::gDaemon.addUserEvent(*owner, event))
@@ -252,7 +251,7 @@ void Handler::onMessage(const string& from, const string& message, time_t sent,
   TRACE();
 
   Licq::EventMsg* event = new Licq::EventMsg(
-      message.c_str(), ICQ_CMDxRCV_SYSxMSGxOFFLINE, sent,
+      message.c_str(), sent,
       urgent ? unsigned(Licq::UserEvent::FlagUrgent) : 0,
       getConvoId(from));
 

jabber/src/plugin.cpp
@@ -356,8 +356,7 @@ void Plugin::doSendMessage(Licq::ProtoSendMessageSignal* signal)
       signal->userId().accountId(), signal->message(), signal->flags() & 0x40);
 
   Licq::EventMsg* message = new Licq::EventMsg(
-      signal->message().c_str(), 0, Licq::UserEvent::TimeNow, 0);
-  message->setIsReceiver(false);
+      signal->message().c_str(), Licq::EventMsg::TimeNow, Licq::EventMsg::FlagSender);
 
   Licq::Event* event = new Licq::Event(signal->eventId(), 0, NULL,
       Licq::Event::ConnectServer, signal->userId(), message);

licq/include/licq/userevents.h
@@ -55,6 +55,8 @@ public:
     FlagUrgent          = 0x00040000,
     FlagCancelled       = 0x00080000,
     FlagEncrypted       = 0x00100000,
+    FlagSender          = 0x00200000,
+    FlagOffline         = 0x00400000,
     FlagUnknown         = 0x80000000,
   };
 
@@ -81,7 +83,7 @@ public:
   // Use this constant for constructor to get current time
   static const time_t TimeNow = 0;
 
-  UserEvent(EventType eventType, unsigned short _nCommand,
+  UserEvent(EventType eventType,
               unsigned short _nSequence, time_t _tTime,
               unsigned long _nFlags, unsigned long _nConvoId = 0);
   UserEvent(const UserEvent *);
@@ -96,7 +98,6 @@ public:
 
   static const std::string licqVersionToString(unsigned long);
   unsigned short Sequence() const { return m_nSequence; }
-  unsigned short Command() const { return m_nCommand; }
 
   /// Get type of event
   unsigned eventType() const { return myEventType; }
@@ -112,7 +113,14 @@ public:
   bool IsLicq() const { return LicqVersion() != 0; };
   bool IsEncrypted() const { return m_nFlags & FlagEncrypted; };
   unsigned short LicqVersion() const { return m_nFlags & FlagLicqVerMask; }
-  bool isReceiver() const { return myIsReceiver; }
+
+  /// Was message sent from us
+  bool isSender() const { return m_nFlags & FlagSender; }
+
+  /// Was message sent while receiver was offline
+  bool isOffline() const { return m_nFlags & FlagOffline; }
+
+  bool isReceiver() const { return !isSender(); }
   const Color* color() const { return &myColor; }
   unsigned long ConvoId() const { return m_nConvoId; }
 
@@ -124,7 +132,6 @@ protected:
   int AddToHistory_Header(bool isReceiver, char* szOut) const;
   void AddToHistory_Flush(User* u, const std::string& text) const;
 
-  void setIsReceiver(bool isReceiver) { myIsReceiver = isReceiver; }
   void Cancel() { m_nFlags |= FlagCancelled; }
   void SetColor(unsigned fore, unsigned back) { myColor.set(fore, back); }
   void SetColor(const Color* c) { myColor.set(c); }
@@ -139,14 +146,12 @@ protected:
   // m_szText is not initialized until it is accessed. Allow this delayed
   // initialization even if called in const context.
   mutable std::string myText;
-   unsigned short m_nCommand;
   unsigned myEventType;
    unsigned short m_nSequence;
    int            m_nId;
    time_t         m_tTime;
    unsigned long  m_nFlags;
 
-  bool myIsReceiver;
    bool           m_bPending;
   Color myColor;
    unsigned long  m_nConvoId;
@@ -164,7 +169,7 @@ protected:
 class EventMsg : public UserEvent
 {
 public:
-  EventMsg(const std::string& message, unsigned short _nCommand,
+  EventMsg(const std::string& message,
              time_t _tTime, unsigned long _nFlags, unsigned long _nConvoId = 0);
 
   virtual EventMsg* Copy() const;
@@ -213,14 +218,13 @@ class EventUrl : public UserEvent
 {
 public:
   EventUrl(const std::string& url, const std::string& urlDescription,
-             unsigned short _nCommand, time_t _tTime,
-             unsigned long _nFlags, unsigned long _nConvoId = 0);
+      time_t _tTime, unsigned long _nFlags, unsigned long _nConvoId = 0);
   virtual EventUrl* Copy() const;
   virtual void AddToHistory(User* u, bool isReceiver) const;
   const std::string& url() const { return myUrl; }
   const std::string& urlDescription() const { return myUrlDescription; }
 
-  static EventUrl *Parse(char *sz, unsigned short nCmd, time_t nTime,
+  static EventUrl *Parse(char *sz, time_t nTime,
      unsigned long nFlags, unsigned long nConvoId = 0);
 protected:
   void CreateDescription() const;
@@ -264,8 +268,7 @@ class EventAdded : public UserEvent
 public:
   EventAdded(const UserId& userId, const std::string& alias,
       const std::string& firstName, const std::string& lastName,
-      const std::string& email, unsigned short _nCommand, time_t _tTime,
-      unsigned long _nFlags);
+      const std::string& email, time_t _tTime, unsigned long _nFlags);
   virtual EventAdded* Copy() const;
   virtual void AddToHistory(User* u, bool isReceiver) const;
   const UserId& userId() const { return myUserId; }
@@ -289,7 +292,7 @@ public:
   EventAuthRequest(const UserId& userId, const std::string& alias,
       const std::string& firstName, const std::string& lastName,
       const std::string& email, const std::string& reason,
-      unsigned short _nCommand, time_t _tTime, unsigned long _nFlags);
+      time_t _tTime, unsigned long _nFlags);
   virtual EventAuthRequest* Copy() const;
   virtual void AddToHistory(User* u, bool isReceiver) const;
   const UserId& userId() const { return myUserId; }
@@ -313,7 +316,7 @@ class EventAuthGranted : public UserEvent
 {
 public:
   EventAuthGranted(const UserId& userId, const std::string& message,
-                     unsigned short _nCommand, time_t _tTime, unsigned long _nFlags);
+      time_t _tTime, unsigned long _nFlags);
   virtual EventAuthGranted* Copy() const;
   virtual void AddToHistory(User* u, bool isReceiver) const;
   const UserId& userId() const { return myUserId; }
@@ -333,7 +336,7 @@ class EventAuthRefused : public UserEvent
 {
 public:
   EventAuthRefused(const UserId& userId, const std::string& message,
-                     unsigned short _nCommand, time_t _tTime, unsigned long _nFlags);
+      time_t _tTime, unsigned long _nFlags);
   virtual EventAuthRefused* Copy() const;
   virtual void AddToHistory(User* u, bool isReceiver) const;
   const UserId& userId() const { return myUserId; }
@@ -353,8 +356,7 @@ class EventWebPanel : public UserEvent
 {
 public:
   EventWebPanel(const std::string& name, const std::string& email,
-      const std::string& message, unsigned short _nCommand, time_t _tTime,
-      unsigned long _nFlags);
+      const std::string& message, time_t _tTime, unsigned long _nFlags);
   virtual EventWebPanel* Copy() const;
   virtual void AddToHistory(User* u, bool isReceiver) const;
   const std::string& message() const { return myMessage; }
@@ -374,8 +376,7 @@ class EventEmailPager : public UserEvent
 {
 public:
   EventEmailPager(const std::string& name, const std::string& email,
-      const std::string& message, unsigned short _nCommand, time_t _tTime,
-      unsigned long _nFlags);
+      const std::string& message, time_t _tTime, unsigned long _nFlags);
   virtual EventEmailPager* Copy() const;
   virtual void AddToHistory(User* u, bool isReceiver) const;
   const std::string& message() const { return myMessage; }
@@ -409,7 +410,7 @@ public:
   typedef std::list<Contact *> ContactList;
 
 
-  EventContactList(const ContactList& cl, bool bDeep, unsigned short nCommand,
+  EventContactList(const ContactList& cl, bool bDeep,
      time_t tTime, unsigned long nFlags);
   virtual ~EventContactList();
   virtual EventContactList* Copy() const;
@@ -417,7 +418,7 @@ public:
 
   const ContactList &Contacts() const { return m_vszFields; }
 
-  static EventContactList *Parse(char *sz, unsigned short nCmd, time_t nTime, unsigned long nFlags);
+  static EventContactList *Parse(char *sz, time_t nTime, unsigned long nFlags);
 protected:
   void CreateDescription() const;
   std::string eventName() const;
@@ -431,13 +432,13 @@ class EventSms : public UserEvent
 {
 public:
   EventSms(const std::string& number, const std::string& message,
-             unsigned short _nCommand, time_t _tTime, unsigned long _nFlags);
+      time_t _tTime, unsigned long _nFlags);
   virtual EventSms* Copy() const;
   const std::string& number() const { return myNumber; }
   const std::string& message() const { return myMessage; }
   virtual void AddToHistory(User* u, bool isReceiver) const;
 
-  static EventSms* Parse(const std::string& s, unsigned short nCmd, time_t nTime, unsigned long nFlags);
+  static EventSms* Parse(const std::string& s, time_t nTime, unsigned long nFlags);
 protected:
   void CreateDescription() const;
   std::string eventName() const;
@@ -524,10 +525,13 @@ public:
   virtual EventUnknownSysMsg* Copy() const;
   virtual void AddToHistory(User* u, bool isReceiver) const;
   unsigned short subCommand() const { return m_nSubCommand; }
+  unsigned short Command() const { return m_nCommand; }
+
 protected:
   void CreateDescription() const;
   std::string eventName() const;
 
+  unsigned short m_nCommand;
   unsigned short m_nSubCommand;
   UserId myUserId;
   std::string myMessage;

licq/src/contactlist/userhistory.cpp
@@ -148,6 +148,16 @@ bool UserHistory::load(Licq::HistoryList& lHistory) const
     nCommand = atoi(&sz[13]);
     nFlags = atoi(&sz[20]) << 16;
     tTime = (time_t)atoi(&sz[27]);
+
+    // nCommand == ICQ_CMDxTCP_START => FlagDirect (already present in flags)
+    // nCommand == ICQ_CMDxSND_THRUxSERVER => FlagSender (present in cDir)
+    // nCommand == ICQ_CMDxRCV_SYSxMSGxONLINE => No additional flags set
+    if (nCommand == ICQ_CMDxRCV_SYSxMSGxOFFLINE)
+      nFlags |= Licq::UserEvent::FlagOffline;
+
+    if (cDir != 'R')
+      nFlags |= Licq::UserEvent::FlagSender;
+
     // Now read in the message
     szMsg[0] = '\0';
     e = NULL;
@@ -156,7 +166,7 @@ bool UserHistory::load(Licq::HistoryList& lHistory) const
     case ICQ_CMDxSUB_MSG:
     {
       GET_VALID_LINES;
-        e = new Licq::EventMsg(szMsg, nCommand, tTime, nFlags);
+        e = new Licq::EventMsg(szMsg, tTime, nFlags);
         break;
     }
     case ICQ_CMDxSUB_CHAT:
@@ -198,9 +208,9 @@ bool UserHistory::load(Licq::HistoryList& lHistory) const
       GET_VALID_LINE_OR_BREAK;
         string url = &szResult[1];
       GET_VALID_LINES;
-        e = new Licq::EventUrl(url, szMsg, nCommand, tTime, nFlags);
-      break;
-    }
+        e = new Licq::EventUrl(url, szMsg, tTime, nFlags);
+        break;
+      }
     case ICQ_CMDxSUB_AUTHxREQUEST:
     {
       GET_VALID_LINE_OR_BREAK;
@@ -215,25 +225,25 @@ bool UserHistory::load(Licq::HistoryList& lHistory) const
         string email = &szResult[1];
       GET_VALID_LINES;
         e = new Licq::EventAuthRequest(userId, alias, firstName, lastName,
-            email, szMsg, nCommand, tTime, nFlags);
-      break;
-    }
+            email, szMsg, tTime, nFlags);
+        break;
+      }
     case ICQ_CMDxSUB_AUTHxGRANTED:
     {
       GET_VALID_LINE_OR_BREAK;
         UserId userId(&szResult[1], myPpid);
       GET_VALID_LINES;
-        e = new Licq::EventAuthGranted(userId, szMsg, nCommand, tTime, nFlags);
-      break;
-    }
+        e = new Licq::EventAuthGranted(userId, szMsg, tTime, nFlags);
+        break;
+      }
     case ICQ_CMDxSUB_AUTHxREFUSED:
     {
       GET_VALID_LINE_OR_BREAK;
         UserId userId(&szResult[1], myPpid);
       GET_VALID_LINES;
-        e = new Licq::EventAuthRefused(userId, szMsg, nCommand, tTime, nFlags);
-      break;
-    }
+        e = new Licq::EventAuthRefused(userId, szMsg, tTime, nFlags);
+        break;
+      }
     case ICQ_CMDxSUB_ADDEDxTOxLIST:
     {
       GET_VALID_LINE_OR_BREAK;
@@ -247,7 +257,7 @@ bool UserHistory::load(Licq::HistoryList& lHistory) const
       GET_VALID_LINE_OR_BREAK;
         string email = &szResult[1];
         e = new Licq::EventAdded(userId, alias, firstName, lastName, email,
-                            nCommand, tTime, nFlags);
+            tTime, nFlags);
       break;
     }
     case ICQ_CMDxSUB_WEBxPANEL:
@@ -257,10 +267,9 @@ bool UserHistory::load(Licq::HistoryList& lHistory) const
       GET_VALID_LINE_OR_BREAK;
         string email = &szResult[1];
       GET_VALID_LINES;
-        e = new Licq::EventWebPanel(name, email, szMsg,
-                             nCommand, tTime, nFlags);
-      break;
-    }
+        e = new Licq::EventWebPanel(name, email, szMsg, tTime, nFlags);
+        break;
+      }
     case ICQ_CMDxSUB_EMAILxPAGER:
     {
       GET_VALID_LINE_OR_BREAK;
@@ -268,10 +277,9 @@ bool UserHistory::load(Licq::HistoryList& lHistory) const
       GET_VALID_LINE_OR_BREAK;
         string email = &szResult[1];
       GET_VALID_LINES;
-        e = new Licq::EventWebPanel(name, email, szMsg,
-                             nCommand, tTime, nFlags);
-      break;
-    }
+        e = new Licq::EventEmailPager(name, email, szMsg, tTime, nFlags);
+        break;
+      }
     case ICQ_CMDxSUB_CONTACTxLIST:
     {
       Licq::EventContactList::ContactList vc;
@@ -289,7 +297,7 @@ bool UserHistory::load(Licq::HistoryList& lHistory) const
           }
         b = !b;
       }
-        e = new Licq::EventContactList(vc, false, nCommand, tTime, nFlags);
+        e = new Licq::EventContactList(vc, false, tTime, nFlags);
         break;
     }
     case ICQ_CMDxSUB_SMS:
@@ -297,9 +305,9 @@ bool UserHistory::load(Licq::HistoryList& lHistory) const
       GET_VALID_LINE_OR_BREAK;
         string number = &szResult[1];
       GET_VALID_LINES;
-        e = new Licq::EventSms(number, szMsg, nCommand, tTime, nFlags);
-      break;
-    }
+        e = new Licq::EventSms(number, szMsg, tTime, nFlags);
+        break;
+      }
     case ICQ_CMDxSUB_MSGxSERVER:
     {
       GET_VALID_LINE_OR_BREAK;
@@ -326,7 +334,6 @@ bool UserHistory::load(Licq::HistoryList& lHistory) const
     }
     if (e != NULL)
     {
-      e->setIsReceiver(cDir == 'R');
       e->SetPending(false);
       lHistory.push_back(e);
     }

licq/src/icq/icq-srv.cpp
@@ -1295,8 +1295,6 @@ Licq::Event* IcqProtocol::icqSendThroughServer(unsigned long eventId, const Licq
   if (gDaemon.shuttingDown())
     return NULL;
 
-  if (ue != NULL)
-    ue->setIsReceiver(false);
   Licq::Event* e = new Licq::Event(eventId, m_nTCPSrvSocketDesc, p, Licq::Event::ConnectServer, userId, ue);
   if (e == NULL) return 0;
   e->m_NoAck = true;
@@ -1308,8 +1306,8 @@ Licq::Event* IcqProtocol::icqSendThroughServer(unsigned long eventId, const Licq
 unsigned long IcqProtocol::icqSendSms(const Licq::UserId& userId,
     const string& number, const string& message)
 {
-  Licq::EventSms* ue = new Licq::EventSms(number, message, ICQ_CMDxSND_THRUxSERVER,
-      Licq::EventSms::TimeNow, LICQ_VERSION);
+  Licq::EventSms* ue = new Licq::EventSms(number, message,
+      Licq::EventSms::TimeNow, LICQ_VERSION | Licq::EventSms::FlagSender);
   CPU_SendSms* p = new CPU_SendSms(number, message);
   gLog.info(tr("Sending SMS through server (#%hu/#%d)..."), p->Sequence(), p->SubSequence());
   Licq::Event* e = SendExpectEvent_Server(userId, p, ue);
@@ -2717,7 +2715,7 @@ void IcqProtocol::ProcessMessageFam(CBuffer &packet, unsigned short nSubtype)
           }
 
       // now send the message to the user
-          Licq::EventMsg* e = new Licq::EventMsg(m, ICQ_CMDxRCV_SYSxMSGxONLINE, nTimeSent, 0);
+          Licq::EventMsg* e = new Licq::EventMsg(m, nTimeSent, 0);
 
       if (ignore)
       {
@@ -3017,7 +3015,7 @@ However it seems to always think contact is online instead of away/occupied/etc.
         case ICQ_CMDxSUB_MSG:
             {
               Licq::EventMsg* e = new Licq::EventMsg(Licq::gTranslator.serverToClient(szMessage),
-                  ICQ_CMDxRCV_SYSxMSGxONLINE, nTimeSent, nMask);
+                  nTimeSent, nMask);
               type = tr("Message");
               onEventType = OnEventData::OnEventMessage;
           eEvent = e;
@@ -3025,7 +3023,7 @@ However it seems to always think contact is online instead of away/occupied/etc.
         }
         case ICQ_CMDxSUB_URL:
         {
-          Licq::EventUrl* e = Licq::EventUrl::Parse(szMessage, ICQ_CMDxRCV_SYSxMSGxONLINE, nTimeSent, nMask);
+          Licq::EventUrl* e = Licq::EventUrl::Parse(szMessage, nTimeSent, nMask);
           if (e == NULL)
           {
             packet.log(Log::Warning, tr("Invalid URL message"));
@@ -3058,7 +3056,6 @@ However it seems to always think contact is online instead of away/occupied/etc.
                                                        szFields[0], szFields[1],
                                                        szFields[2], szFields[3],
                                                        szFields[5],
-                                                       ICQ_CMDxRCV_SYSxMSGxONLINE,
                                                        nTimeSent, 0);
 
           delete [] szFields;
@@ -3073,7 +3070,6 @@ However it seems to always think contact is online instead of away/occupied/etc.
           gTranslator.ServerToClient(szMessage);
 
               Licq::EventAuthRefused* e = new Licq::EventAuthRefused(userId, szMessage,
-                                                       ICQ_CMDxRCV_SYSxMSGxONLINE, 
                                                        nTimeSent, 0);
           eEvent = e;
           break;
@@ -3092,7 +3088,7 @@ However it seems to always think contact is online instead of away/occupied/etc.
               }
 
               Licq::EventAuthGranted* e = new Licq::EventAuthGranted(userId,
-            szMessage, ICQ_CMDxRCV_SYSxMSGxONLINE, nTimeSent, 0);
+                  szMessage, nTimeSent, 0);
           eEvent = e;
           break;
         }
@@ -3129,7 +3125,7 @@ However it seems to always think contact is online instead of away/occupied/etc.
           gTranslator.ServerToClient (szFields[2]);  // last name
 
               Licq::EventAdded* e = new Licq::EventAdded(userId, szFields[0],
-            szFields[1], szFields[2], szFields[3], ICQ_CMDxRCV_SYSxMSGxONLINE,
+                  szFields[1], szFields[2], szFields[3],
             nTimeSent, 0);
           delete [] szFields;
           eEvent = e;
@@ -3155,8 +3151,7 @@ However it seems to always think contact is online instead of away/occupied/etc.
 
           gLog.info(tr("From %s (%s)"), szFields[0], szFields[3]);
               Licq::EventWebPanel* e = new Licq::EventWebPanel(szFields[0], szFields[3],
-                                                 szFields[5], ICQ_CMDxRCV_SYSxMSGxONLINE,
-                                                 nTimeSent, 0);
+                  szFields[5], nTimeSent, 0);
           delete [] szFields;
           eEvent = e;
           break;
@@ -3181,8 +3176,7 @@ However it seems to always think contact is online instead of away/occupied/etc.
 
           gLog.info(tr("From %s (%s)"), szFields[0], szFields[3]);
               Licq::EventEmailPager* e = new Licq::EventEmailPager(szFields[0], szFields[3],
-                                                     szFields[5], ICQ_CMDxRCV_SYSxMSGxONLINE,
-                                                     nTimeSent, 0);
+                  szFields[5], nTimeSent, 0);
           delete [] szFields;
           eEvent = e;
           break;
@@ -3190,7 +3184,7 @@ However it seems to always think contact is online instead of away/occupied/etc.
         case ICQ_CMDxSUB_CONTACTxLIST:
         {
           Licq::EventContactList* e = Licq::EventContactList::Parse(szMessage,
-              ICQ_CMDxRCV_SYSxMSGxONLINE,nTimeSent, nMask);
+                  nTimeSent, nMask);
           if (e == NULL)
           {
             packet.log(Log::Warning, tr("Invalid Contact List message"));
@@ -3204,8 +3198,7 @@ However it seems to always think contact is online instead of away/occupied/etc.
         }
         case ICQ_CMDxSUB_SMS:
         {
-          Licq::EventSms* e = Licq::EventSms::Parse(szMessage, ICQ_CMDxRCV_SYSxMSGxONLINE,
-              nTimeSent, nMask);
+              Licq::EventSms* e = Licq::EventSms::Parse(szMessage, nTimeSent, nMask);
           if (e == NULL)
           {
             packet.log(Log::Warning, tr("Invalid SMS message"));
@@ -3990,7 +3983,7 @@ void IcqProtocol::ProcessListFam(CBuffer &packet, unsigned short nSubtype)
       szMsg[nMsgLen] = '\0';
 
       Licq::EventAuthRequest* e = new Licq::EventAuthRequest(userId, "", "", "", "", nMsgLen ? szMsg : "",
-                                                   ICQ_CMDxRCV_SYSxMSGxONLINE, time(0), 0);
+          time(0), 0);
 
       Licq::OwnerWriteGuard o(LICQ_PPID);
       if (gDaemon.addUserEvent(*o, e))
@@ -4023,8 +4016,7 @@ void IcqProtocol::ProcessListFam(CBuffer &packet, unsigned short nSubtype)
       Licq::UserEvent* eEvent;
       if (granted)
       {
-        eEvent = new Licq::EventAuthGranted(userId, szMsg,
-           ICQ_CMDxRCV_SYSxMSGxONLINE, time(0), 0);
+        eEvent = new Licq::EventAuthGranted(userId, szMsg, time(0), 0);
 
         Licq::UserWriteGuard u(userId);
         if (u.isLocked())
@@ -4035,8 +4027,7 @@ void IcqProtocol::ProcessListFam(CBuffer &packet, unsigned short nSubtype)
       }
       else
       {
-        eEvent = new Licq::EventAuthRefused(userId, szMsg,
-            ICQ_CMDxRCV_SYSxMSGxONLINE, time(0), 0);
+        eEvent = new Licq::EventAuthRefused(userId, szMsg, time(0), 0);
       }
 
       Licq::OwnerWriteGuard o(LICQ_PPID);
@@ -4058,7 +4049,7 @@ void IcqProtocol::ProcessListFam(CBuffer &packet, unsigned short nSubtype)
       gLog.info(tr("User %s added you to their contact list."), szId);
 
       Licq::EventAdded* e = new Licq::EventAdded(userId, "", "", "", "",
-                                       ICQ_CMDxRCV_SYSxMSGxONLINE, time(0), 0);
+          time(0), 0);
       {
         Licq::OwnerWriteGuard o(LICQ_PPID);
         if (gDaemon.addUserEvent(*o, e))
@@ -4187,7 +4178,8 @@ void IcqProtocol::ProcessVariousFam(CBuffer &packet, unsigned short nSubtype)
 
       // Msg type & flags
       unsigned short nTypeMsg = msg.UnpackUnsignedShort();
-          unsigned long nMask = ((nTypeMsg & ICQ_CMDxSUB_FxMULTIREC) ? (int)Licq::UserEvent::FlagMultiRec : 0);
+          unsigned long nMask = Licq::UserEvent::FlagOffline |
+              ((nTypeMsg & ICQ_CMDxSUB_FxMULTIREC) ? (int)Licq::UserEvent::FlagMultiRec : 0);
       nTypeMsg &= ~ICQ_CMDxSUB_FxMULTIREC;
       
       char* szMessage = new char[msg.getDataMaxSize()];
@@ -4202,7 +4194,7 @@ void IcqProtocol::ProcessVariousFam(CBuffer &packet, unsigned short nSubtype)
         case ICQ_CMDxSUB_MSG:
             {
               Licq::EventMsg* e = new Licq::EventMsg(Licq::gTranslator.serverToClient(szMessage),
-                  ICQ_CMDxRCV_SYSxMSGxOFFLINE, nTimeSent, nMask);
+                  nTimeSent, nMask);
 	      type = tr("Message");
               onEventType = OnEventData::OnEventMessage;
 	  eEvent = e;
@@ -4210,7 +4202,7 @@ void IcqProtocol::ProcessVariousFam(CBuffer &packet, unsigned short nSubtype)
 	}
 	case ICQ_CMDxSUB_URL:
         {
-          Licq::EventUrl* e = Licq::EventUrl::Parse(szMessage, ICQ_CMDxRCV_SYSxMSGxOFFLINE, nTimeSent, nMask);
+          Licq::EventUrl* e = Licq::EventUrl::Parse(szMessage, nTimeSent, nMask);
 	  if (e == NULL)
 	  {
             packet.log(Log::Warning, tr("Invalid offline URL message"));
@@ -4243,7 +4235,7 @@ void IcqProtocol::ProcessVariousFam(CBuffer &packet, unsigned short nSubtype)
 
               Licq::EventAuthRequest* e = new Licq::EventAuthRequest(userId,
                   szFields[0], szFields[1], szFields[2], szFields[3],
-                  szFields[5], ICQ_CMDxRCV_SYSxMSGxOFFLINE, nTimeSent, 0);
+                  szFields[5], nTimeSent, Licq::EventAuthRequest::FlagOffline);
               delete [] szFields;
 	  eEvent = e;
 	  break;
@@ -4256,7 +4248,7 @@ void IcqProtocol::ProcessVariousFam(CBuffer &packet, unsigned short nSubtype)
           gTranslator.ServerToClient(szMessage);
 
               Licq::EventAuthRefused* e = new Licq::EventAuthRefused(userId,
-                  szMessage, ICQ_CMDxRCV_SYSxMSGxOFFLINE, nTimeSent, 0);
+                  szMessage, nTimeSent, Licq::EventAuthRefused::FlagOffline);
 	  eEvent = e;
 	  break;
 	}
@@ -4274,7 +4266,7 @@ void IcqProtocol::ProcessVariousFam(CBuffer &packet, unsigned short nSubtype)
               }
 
               Licq::EventAuthGranted* e = new Licq::EventAuthGranted(userId,
-                  szMessage, ICQ_CMDxRCV_SYSxMSGxOFFLINE, nTimeSent, 0);
+                  szMessage, nTimeSent, Licq::EventAuthGranted::FlagOffline);
 	  eEvent = e;
 	  break;
 	}
@@ -4312,7 +4304,7 @@ void IcqProtocol::ProcessVariousFam(CBuffer &packet, unsigned short nSubtype)
 
               Licq::EventAdded* e = new Licq::EventAdded(userId, szFields[0],
                   szFields[1], szFields[2], szFields[3],
-                  ICQ_CMDxRCV_SYSxMSGxOFFLINE, nTimeSent, 0);
+                  nTimeSent, Licq::EventAdded::FlagOffline);
               delete [] szFields;
 	  eEvent = e;
 	  break;
@@ -4337,7 +4329,7 @@ void IcqProtocol::ProcessVariousFam(CBuffer &packet, unsigned short nSubtype)
 
           gLog.info(tr("From %s (%s)"), szFields[0], szFields[3]);
               Licq::EventWebPanel* e = new Licq::EventWebPanel(szFields[0], szFields[3], szFields[5],
-                                                 ICQ_CMDxRCV_SYSxMSGxOFFLINE, nTimeSent, 0);
+                  nTimeSent, Licq::EventWebPanel::FlagOffline);
           delete [] szFields;	
           eEvent = e;
           break;
@@ -4362,7 +4354,7 @@ void IcqProtocol::ProcessVariousFam(CBuffer &packet, unsigned short nSubtype)
 
           gLog.info(tr("From %s (%s)"), szFields[0], szFields[3]);
               Licq::EventEmailPager* e = new Licq::EventEmailPager(szFields[0], szFields[3], szFields[5],
-                                                     ICQ_CMDxRCV_SYSxMSGxOFFLINE, nTimeSent, 0);
+                  nTimeSent, Licq::EventEmailPager::FlagOffline);
 	  delete [] szFields;	
 	  eEvent = e;
 	  break;
@@ -4370,7 +4362,7 @@ void IcqProtocol::ProcessVariousFam(CBuffer &packet, unsigned short nSubtype)
 	case ICQ_CMDxSUB_CONTACTxLIST:
         {
           Licq::EventContactList* e = Licq::EventContactList::Parse(szMessage,
-              ICQ_CMDxRCV_SYSxMSGxOFFLINE, nTimeSent, nMask);
+                  nTimeSent, nMask);
           if (e == NULL)
           {
             packet.log(Log::Warning, tr("Invalid offline Contact List message"));
@@ -4383,7 +4375,7 @@ void IcqProtocol::ProcessVariousFam(CBuffer &packet, unsigned short nSubtype)
 	}
 	case ICQ_CMDxSUB_SMS:
         {
-          Licq::EventSms* e = Licq::EventSms::Parse(szMessage, ICQ_CMDxRCV_SYSxMSGxONLINE, nTimeSent, nMask);
+              Licq::EventSms* e = Licq::EventSms::Parse(szMessage, nTimeSent, nMask);
 	  if (e == NULL)
           {
             packet.log(Log::Warning, tr("Invalid SMS message"));

commit 57dd99315e675df089a36a0cb5e8c3cafda1c04f

msn: Flag sent messages as sent
Broken since: 4b8780ebaf75a344934af3157d50386e85015e5d

msn/src/msn-sb.cpp
@@ -608,7 +608,7 @@ void CMSN::MSNSendMessage(unsigned long eventId, const UserId& userId, const str
 
   string msgDos = Licq::gTranslator.returnToDos(message);
   CMSNPacket* pSend = new CPS_MSNMessage(msgDos.c_str());
-  Licq::EventMsg* m = new Licq::EventMsg(msgDos, 0, Licq::UserEvent::TimeNow, 0);
+  Licq::EventMsg* m = new Licq::EventMsg(msgDos, Licq::UserEvent::TimeNow, Licq::EventMsg::FlagSender);
   Licq::Event* e = new Licq::Event(eventId, 0, pSend, Licq::Event::ConnectServer, userId, m);
   e->thread_plugin = _tPlugin;  
 