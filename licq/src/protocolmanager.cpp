licq-im/licq

commit 502672abae00d9dec74ff11e1d3efc3f9e43bb5a

Created a new manager to handle protocol requests from plugins. This manager will replace the proto functions in daemon class.

licq/src/protocolmanager.cpp
@@ -1,0 +1,474 @@
+/*
+ * This file is part of Licq, an instant messaging client for UNIX.
+ * Copyright (C) 2010 Licq developers
+ *
+ * Licq is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Licq is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Licq; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "protocolmanager.h"
+
+#include <licq_events.h>
+#include <licq_icqd.h>
+#include <licq_log.h>
+#include <licq/contactlist/usermanager.h>
+#include <licq/userid.h>
+
+#include "gettext.h"
+
+using namespace std;
+using namespace LicqDaemon;
+using Licq::OwnerReadGuard;
+using Licq::OwnerWriteGuard;
+using Licq::UserId;
+using Licq::UserReadGuard;
+using Licq::gUserManager;
+
+
+// Declare global PluginManager (internal for daemon)
+LicqDaemon::ProtocolManager LicqDaemon::gProtocolManager;
+
+// Initialize global Licq::PluginManager to refer to the internal PluginManager
+Licq::ProtocolManager& Licq::gProtocolManager(LicqDaemon::gProtocolManager);
+
+const char* const Licq::ProtocolManager::KeepAutoResponse = "__unset__";
+
+
+ProtocolManager::ProtocolManager()
+{
+  // Empty
+}
+
+ProtocolManager::~ProtocolManager()
+{
+  // Empty
+}
+
+unsigned long ProtocolManager::getNextEventId()
+{
+  // Event id generation is still owned by daemon as it's used directly by some ICQ functions
+  return gLicqDaemon->getNextEventId();
+}
+
+void ProtocolManager::pushProtoSignal(LicqProtoSignal* s, const UserId& userId)
+{
+  gLicqDaemon->PushProtoSignal(s, userId.protocolId());
+}
+
+void ProtocolManager::addUser(const UserId& userId, int groupId)
+{
+  if (userId.protocolId() == LICQ_PPID)
+    gLicqDaemon->icqAddUser(userId.accountId().c_str(), false, groupId);
+  else
+    pushProtoSignal(new LicqProtoAddUserSignal(userId, false), userId);
+}
+
+void ProtocolManager::removeUser(const UserId& userId)
+{
+  bool tempUser;
+
+  {
+    UserReadGuard user(userId);
+    if (!user.isLocked())
+      return;
+
+    tempUser = user->NotInList();
+  }
+
+  if (userId.protocolId() == LICQ_PPID)
+  {
+    if (!tempUser)
+      gLicqDaemon->icqRemoveUser(userId.accountId().c_str());
+  }
+  else
+    pushProtoSignal(new LicqProtoRemoveUserSignal(userId), userId);
+}
+
+void ProtocolManager::updateUserAlias(const UserId& userId)
+{
+  string newAlias;
+
+  {
+    UserReadGuard user(userId);
+    if (!user.isLocked())
+      return;
+
+    newAlias = user->getAlias();
+  }
+
+  if (userId.protocolId() == LICQ_PPID)
+    gLicqDaemon->icqRenameUser(userId.accountId(), newAlias);
+  else
+    pushProtoSignal(new LicqProtoRenameUserSignal(userId), userId);
+}
+
+unsigned long ProtocolManager::setStatus(const UserId& ownerId,
+    unsigned short newStatus, const string& message)
+{
+  bool isOffline;
+
+  {
+    OwnerWriteGuard owner(ownerId);
+    if (!owner.isLocked())
+      return 0;
+
+    isOffline = owner->StatusOffline();
+    if (message != KeepAutoResponse)
+      owner->SetAutoResponse(message.c_str());
+  }
+
+  unsigned long eventId = 0;
+
+  if (newStatus == ICQ_STATUS_OFFLINE)
+  {
+    if (isOffline)
+      return 0;
+
+    if (ownerId.protocolId() == LICQ_PPID)
+      gLicqDaemon->icqLogoff();
+    else
+      pushProtoSignal(new LicqProtoLogoffSignal(), ownerId);
+  }
+  else if(isOffline)
+  {
+    if (ownerId.protocolId() == LICQ_PPID)
+      eventId = gLicqDaemon->icqLogon(newStatus);
+    else
+      pushProtoSignal(new LicqProtoLogonSignal(newStatus), ownerId);
+  }
+  else
+  {
+    if (ownerId.protocolId() == LICQ_PPID)
+      eventId = gLicqDaemon->icqSetStatus(newStatus);
+    else
+      pushProtoSignal(new LicqProtoChangeStatusSignal(newStatus), ownerId);
+  }
+
+  return eventId;
+}
+
+void ProtocolManager::sendTypingNotification(const UserId& userId, bool active, int nSocket)
+{
+  if (!gLicqDaemon->SendTypingNotification())
+    return;
+
+  if (userId.protocolId() == LICQ_PPID)
+    gLicqDaemon->icqTypingNotification(userId.accountId().c_str(), active);
+  else
+    pushProtoSignal(new LicqProtoTypingNotificationSignal(userId, active, nSocket), userId);
+}
+
+unsigned long ProtocolManager::sendMessage(const UserId& userId, const string& message,
+    bool viaServer, unsigned short flags, bool multipleRecipients, CICQColor* color,
+    unsigned long convoId)
+{
+  unsigned long eventId = getNextEventId();
+
+  if (userId.protocolId() == LICQ_PPID)
+    gLicqDaemon->icqSendMessage(eventId, userId, message, viaServer, flags, multipleRecipients, color);
+  else
+    pushProtoSignal(new LicqProtoSendMessageSignal(eventId, userId, message, convoId), userId);
+
+  return eventId;
+}
+
+unsigned long ProtocolManager::sendUrl(const UserId& userId, const string& url,
+    const string& message, bool viaServer, unsigned short flags,
+    bool multipleRecipients, CICQColor* color)
+{
+  unsigned long eventId = getNextEventId();
+
+  if (userId.protocolId() == LICQ_PPID)
+    gLicqDaemon->icqSendUrl(eventId, userId, url, message, viaServer, flags, multipleRecipients, color);
+  else
+    eventId = 0;
+
+  return eventId;
+}
+
+unsigned long ProtocolManager::requestUserAutoResponse(const UserId& userId)
+{
+  unsigned long eventId = getNextEventId();
+
+  if (userId.protocolId() == LICQ_PPID)
+    gLicqDaemon->icqFetchAutoResponseServer(eventId, userId.accountId().c_str());
+  else
+    eventId = 0;
+
+  return eventId;
+}
+
+
+unsigned long ProtocolManager::fileTransferPropose(const UserId& userId,
+    const string& filename, const string& message, ConstFileList& files,
+    unsigned short flags, bool viaServer)
+{
+  unsigned long eventId = getNextEventId();
+
+  if (userId.protocolId() == LICQ_PPID)
+    gLicqDaemon->icqFileTransfer(eventId, userId, filename, message, files, flags, viaServer);
+  else
+    pushProtoSignal(new LicqProtoSendFileSignal(eventId, userId, filename, message, files), userId);
+
+  return eventId;
+}
+
+void ProtocolManager::fileTransferCancel(const UserId& userId, unsigned long eventId)
+{
+  if (userId.protocolId() == LICQ_PPID)
+    gLicqDaemon->icqFileTransferCancel(userId, (unsigned short)eventId);
+  else
+    pushProtoSignal(new LicqProtoCancelEventSignal(userId, eventId), userId);
+}
+
+void ProtocolManager::fileTransferAccept(const UserId& userId, unsigned short port,
+    unsigned long eventId, unsigned long flag1, unsigned long flag2,
+    const string& message, const string filename, unsigned long filesize,
+    bool viaServer)
+{
+  if (userId.protocolId() == LICQ_PPID)
+  {
+    unsigned long nMsgId[] = { flag1, flag2 };
+    gLicqDaemon->icqFileTransferAccept(userId, port, (unsigned short)eventId,
+        nMsgId, viaServer, message, filename, filesize);
+  }
+  else
+    pushProtoSignal(new LicqProtoSendEventReplySignal(userId, string(), true, port,
+        eventId, flag1, flag2, !viaServer), userId);
+}
+
+void ProtocolManager::fileTransferRefuse(const UserId& userId, const string& message,
+    unsigned long eventId, unsigned long flag1, unsigned long flag2, bool viaServer)
+{
+  if (userId.protocolId() == LICQ_PPID)
+  {
+    unsigned long msgId[] = { flag1, flag2 };
+    gLicqDaemon->icqFileTransferRefuse(userId, message, (unsigned short)eventId, msgId, viaServer);
+  }
+  else
+    pushProtoSignal(new LicqProtoSendEventReplySignal(userId, message, false,
+        eventId, flag1, flag2, !viaServer), userId);
+}
+
+unsigned long ProtocolManager::authorizeReply(const UserId& userId, bool grant, const string& message)
+{
+  unsigned long eventId = 0;
+
+  if (grant)
+  {
+    if (userId.protocolId() == LICQ_PPID)
+      eventId = gLicqDaemon->icqAuthorizeGrant(userId, message);
+    else
+      pushProtoSignal(new LicqProtoGrantAuthSignal(userId, message), userId);
+  }
+  else
+  {
+    if (userId.protocolId() == LICQ_PPID)
+      eventId = gLicqDaemon->icqAuthorizeRefuse(userId, message);
+    else
+      pushProtoSignal(new LicqProtoRefuseAuthSignal(userId, message), userId);
+  }
+
+  return eventId;
+}
+
+unsigned long ProtocolManager::requestUserInfo(const UserId& userId)
+{
+  unsigned long eventId = 0;
+
+  if (userId.protocolId() == LICQ_PPID)
+    eventId = gLicqDaemon->icqRequestMetaInfo(userId.accountId().c_str());
+  else
+    pushProtoSignal(new LicqProtoRequestInfo(userId), userId);
+
+  return eventId;
+}
+
+unsigned long ProtocolManager::updateOwnerInfo(const UserId& ownerId)
+{
+  string alias, firstName, lastName, email, address, city, state, zipCode;
+  string phoneNumber, faxNumber, cellNumber;
+  unsigned short countryCode;
+  bool hideEmail;
+
+  {
+    OwnerReadGuard owner(ownerId);
+    if (!owner.isLocked())
+      return 0;
+
+    alias = owner->getAlias();
+    firstName = owner->getFirstName();
+    firstName = owner->getLastName();
+    email = owner->getUserInfoString("Email1");
+    address = owner->getUserInfoString("Address");
+    city = owner->getUserInfoString("City");
+    state = owner->getUserInfoString("State");
+    zipCode = owner->getUserInfoString("Zipcode");
+    phoneNumber = owner->getUserInfoString("PhoneNumber");
+    faxNumber = owner->getUserInfoString("FaxNumber");
+    cellNumber = owner->getUserInfoString("CellularNumber");
+    countryCode = owner->getUserInfoUint("Country");
+    hideEmail = owner->getUserInfoBool("HideEmail");
+  }
+
+  unsigned long eventId = 0;
+
+  if (ownerId.protocolId() == LICQ_PPID)
+    eventId = gLicqDaemon->icqSetGeneralInfo(alias.c_str(), firstName.c_str(),
+        lastName.c_str(), email.c_str(), city.c_str(), state.c_str(),
+        phoneNumber.c_str(), faxNumber.c_str(), address.c_str(),
+        cellNumber.c_str(), zipCode.c_str(), countryCode, hideEmail);
+  else
+    pushProtoSignal(new LicqProtoUpdateInfoSignal(alias, firstName,
+        lastName, email, city, state, phoneNumber, faxNumber, address,
+        cellNumber, zipCode), ownerId);
+
+  return eventId;
+}
+
+unsigned long ProtocolManager::requestUserPicture(const UserId& userId)
+{
+  size_t iconHashSize;
+  bool sendServer;
+  {
+    UserReadGuard user(userId);
+    if (!user.isLocked())
+      return 0;
+
+    iconHashSize = strlen(user->BuddyIconHash());
+    sendServer = (user->SocketDesc(ICQ_CHNxINFO) < 0);
+  }
+
+  unsigned long eventId = 0;
+
+  if (userId.protocolId() == LICQ_PPID)
+    eventId = gLicqDaemon->icqRequestPicture(userId, sendServer, iconHashSize);
+  else
+    pushProtoSignal(new LicqProtoRequestPicture(userId), userId);
+
+  return eventId;
+}
+
+unsigned long ProtocolManager::secureChannelOpen(const UserId& userId)
+{
+  if (gUserManager.isOwner(userId))
+    return 0;
+
+  {
+    UserReadGuard user(userId);
+    if (!user.isLocked())
+    {
+      gLog.Warn(tr("%sCannot send secure channel request to user not on list (%s).\n"),
+          L_WARNxSTR, userId.toString().c_str());
+      return 0;
+    }
+
+    // Check that the user doesn't already have a secure channel
+    if (user->Secure())
+    {
+      gLog.Warn(tr("%s%s (%s) already has a secure channel.\n"), L_WARNxSTR,
+          user->getAlias().c_str(), userId.toString().c_str());
+      return 0;
+    }
+  }
+
+  unsigned long eventId = getNextEventId();
+
+  if (userId.protocolId() == LICQ_PPID)
+    gLicqDaemon->icqOpenSecureChannel(eventId, userId);
+  else
+    pushProtoSignal(new LicqProtoOpenSecureSignal(eventId, userId), userId);
+
+  return eventId;
+}
+
+unsigned long ProtocolManager::secureChannelClose(const UserId& userId)
+{
+  {
+    UserReadGuard user(userId);
+    if (!user.isLocked())
+    {
+      gLog.Warn(tr("%sCannot send secure channel request to user not on list (%s).\n"),
+          L_WARNxSTR, userId.toString().c_str());
+      return 0;
+    }
+
+    // Check that the user have a secure channel to close
+    if (!user->Secure())
+    {
+      gLog.Warn(tr("%s%s (%s) does not have a secure channel.\n"), L_WARNxSTR,
+          user->getAlias().c_str(), userId.toString().c_str());
+      return 0;
+    }
+  }
+
+  unsigned long eventId = getNextEventId();
+
+  if (userId.protocolId() == LICQ_PPID)
+    gLicqDaemon->icqCloseSecureChannel(eventId, userId);
+  else
+    pushProtoSignal(new LicqProtoCloseSecureSignal(eventId, userId), userId);
+
+  return eventId;
+}
+
+void ProtocolManager::secureChannelCancelOpen(const UserId& userId, unsigned long eventId)
+{
+  if (userId.protocolId() == LICQ_PPID)
+    gLicqDaemon->icqOpenSecureChannelCancel(userId, (unsigned short)eventId);
+  else
+    pushProtoSignal(new LicqProtoCancelEventSignal(userId, eventId), userId);
+}
+
+void ProtocolManager::visibleListSet(const UserId& userId, bool visible)
+{
+  if (visible)
+    if (userId.protocolId() == LICQ_PPID)
+      gLicqDaemon->icqAddToVisibleList(userId);
+    else
+      pushProtoSignal(new LicqProtoAcceptUserSignal(userId), userId);
+  else
+    if (userId.protocolId() == LICQ_PPID)
+      gLicqDaemon->icqRemoveFromVisibleList(userId);
+    else
+      pushProtoSignal(new LicqProtoUnacceptUserSignal(userId), userId);
+}
+
+void ProtocolManager::invisibleListSet(const UserId& userId, bool invisible)
+{
+  if (invisible)
+    if (userId.protocolId() == LICQ_PPID)
+      gLicqDaemon->icqAddToInvisibleList(userId);
+    else
+      pushProtoSignal(new LicqProtoBlockUserSignal(userId), userId);
+  else
+    if (userId.protocolId() == LICQ_PPID)
+      gLicqDaemon->icqRemoveFromInvisibleList(userId);
+    else
+      pushProtoSignal(new LicqProtoUnblockUserSignal(userId), userId);
+}
+
+void ProtocolManager::ignoreListSet(const UserId& userId, bool ignore)
+{
+  if (ignore)
+    if (userId.protocolId() == LICQ_PPID)
+      gLicqDaemon->icqAddToIgnoreList(userId);
+    else
+      pushProtoSignal(new LicqProtoIgnoreUserSignal(userId), userId);
+  else
+    if (userId.protocolId() == LICQ_PPID)
+      gLicqDaemon->icqRemoveFromIgnoreList(userId);
+    else
+      pushProtoSignal(new LicqProtoUnignoreUserSignal(userId), userId);
+}

licq/include/licq/CMakeLists.txt
@@ -12,6 +12,7 @@ set(licq_HEADERS
   plugin.h
   pluginlogsink.h
   pluginmanager.h
+  protocolmanager.h
   types.h
   userid.h
   version.h

licq/include/licq/protocolmanager.h
@@ -1,0 +1,232 @@
+/*
+ * This file is part of Licq, an instant messaging client for UNIX.
+ * Copyright (C) 2010 Licq developers
+ *
+ * Licq is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Licq is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Licq; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef LICQ_PROTOCOLMANAGER_H
+#define LICQ_PROTOCOLMANAGER_H
+
+#include <boost/noncopyable.hpp>
+#include <string>
+
+#include "../licq_filetransfer.h" // ConstFileList
+
+class CICQColor;
+
+
+namespace Licq
+{
+class UserId;
+
+/**
+ * Use this manager to communicate with protocol plugins
+ *
+ */
+class ProtocolManager : private boost::noncopyable
+{
+public:
+  static const char* const KeepAutoResponse;
+
+  /**
+   * Update user alias on server contact list
+   * Alias is taken from local contact list
+   *
+   * @param userId User to update
+   */
+  virtual void updateUserAlias(const UserId& userId) = 0;
+
+  /**
+   * Set status for a protocol
+   *
+   * @param ownerId Owner of protocol to change
+   * @param newStatus The status to change to
+   * @param message The status message to be set for the status
+   * @return Event id
+   */
+  virtual unsigned long setStatus(const UserId& ownerId,
+      unsigned short newStatus, const std::string& message = KeepAutoResponse) = 0;
+
+  /**
+   * Notify a user that we've started/stopped typing
+   *
+   * @param userId User to notify
+   * @param active True if we've started typing, false if we've stopped
+   * @param nSocket ?
+   */
+  virtual void sendTypingNotification(const UserId& userId, bool active, int nSocket = -1) = 0;
+
+  /**
+   * Send a normal message to a user
+   *
+   * @param userId User to send message to
+   * @param message The message to be sent
+   * @param viaServer True to send via server or false to use direct connection (ICQ only)
+   * @param flags Any special flags (ICQ only)
+   * @param multipleRecipients True if sending the same message to more than one user (ICQ only)
+   * @param color The color of the text and background (ICQ only)
+   * @param convoId Conversation ID for group messages (Non-ICQ only)
+   * @return Event id
+   */
+  virtual unsigned long sendMessage(const UserId& userId, const std::string& message,
+      bool viaServer, unsigned short flags, bool multipleRecipients = false,
+      CICQColor* color = NULL, unsigned long convoId = 0) = 0;
+
+  /**
+   * Send URL message to a user
+   *
+   * @param userId User to sent URL to
+   * @param url The URL to be sent
+   * @param message Message or description of URL
+   * @param viaServer True to send via server or false to use direct connection (ICQ only)
+   * @param flags Any special flags (ICQ only)
+   * @param multipleRecipients True if sending the same message to more than one user (ICQ only)
+   * @param color The color of the text and background (ICQ only)
+   * @return Event id
+   */
+  virtual unsigned long sendUrl(const UserId& userId, const std::string& url,
+      const std::string& message, bool viaServer, unsigned short flags,
+      bool multipleRecipients = false, CICQColor* color = NULL) = 0;
+
+  /**
+   * Request user auto response from server
+   *
+   * @param userId User to fetch auto response for
+   * @return Event id
+   */
+  virtual unsigned long requestUserAutoResponse(const UserId& userId) = 0;
+
+  /**
+   * Initiate a file transfer to a user
+   *
+   * @param userId User to send file to
+   * @param filename Name of file to send
+   * @param message Message or description of file(s)
+   * @param files List of files to send
+   * @param flags Any special flags (ICQ only)
+   * @param viaServer True to send via server or false to use direct connection (ICQ only)
+   * @return Event id
+   */
+  virtual unsigned long fileTransferPropose(const UserId& userId,
+      const std::string& filename, const std::string& message,
+      ConstFileList& files, unsigned short flags, bool viaServer) = 0;
+
+  /**
+   * Refuse a proposed file transfer
+   *
+   * @param userId User to send refusal to
+   * @param message Message with reason for refusal
+   * @param eventId Event id of a pending transfer
+   * @param flag1 ?
+   * @param flag2 ?
+   * @param viaServer True to send via server or false to use direct connection
+   */
+  virtual void fileTransferRefuse(const UserId& userId, const std::string& message,
+      unsigned long eventId, unsigned long flag1, unsigned long flag2,
+      bool viaServer = true) = 0;
+
+  /**
+   * Cancel a file transfer
+   *
+   * @param userId User to cancel transfer for
+   * @param eventId Event id of transfer to cancel
+   */
+  virtual void fileTransferCancel(const UserId& userId, unsigned long eventId) = 0;
+
+  /**
+   * Accept a proposed file transfer
+   *
+   * @param userId User to accept file transfer from
+   * @param port Local tcp port to use
+   * @param eventId Event id of transfer to accept
+   * @param flag1 ?
+   * @param flag2 ?
+   * @param message Description text from file transfer event (ICQ only)
+   * @param filename Filename from file transfer event (ICQ only)
+   * @param filesize File size from file transfer event (ICQ only)
+   * @param viaServer True to send via server or false to use direct connection
+   */
+  virtual void fileTransferAccept(const UserId& userId, unsigned short port,
+      unsigned long eventId = 0, unsigned long flag1 = 0, unsigned long flag2 = 0,
+      const std::string& message = "", const std::string filename = "",
+      unsigned long filesize = 0, bool viaServer = true) = 0;
+
+  /**
+   * Grant or refuse authorization for a user to add us
+   *
+   * @param userId User to send grant to
+   * @param grant True to grant or false to refuse
+   * @param message Message to send with grant
+   * @return Event id
+   */
+  virtual unsigned long authorizeReply(const UserId& userId, bool grant,
+      const std::string& message) = 0;
+
+  /**
+   * Request user information from server
+   *
+   * @param userId User to get information for
+   * @return Event id
+   */
+  virtual unsigned long requestUserInfo(const UserId& userId) = 0;
+
+  /**
+   * Update general user info for owner on server side
+   * User info is taken from local data
+   */
+  virtual unsigned long updateOwnerInfo(const UserId& ownerId) = 0;
+
+  /**
+   * Request user picture from server
+   *
+   * @param userId User to get picture for
+   * @return Event id
+   */
+  virtual unsigned long requestUserPicture(const UserId& userId) = 0;
+
+  /**
+   * Enable encrypted communication towards a user
+   *
+   * @param userId User to enable encryption for
+   * @return Event id
+   */
+  virtual unsigned long secureChannelOpen(const UserId& userId) = 0;
+
+  /**
+   * Disable encrypted communication towards a user
+   *
+   * @param userId User to disable encryption for
+   * @return Event id
+   */
+  virtual unsigned long secureChannelClose(const UserId& userId) = 0;
+
+  /**
+   * Cancel encrypted communication about to be enabled
+   *
+   * @param userId User to cancel encryption for
+   * @param eventId Event of open request to cancel
+   */
+  virtual void secureChannelCancelOpen(const UserId& userId, unsigned long eventId) = 0;
+
+protected:
+  virtual ~ProtocolManager() { /* Empty */ }
+};
+
+extern ProtocolManager& gProtocolManager;
+
+} // namespace Licq
+
+#endif

licq/include/licq_icqd.h
@@ -36,6 +36,7 @@ class CMSN;
 
 namespace LicqDaemon
 {
+class ProtocolManager;
 class UserManager;
 }
 
@@ -944,6 +945,7 @@ protected:
   friend class CChatManager;
   friend class CFileTransferManager;
   friend class COnEventManager;
+  friend class LicqDaemon::ProtocolManager;
   friend class LicqDaemon::UserManager;
   friend class CLicq;
   friend class CMSN;

licq/src/CMakeLists.txt
@@ -41,6 +41,7 @@ set(licq_SRCS
   message.cpp
   onevent.cpp
   oscarservice.cpp
+  protocolmanager.cpp
   proxy.cpp
   rtf.cc
   sar.cpp

licq/src/protocolmanager.h
@@ -1,0 +1,124 @@
+/*
+ * This file is part of Licq, an instant messaging client for UNIX.
+ * Copyright (C) 2010 Licq developers
+ *
+ * Licq is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Licq is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Licq; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef LICQDAEMON_PROTOCOLMANAGER_H
+#define LICQDAEMON_PROTOCOLMANAGER_H
+
+#include <licq/protocolmanager.h>
+
+class LicqProtoSignal;
+
+namespace LicqDaemon
+{
+
+class ProtocolManager : public Licq::ProtocolManager
+{
+public:
+  ProtocolManager();
+  ~ProtocolManager();
+
+  /**
+   * Add a user to server side list
+   * Used by UserManager after user has been added to local list.
+   * Plugins should call gUserManager.addUser() instead
+   *
+   * @param userId User to add
+   * @param groupId Initial group, only used for ICQ contacts
+   */
+  void addUser(const Licq::UserId& userId, int groupId);
+
+  /**
+   * Remove a user from the server side list
+   * Used by UserManager before removing user from local list.
+   * Plugins should call gUserManageer.removeUser() instead
+   *
+   * @param userId Id of user to remove
+   */
+  void removeUser(const Licq::UserId& userId);
+
+  /**
+   * Set visible list status for a contact
+   *
+   * @param userId User to change visible status for
+   * @param visible True to add user to visible list or false to remove
+   */
+  void visibleListSet(const Licq::UserId& userId, bool visible);
+
+  /**
+   * Set invisible list status for a contact
+   *
+   * @param userId User to change invisible status for
+   * @param invisible True to add user to invisible list or false to remove
+   */
+  void invisibleListSet(const Licq::UserId& userId, bool invisible);
+
+  /**
+   * Set ignore list status for a contact
+   *
+   * @param userId User to set ignore status for
+   * @param ignore True to add user to ignore list or false to remove
+   */
+  void ignoreListSet(const Licq::UserId& userId, bool ignore);
+
+
+  // From Licq::ProtocolManager
+  void updateUserAlias(const Licq::UserId& userId);
+  unsigned long setStatus(const Licq::UserId& ownerId,
+      unsigned short newStatus, const std::string& message = KeepAutoResponse);
+  void sendTypingNotification(const Licq::UserId& userId, bool active, int nSocket = -1);
+  unsigned long sendMessage(const Licq::UserId& userId, const std::string& message,
+      bool viaServer, unsigned short flags, bool multipleRecipients = false,
+      CICQColor* color = NULL, unsigned long convoId = 0);
+  unsigned long sendUrl(const Licq::UserId& userId, const std::string& url,
+      const std::string& message, bool viaServer, unsigned short flags,
+      bool multipleRecipients = false, CICQColor* color = NULL);
+  unsigned long requestUserAutoResponse(const Licq::UserId& userId);
+  unsigned long fileTransferPropose(const Licq::UserId& userId, const std::string& filename,
+      const std::string& message, ConstFileList& files, unsigned short flags,
+      bool viaServer);
+  void fileTransferRefuse(const Licq::UserId& userId, const std::string& message,
+      unsigned long eventId, unsigned long flag1, unsigned long flag2,
+      bool viaServer = true);
+  void fileTransferCancel(const Licq::UserId& userId, unsigned long eventId);
+  void fileTransferAccept(const Licq::UserId& userId, unsigned short port,
+      unsigned long eventId = 0, unsigned long flag1 = 0, unsigned long flag2 = 0,
+      const std::string& message = "", const std::string filename = "",
+      unsigned long filesize = 0, bool viaServer = true);
+  unsigned long authorizeReply(const Licq::UserId& userId, bool grant, const std::string& message);
+  unsigned long requestUserInfo(const Licq::UserId& userId);
+  unsigned long updateOwnerInfo(const Licq::UserId& ownerId);
+  unsigned long requestUserPicture(const Licq::UserId& userId);
+  unsigned long secureChannelOpen(const Licq::UserId& userId);
+  unsigned long secureChannelClose(const Licq::UserId& userId);
+  void secureChannelCancelOpen(const Licq::UserId& userId, unsigned long eventId);
+
+private:
+  /**
+   * Get next available id to use for an event
+   */
+  unsigned long getNextEventId();
+
+  void pushProtoSignal(LicqProtoSignal* s, const Licq::UserId& userId);
+};
+
+extern ProtocolManager gProtocolManager;
+
+} // namespace Licq
+
+#endif

commit 311ee7458696ee9c05a98c95533fdce90ea7b789

icq: Fix owner info sending firstname as lastname
Broken since: 502672abae00d9dec74ff11e1d3efc3f9e43bb5a

licq/src/protocolmanager.cpp
@@ -1,6 +1,6 @@
 /*
  * This file is part of Licq, an instant messaging client for UNIX.
- * Copyright (C) 2010 Licq developers
+ * Copyright (C) 2010-2011 Licq developers
  *
  * Licq is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -384,7 +384,7 @@ unsigned long ProtocolManager::updateOwnerInfo(const UserId& ownerId)
 
       alias = owner->getAlias();
       firstName = owner->getFirstName();
-      firstName = owner->getLastName();
+      lastName = owner->getLastName();
       email = owner->getUserInfoString("Email1");
       address = owner->getUserInfoString("Address");
       city = owner->getUserInfoString("City");
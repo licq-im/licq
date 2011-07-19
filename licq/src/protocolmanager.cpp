311ee7458696ee9c05a98c95533fdce90ea7b789
Anders Olofsson <flynd@licq.org>
2011-07-19 17:11:37 +0200

    icq: Fix owner info sending firstname as lastname
    Broken since: 502672abae00d9dec74ff11e1d3efc3f9e43bb5a

--- licq/src/protocolmanager.cpp
+++ licq/src/protocolmanager.cpp
@@ -1,6 +1,6 @@
 /*
  * This file is part of Licq, an instant messaging client for UNIX.
- * Copyright (C) 2010 Licq developers
+ * Copyright (C) 2010-2011 Licq developers
  *
  * Licq is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -384,7 +384,7 @@ unsigned long ProtocolManager::updateOwnerInfo(const UserId& ownerId)
 
       alias = owner->getAlias();
       firstName = owner->getFirstName();
-      firstName = owner->getLastName();
+      lastName = owner->getLastName();
       email = owner->getUserInfoString("Email1");
       address = owner->getUserInfoString("Address");
       city = owner->getUserInfoString("City");
