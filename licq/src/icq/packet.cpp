licq-im/licq

commit 3f73787bc525babc34db2ed2c3710c48a961eb48

Dropped the last uses of FetchOwner(), fetchOwner() and DropOwner() from gUserManager.

licq/src/icq/packet.cpp
@@ -1073,9 +1073,11 @@ CPU_SetPrivacy::CPU_SetPrivacy(unsigned char _cPrivacy)
 
   InitBuffer();
 
-  const ICQOwner* o = gUserManager.FetchOwner(LICQ_PPID, LOCK_R);
-  unsigned short nPDINFO = o->GetPDINFO();
-  gUserManager.DropOwner(o);
+  unsigned short nPDINFO;
+  {
+    Licq::OwnerReadGuard o(LICQ_PPID);
+    nPDINFO = o->GetPDINFO();
+  }
 
   buffer->PackUnsignedLongBE(0);
   buffer->PackUnsignedShortBE(nPDINFO);
@@ -1156,7 +1158,7 @@ void CPU_SetStatusFamily::InitBuffer()
   
   /* should eventually switch to some other way to identify licq, probably
      use capabilities */
-  const ICQOwner* o = gUserManager.FetchOwner(LICQ_PPID, LOCK_R);
+  Licq::OwnerReadGuard o(LICQ_PPID);
 #ifdef USE_OPENSSL
    buffer->PackUnsignedLongBE(LICQ_WITHSSL | LICQ_VERSION);
 #else
@@ -1165,7 +1167,6 @@ void CPU_SetStatusFamily::InitBuffer()
    // some kind of timestamp ?
   buffer->PackUnsignedLongBE(o->ClientStatusTimestamp());
   buffer->PackUnsignedLongBE(o->ClientInfoTimestamp());
-  gUserManager.DropOwner(o);
   buffer->PackUnsignedShortBE(0x0000);
 }
 
@@ -1180,15 +1181,12 @@ CPU_SetLogonStatus::CPU_SetLogonStatus(unsigned long _nNewStatus)
 CPU_UpdateInfoTimestamp::CPU_UpdateInfoTimestamp(const char *GUID)
   : CPU_SetStatusFamily()
 {
-  const ICQOwner* o = gUserManager.FetchOwner(LICQ_PPID, LOCK_R);
+  Licq::OwnerReadGuard o(LICQ_PPID);
   m_nNewStatus = o->StatusFull();
-  gUserManager.DropOwner(o);
 
   m_nSize += 4 + 1 + 4 + 6 + GUID_LENGTH + 4 + 1;
 
   InitBuffer();
-
-  o = gUserManager.FetchOwner(LICQ_PPID, LOCK_R);
 
   buffer->PackUnsignedLongBE(0x00110022); // TLV
   buffer->PackChar(2);                    // info update
@@ -1199,8 +1197,6 @@ CPU_UpdateInfoTimestamp::CPU_UpdateInfoTimestamp(const char *GUID)
   buffer->Pack(GUID, GUID_LENGTH);
   buffer->PackUnsignedLong(o->ClientInfoTimestamp());
   buffer->PackChar(0);                    //No info follows ??
-
-  gUserManager.DropOwner(o);
 }
 
 CPU_UpdateStatusTimestamp::CPU_UpdateStatusTimestamp(const char *GUID,
@@ -1208,15 +1204,12 @@ CPU_UpdateStatusTimestamp::CPU_UpdateStatusTimestamp(const char *GUID,
                                                      unsigned long nStatus)
   : CPU_SetStatusFamily()
 {
-  const ICQOwner* o = gUserManager.FetchOwner(LICQ_PPID, LOCK_R);
+  Licq::OwnerReadGuard o(LICQ_PPID);
   m_nNewStatus = nStatus != ICQ_STATUS_OFFLINE ? nStatus : o->StatusFull();
-  gUserManager.DropOwner(o);
 
   m_nSize += 4 + 1 + 4 + 6 + GUID_LENGTH + 1 + 4 + 4 + 6;
 
   InitBuffer();
-
-  o = gUserManager.FetchOwner(LICQ_PPID, LOCK_R);
 
   buffer->PackUnsignedLongBE(0x0011002C); // TLV
   buffer->PackChar(3);                    // info update
@@ -1231,28 +1224,21 @@ CPU_UpdateStatusTimestamp::CPU_UpdateStatusTimestamp(const char *GUID,
   buffer->PackUnsignedShort(0);           //Unknown
   buffer->PackUnsignedShort(0);           //Unknown
   buffer->PackUnsignedShort(1);           //Unknown
-
-  gUserManager.DropOwner(o);
 }
 
 CPU_UpdateTimestamp::CPU_UpdateTimestamp()
   : CPU_SetStatusFamily()
 {
-  const ICQOwner* o = gUserManager.FetchOwner(LICQ_PPID, LOCK_R);
+  Licq::OwnerReadGuard o(LICQ_PPID);
   m_nNewStatus = o->StatusFull();
-  gUserManager.DropOwner(o);
 
   m_nSize += 4 + 1 + 4;
 
   InitBuffer();
 
-  o = gUserManager.FetchOwner(LICQ_PPID, LOCK_R);
-
   buffer->PackUnsignedLongBE(0x00110005);         // TLV
   buffer->PackChar(0);                            // server info update
   buffer->PackUnsignedLong(o->ClientTimestamp());
-
-  gUserManager.DropOwner(o);
 }
 
 //-----GenericUinList-----------------------------------------------------------
@@ -1578,7 +1564,7 @@ void CPU_Type2Message::InitBuffer()
 {
 	CPU_CommonFamily::InitBuffer();
 
-  const ICQOwner* o = gUserManager.FetchOwner(LICQ_PPID, LOCK_R);
+  Licq::OwnerReadGuard o(LICQ_PPID);
 
 	int nUinLen = strlen(m_pUser->IdString());
 
@@ -1623,8 +1609,6 @@ void CPU_Type2Message::InitBuffer()
 	buffer->PackUnsignedShortBE(0x2711); // tlv - more message info
 	buffer->PackUnsignedShortBE(m_nSize - 29 - nUinLen - 36 - nDirectInfo -
           m_nExtraLen);
-
-  gUserManager.DropOwner(o);
 }
 
 //-----ReverseConnect-----------------------------------------------------------
@@ -1766,9 +1750,10 @@ CPU_InfoPluginListResp::CPU_InfoPluginListResp(const ICQUser *u,
   buffer->PackUnsignedShort(0);
   buffer->PackUnsignedShort(1);
 
-  const ICQOwner* o = gUserManager.FetchOwner(LICQ_PPID, LOCK_R);
-  buffer->PackUnsignedLong(o->ClientInfoTimestamp());
-  gUserManager.DropOwner(o);
+  {
+    Licq::OwnerReadGuard o(LICQ_PPID);
+    buffer->PackUnsignedLong(o->ClientInfoTimestamp());
+  }
   buffer->PackUnsignedLong(nLen);
   if (nLen != 0)
   {
@@ -1801,8 +1786,7 @@ CPU_InfoPhoneBookResp::CPU_InfoPhoneBookResp(const ICQUser* u, unsigned long nMs
   : CPU_AckThroughServer(u, nMsgID1, nMsgID2, nSequence, 0, true,
                          ICQ_TCPxMSG_URGENT2, PLUGIN_INFOxMANAGER)
 {
-
-  const ICQOwner* o = gUserManager.FetchOwner(LICQ_PPID, LOCK_R);
+  Licq::OwnerReadGuard o(LICQ_PPID);
   const Licq::ICQUserPhoneBook* book = o->GetPhoneBook();
 
   unsigned long num_entries;
@@ -1863,8 +1847,6 @@ CPU_InfoPhoneBookResp::CPU_InfoPhoneBookResp(const ICQUser* u, unsigned long nMs
     buffer->PackUnsignedLong(entry->nRemoveLeading0s);
     buffer->PackUnsignedLong(entry->nPublish);
   }
-
-  gUserManager.DropOwner(o);
 }
 
 //-----Send picture response-------------------------------------------------
@@ -1874,7 +1856,7 @@ CPU_InfoPictureResp::CPU_InfoPictureResp(const ICQUser* u, unsigned long nMsgID1
  : CPU_AckThroughServer(u, nMsgID1, nMsgID2, nSequence, 0, true,
                          ICQ_TCPxMSG_URGENT2, PLUGIN_INFOxMANAGER)
 {
-  const ICQOwner* o = gUserManager.FetchOwner(LICQ_PPID, LOCK_R);
+  Licq::OwnerReadGuard o(LICQ_PPID);
   string filename = o->pictureFileName();
   unsigned long nLen = 0, nFileLen = 0;
   int fd = -1;
@@ -1958,8 +1940,6 @@ CPU_InfoPictureResp::CPU_InfoPictureResp(const ICQUser* u, unsigned long nMsgID1
 
   if (fd != -1)
     close(fd);
-
-  gUserManager.DropOwner(o);
 }
 
 //-----Send status plugin request---------------------------------------------
@@ -2009,9 +1989,10 @@ CPU_StatusPluginListResp::CPU_StatusPluginListResp(const ICQUser* u,
   buffer->PackUnsignedLong(0);    //Unknown
   buffer->PackUnsignedLong(0);    //Unknown
   buffer->PackChar(1);            //Unknown
-  const ICQOwner* o = gUserManager.FetchOwner(LICQ_PPID, LOCK_R);
-  buffer->PackUnsignedLong(o->ClientStatusTimestamp());
-  gUserManager.DropOwner(o);
+  {
+    Licq::OwnerReadGuard o(LICQ_PPID);
+    buffer->PackUnsignedLong(o->ClientStatusTimestamp());
+  }
   buffer->PackUnsignedLong(nLen);  //Bytes remaining in packet
 
   if (nLen != 0)
@@ -2055,9 +2036,8 @@ CPU_StatusPluginResp::CPU_StatusPluginResp(const ICQUser* u, unsigned long nMsgI
   buffer->PackUnsignedShort(1);   //Unknown
 
   buffer->PackUnsignedLong(nStatus);
-  const ICQOwner* o = gUserManager.FetchOwner(LICQ_PPID, LOCK_R);
+  Licq::OwnerReadGuard o(LICQ_PPID);
   buffer->PackUnsignedLong(o->ClientStatusTimestamp());
-  gUserManager.DropOwner(o);
   buffer->PackChar(1);            //Unknown
 }
 
@@ -2098,11 +2078,13 @@ void CPU_AdvancedMessage::InitBuffer()
 {
   CPU_Type2Message::InitBuffer();
 
-  const ICQOwner* o = gUserManager.FetchOwner(LICQ_PPID, LOCK_R);
-  unsigned short nStatus = o->Status();
-  if (m_pUser->StatusToUser() != ICQ_STATUS_OFFLINE)
-    nStatus = m_pUser->StatusToUser();
-  gUserManager.DropOwner(o);
+  unsigned short nStatus;
+  {
+    Licq::OwnerReadGuard o(LICQ_PPID);
+    nStatus = o->Status();
+    if (m_pUser->StatusToUser() != ICQ_STATUS_OFFLINE)
+      nStatus = m_pUser->StatusToUser();
+  }
 
   // XXX Is this really a status? XXX
   // If this is non-zero, ICQ5 ignores our accept file ack.
@@ -2281,7 +2263,7 @@ CPU_AckThroughServer::CPU_AckThroughServer(const ICQUser* u,
   }
   else
   {
-    const ICQOwner* o = gUserManager.FetchOwner(LICQ_PPID, LOCK_R);
+    Licq::OwnerReadGuard o(LICQ_PPID);
     unsigned short s = o->Status();
     if (u->StatusToUser() != ICQ_STATUS_OFFLINE)
       s = u->StatusToUser();
@@ -2335,8 +2317,6 @@ CPU_AckThroughServer::CPU_AckThroughServer(const ICQUser* u,
     else
       m_szMessage = strdup("");
 
-    gUserManager.DropOwner(o);
-  
     // Check for pipes, should possibly go after the ClientToServer call
     m_szMessage = PipeInput(m_szMessage);
   
@@ -2504,12 +2484,11 @@ CPU_SendSms::CPU_SendSms(const char *szNumber, const char *szMessage)
   char szParsedNumber[17] = "+";
   ParseDigits(&szParsedNumber[1], szNumber, 15);
 
-  const ICQOwner* o = gUserManager.FetchOwner(LICQ_PPID, LOCK_R);
+  Licq::OwnerReadGuard o(LICQ_PPID);
 
   snprintf(szXmlStr, 460, "<icq_sms_message><destination>%s</destination><text>%.160s</text><codepage>1252</codepage><encoding>utf8</encoding><senders_UIN>%s</senders_UIN><senders_name>%s</senders_name><delivery_receipt>Yes</delivery_receipt><time>%s</time></icq_sms_message>",
 	   szParsedNumber, szMessage, o->IdString(), o->GetAlias(), szTime);
   szXmlStr[459] = '\0';
-  gUserManager.DropOwner(o);
 
   int nLenXmlStr = strlen_safe(szXmlStr) + 1;
   int packetSize = 2+2+2+4+2+2+2 + 22 + 2 + nLenXmlStr;
@@ -2603,10 +2582,9 @@ CPU_RequestList::CPU_RequestList()
   m_nSize += 6;
   InitBuffer();
 
-  const ICQOwner* o = gUserManager.FetchOwner(LICQ_PPID, LOCK_R);
+  Licq::OwnerReadGuard o(LICQ_PPID);
   buffer->PackUnsignedLongBE(o->GetSSTime());
   buffer->PackUnsignedShortBE(o->GetSSCount());
-  gUserManager.DropOwner(o);
 }
 
 //-----ExportContactStart-------------------------------------------------------
@@ -2794,10 +2772,9 @@ CPU_AddPDINFOToServerList::CPU_AddPDINFOToServerList()
   m_nSID = gUserManager.GenerateSID();
   m_nSize += 15;
   InitBuffer();
-  
-  ICQOwner *o = gUserManager.FetchOwner(LICQ_PPID, LOCK_W);
+
+  Licq::OwnerWriteGuard o(LICQ_PPID);
   o->SetPDINFO(m_nSID);
-  gUserManager.DropOwner(o);
 
   buffer->PackUnsignedShortBE(0);
   buffer->PackUnsignedShortBE(0);
@@ -4463,7 +4440,7 @@ CPacketTcp::CPacketTcp(unsigned long _nCommand, unsigned short _nSubCommand,
    size_t nLen)
 {
   // Setup the message type and status fields using our online status
-  const ICQOwner* o = gUserManager.FetchOwner(LICQ_PPID, LOCK_R);
+  Licq::OwnerReadGuard o(LICQ_PPID);
   unsigned short s = o->Status();
   if (user->StatusToUser() != ICQ_STATUS_OFFLINE) s = user->StatusToUser();
   m_nLevel = nLevel;
@@ -4546,7 +4523,7 @@ CPacketTcp::CPacketTcp(unsigned long _nCommand, unsigned short _nSubCommand,
       break;
     }
   }
-  gUserManager.DropOwner(o);
+  o.unlock();
 
   m_nSourceUin = gUserManager.icqOwnerUin();
   m_nCommand = _nCommand;
@@ -5037,7 +5014,7 @@ CPT_Ack::CPT_Ack(unsigned short _nSubCommand, unsigned short _nSequence,
 {
   m_nSequence = _nSequence;
   free(m_szMessage);
-  ICQOwner* o = gUserManager.FetchOwner(LICQ_PPID, LOCK_R);
+  Licq::OwnerReadGuard o(LICQ_PPID);
 
   // don't sent out AutoResponse if we're online
   // it could contain stuff the other site shouldn't be able to read
@@ -5068,8 +5045,6 @@ CPT_Ack::CPT_Ack(unsigned short _nSubCommand, unsigned short _nSequence,
   }
   else
     m_szMessage = strdup("");
-
-  gUserManager.DropOwner(o);
 
   // Check for pipes, should possibly go after the ClientToServer call
   m_szMessage = PipeInput(m_szMessage);
@@ -5385,9 +5360,8 @@ CPT_InfoPhoneBookResp::CPT_InfoPhoneBookResp(ICQUser *_cUser,
   unsigned short nSequence)
   : CPacketTcp(ICQ_CMDxTCP_ACK, 0, "\x01", true, ICQ_TCPxMSG_URGENT2, _cUser)
 {
-
-  ICQOwner* o = gUserManager.FetchOwner(LICQ_PPID, LOCK_R);
-  Licq::ICQUserPhoneBook* book = o->GetPhoneBook();
+  Licq::OwnerReadGuard o(LICQ_PPID);
+  const Licq::ICQUserPhoneBook* book = o->GetPhoneBook();
 
   unsigned long num_entries;
   unsigned long nLen = 4 + 4;
@@ -5449,8 +5423,6 @@ CPT_InfoPhoneBookResp::CPT_InfoPhoneBookResp(ICQUser *_cUser,
     buffer->PackUnsignedLong(entry->nPublish);
   }
 
-  gUserManager.DropOwner(o);
-
   PostBuffer();
 }
 
@@ -5459,7 +5431,7 @@ CPT_InfoPictureResp::CPT_InfoPictureResp(ICQUser *_cUser,
   unsigned short nSequence)
   : CPacketTcp(ICQ_CMDxTCP_ACK, 0, "\x01", true, ICQ_TCPxMSG_URGENT2, _cUser)
 {
-  ICQOwner* o = gUserManager.FetchOwner(LICQ_PPID, LOCK_R);
+  Licq::OwnerReadGuard o(LICQ_PPID);
   string filename = o->pictureFileName();
   unsigned long nLen = 0, nFileLen = 0;
   int fd = -1;
@@ -5544,8 +5516,6 @@ CPT_InfoPictureResp::CPT_InfoPictureResp(ICQUser *_cUser,
 
   if (fd != -1)
     close(fd);
-
-  gUserManager.DropOwner(o);
 }
 
 //----Reply to plugin list request----------------------------------------------
@@ -5574,9 +5544,10 @@ CPT_InfoPluginListResp::CPT_InfoPluginListResp(ICQUser *_cUser,
 
   buffer->PackUnsignedShort(0);   //Unknown
   buffer->PackUnsignedShort(1);   //Unknown
-  ICQOwner* o = gUserManager.FetchOwner(LICQ_PPID, LOCK_R);
-  buffer->PackUnsignedLong(o->ClientInfoTimestamp());
-  gUserManager.DropOwner(o);
+  {
+    Licq::OwnerReadGuard o(LICQ_PPID);
+    buffer->PackUnsignedLong(o->ClientInfoTimestamp());
+  }
   buffer->PackUnsignedLong(nLen);  //Bytes remaining in packet
   if (nLen != 0)
   {
@@ -5649,9 +5620,10 @@ CPT_StatusPluginListResp::CPT_StatusPluginListResp(ICQUser *_cUser,
   buffer->PackUnsignedLong(0);    //Unknown
   buffer->PackUnsignedLong(0);    //Unknown
   buffer->PackChar(1);            //Unknown
-  ICQOwner* o = gUserManager.FetchOwner(LICQ_PPID, LOCK_R);
-  buffer->PackUnsignedLong(o->ClientStatusTimestamp());
-  gUserManager.DropOwner(o);
+  {
+    Licq::OwnerReadGuard o(LICQ_PPID);
+    buffer->PackUnsignedLong(o->ClientStatusTimestamp());
+  }
   buffer->PackUnsignedLong(nLen);  //Bytes remaining in packet
   if (nLen != 0)
   {
@@ -5691,9 +5663,10 @@ CPT_StatusPluginResp::CPT_StatusPluginResp(ICQUser *_cUser,
   buffer->PackUnsignedShort(0);   //Unknown
   buffer->PackUnsignedShort(1);   //Unknown
   buffer->PackUnsignedLong(nStatus);
-  ICQOwner* o = gUserManager.FetchOwner(LICQ_PPID, LOCK_R);
-  buffer->PackUnsignedLong(o->ClientStatusTimestamp());
-  gUserManager.DropOwner(o);
+  {
+    Licq::OwnerReadGuard o(LICQ_PPID);
+    buffer->PackUnsignedLong(o->ClientStatusTimestamp());
+  }
   buffer->PackChar(1);            //Unknown
 
   PostBuffer();

licq/include/licq/contactlist/usermanager.h
@@ -132,27 +132,6 @@ public:
 
   virtual void RemoveOwner(unsigned long) = 0;
 
-  virtual Owner* FetchOwner(unsigned long ppid, unsigned short lockType) = 0;
-
-  /**
-   * Find and lock an owner object based on userId
-   *
-   * Note: Currently this is just a convenience wrapper but if/when Licq
-   *   starts supporting multiple owners per protocol this call will be needed
-   *   to be able to get any owner.
-   *
-   * @param userId User id of owner
-   * @param lockType Type of lock (LOCK_R or LOCK_W)
-   * @return The locked owner object if owner exists, otherwise NULL
-   */
-  Owner* fetchOwner(const UserId& userId, unsigned short lockType)
-  { return FetchOwner(User::getUserProtocolId(userId), lockType); }
-
-  /**
-   * Release owner lock
-   */
-  virtual void DropOwner(const Owner* owner) = 0;
-
   /**
    * Check if a user id is in the list
    *

licq/src/contactlist/usermanager.cpp
@@ -498,13 +498,11 @@ bool UserManager::userExists(const UserId& userId)
 
 UserId UserManager::ownerUserId(unsigned long ppid)
 {
-  const Licq::Owner* owner = FetchOwner(ppid, LOCK_R);
-  if (owner == NULL)
+  Licq::OwnerReadGuard owner(ppid);
+  if (!owner.isLocked())
     return UserId();
 
-  UserId ret = owner->id();
-  DropOwner(owner);
-  return ret;
+  return owner->id();
 }
 
 bool UserManager::isOwner(const UserId& userId)
@@ -525,7 +523,7 @@ bool UserManager::isOwner(const UserId& userId)
 
 unsigned long UserManager::icqOwnerUin()
 {
-  return strtoul(userOwnerId(LICQ_PPID).accountId().c_str(), (char**)NULL, 10);
+  return strtoul(ownerUserId(LICQ_PPID).accountId().c_str(), (char**)NULL, 10);
 }
 
 void UserManager::notifyUserUpdated(const UserId& userId, unsigned long subSignal)
@@ -587,11 +585,10 @@ int UserManager::AddGroup(const string& name, unsigned short icqGroupId)
   myGroupListMutex.unlockWrite();
 
   bool icqOnline = false;
-  Licq::Owner* icqOwner = FetchOwner(LICQ_PPID, LOCK_R);
-  if (icqOwner != NULL)
   {
-    icqOnline = icqOwner->isOnline();
-    DropOwner(icqOwner);
+    Licq::OwnerReadGuard icqOwner(LICQ_PPID);
+    if (icqOwner.isLocked())
+      icqOnline = icqOwner->isOnline();
   }
 
   if (icqGroupId == 0 && icqOnline)
@@ -865,9 +862,10 @@ unsigned short UserManager::GenerateSID()
   int nSID;
   unsigned short nOwnerPDINFO;
 
-  const Licq::Owner* o = gUserManager.FetchOwner(LICQ_PPID, LOCK_R);
-  nOwnerPDINFO = o->GetPDINFO();
-  gUserManager.DropOwner(o);
+  {
+    Licq::OwnerReadGuard o(LICQ_PPID);
+    nOwnerPDINFO = o->GetPDINFO();
+  }
 
   // Generate a SID
   srand(time(NULL));
@@ -929,27 +927,23 @@ void UserManager::dropUser(const Licq::User* u)
   u->Unlock();
 }
 
-Licq::Owner* UserManager::FetchOwner(unsigned long ppid, unsigned short lockType)
+Licq::Owner* UserManager::fetchOwner(unsigned long protocolId, bool writeLock)
 {
   Owner* o = NULL;
 
   myOwnerListMutex.lockRead();
-  OwnerMap::iterator iter = myOwners.find(ppid);
+  OwnerMap::iterator iter = myOwners.find(protocolId);
   if (iter != myOwners.end())
   {
     o = iter->second;
-    o->Lock(lockType);
+    if (writeLock)
+      o->lockWrite();
+    else
+      o->lockRead();
   }
   myOwnerListMutex.unlockRead();
 
   return o;
-}
-
-void UserManager::DropOwner(const Licq::Owner* owner)
-{
-  if (owner == NULL)
-    return;
-  owner->Unlock();
 }
 
 void UserManager::SaveAllUsers()
@@ -1118,25 +1112,25 @@ UserWriteGuard::UserWriteGuard(const UserId& userId, bool addUser, bool* retWasA
 }
 
 OwnerReadGuard::OwnerReadGuard(const UserId& userId)
-  : ReadMutexGuard<Owner>(gUserManager.fetchOwner(userId, LOCK_R), true)
+  : ReadMutexGuard<Owner>(LicqDaemon::gUserManager.fetchOwner(userId, false), true)
 {
   // Empty
 }
 
 OwnerReadGuard::OwnerReadGuard(unsigned long protocolId)
-  : ReadMutexGuard<Owner>(gUserManager.FetchOwner(protocolId, LOCK_R), true)
+  : ReadMutexGuard<Owner>(LicqDaemon::gUserManager.fetchOwner(protocolId, false), true)
 {
   // Empty
 }
 
 OwnerWriteGuard::OwnerWriteGuard(const UserId& userId)
-  : WriteMutexGuard<Owner>(gUserManager.fetchOwner(userId, LOCK_W), true)
+  : WriteMutexGuard<Owner>(LicqDaemon::gUserManager.fetchOwner(userId, true), true)
 {
   // Empty
 }
 
 OwnerWriteGuard::OwnerWriteGuard(unsigned long protocolId)
-  : WriteMutexGuard<Owner>(gUserManager.FetchOwner(protocolId, LOCK_W), true)
+  : WriteMutexGuard<Owner>(LicqDaemon::gUserManager.fetchOwner(protocolId, true), true)
 {
   // Empty
 }

licq/src/contactlist/usermanager.h
@@ -115,12 +115,32 @@ public:
 
   void dropUser(const Licq::User* user);
 
+  /**
+   * Fetch and lock an owner object based on protocolId
+   *
+   * @param protocolId Protocol to get owner for
+   * @param writeLock True to lock owner for writing, false for read lock
+   * @return The locked owner object if owner exists, otherwise NULL
+   */
+  Licq::Owner* fetchOwner(unsigned long protocolId, bool writeLock = false);
+
+  /**
+   * Find and lock an owner object based on userId
+   *
+   * Note: Currently this is just a convenience wrapper but if/when Licq
+   *   starts supporting multiple owners per protocol this call will be needed
+   *   to be able to get any owner.
+   *
+   * @param userId User id of owner
+   * @param writeLock True to lock owner for writing, false for read lock
+   * @return The locked owner object if owner exists, otherwise NULL
+   */
+  Licq::Owner* fetchOwner(const Licq::UserId& userId, bool writeLock = false)
+  { return fetchOwner(userId.protocolId(), writeLock); }
+
   // From Licq::UserManager
   void addOwner(const Licq::UserId& userId);
   void RemoveOwner(unsigned long);
-
-  Licq::Owner* FetchOwner(unsigned long ppid, unsigned short lockType);
-  void DropOwner(const Licq::Owner* owner);
   bool userExists(const Licq::UserId& userId);
   Licq::UserId ownerUserId(unsigned long ppid);
   bool isOwner(const Licq::UserId& userId);

licq/src/icq/icq.cpp
@@ -774,21 +774,19 @@ void CICQDaemon::ProcessDoneEvent(ICQEvent *e)
 
   case ICQ_CMDxSND_SETxSTATUS:
     if (e->m_eResult == EVENT_ACKED)
-    {
-        ICQOwner* o = gUserManager.FetchOwner(LICQ_PPID, LOCK_W);
+      {
+        Licq::OwnerWriteGuard o(LICQ_PPID);
       ChangeUserStatus(o, ((CPU_SetStatus *)e->m_pPacket)->Status() );
-        gUserManager.DropOwner(o);
-    }
+      }
     PushPluginEvent(e);
     break;
 
   case ICQ_CMDxSND_SETxRANDOMxCHAT:
     if (e->m_eResult == EVENT_ACKED)
-    {
-        ICQOwner* o = gUserManager.FetchOwner(LICQ_PPID, LOCK_W);
+      {
+        Licq::OwnerWriteGuard o(LICQ_PPID);
       o->SetRandomChatGroup(((CPU_SetRandomChatGroup *)e->m_pPacket)->Group());
-        gUserManager.DropOwner(o);
-    }
+      }
     PushPluginEvent(e);
     break;
 

licq/src/oneventmanager.cpp
@@ -166,37 +166,57 @@ void OnEventManager::performOnEvent(OnEventType event, const Licq::User* user)
         return;
     }
 
-    // Get owner for user
-    const Licq::Owner* owner;
-    if (user->isUser())
-      owner = gUserManager.FetchOwner(user->ppid(), LOCK_R);
-    else
-      owner = dynamic_cast<const Licq::Owner*>(user);
-    if (owner == NULL)
-      return;
-
     // Check if current status is reason to block on event action
-    bool blocked = false;
-    switch (owner->Status())
+    bool allow = true;
+    unsigned long ownerStatus;
+
+    if (user->isUser())
     {
-      case ICQ_STATUS_AWAY:
-        blocked = (!owner->AcceptInAway() && !user->AcceptInAway());
-        break;
-      case ICQ_STATUS_NA:
-        blocked = (!owner->AcceptInNA() && !user->AcceptInNA());
-        break;
-      case ICQ_STATUS_OCCUPIED:
-        blocked = (!owner->AcceptInOccupied() && !user->AcceptInOccupied());
-        break;
-      case ICQ_STATUS_DND:
-        blocked = (!owner->AcceptInDND() && !user->AcceptInDND());
-        break;
+      // User isn't owner, so check owner if it will accept
+      Licq::OwnerReadGuard o(user->ppid());
+      ownerStatus = o->Status();
+      switch (ownerStatus)
+      {
+        case ICQ_STATUS_AWAY:
+          allow = o->AcceptInAway();
+          break;
+        case ICQ_STATUS_NA:
+          allow = o->AcceptInNA();
+          break;
+        case ICQ_STATUS_OCCUPIED:
+          allow = o->AcceptInOccupied();
+          break;
+        case ICQ_STATUS_DND:
+          allow = o->AcceptInDND();
+          break;
+      }
+    }
+    else
+    {
+      ownerStatus = user->Status();
     }
 
-    if (user->isUser())
-      gUserManager.DropOwner(owner);
+    if (!allow || user->isUser())
+    {
+      // Either we are owner or owner didn't accept so check user
+      switch (ownerStatus)
+      {
+        case ICQ_STATUS_AWAY:
+          allow = user->AcceptInAway();
+          break;
+        case ICQ_STATUS_NA:
+          allow = user->AcceptInNA();
+          break;
+        case ICQ_STATUS_OCCUPIED:
+          allow = user->AcceptInOccupied();
+          break;
+        case ICQ_STATUS_DND:
+          allow = user->AcceptInDND();
+          break;
+      }
+    }
 
-    if (blocked)
+    if (!allow)
       return;
   }
 

commit d400ef69a0113eecbe3f01ea1fd85ed9a29c0d9c

icq: Fix double locking mutex when sending sms
Broken since: 3f73787bc525babc34db2ed2c3710c48a961eb48 (2010-06-23)

licq/src/icq/packet.cpp
@@ -2447,11 +2447,12 @@ CPU_SendSms::CPU_SendSms(const string& number, const string& message)
   char szParsedNumber[17] = "+";
   ParseDigits(&szParsedNumber[1], number.c_str(), 15);
 
-  Licq::OwnerReadGuard o(LICQ_PPID);
-
-  snprintf(szXmlStr, 460, "<icq_sms_message><destination>%s</destination><text>%.160s</text><codepage>1252</codepage><encoding>utf8</encoding><senders_UIN>%s</senders_UIN><senders_name>%s</senders_name><delivery_receipt>Yes</delivery_receipt><time>%s</time></icq_sms_message>",
-      szParsedNumber, message.c_str(), o->accountId().c_str(), o->getAlias().c_str(), szTime);
-  szXmlStr[459] = '\0';
+  {
+    Licq::OwnerReadGuard o(LICQ_PPID);
+    snprintf(szXmlStr, 460, "<icq_sms_message><destination>%s</destination><text>%.160s</text><codepage>1252</codepage><encoding>utf8</encoding><senders_UIN>%s</senders_UIN><senders_name>%s</senders_name><delivery_receipt>Yes</delivery_receipt><time>%s</time></icq_sms_message>",
+        szParsedNumber, message.c_str(), o->accountId().c_str(), o->getAlias().c_str(), szTime);
+    szXmlStr[459] = '\0';
+  }
 
   int nLenXmlStr = strlen_safe(szXmlStr) + 1;
   int packetSize = 2+2+2+4+2+2+2 + 22 + 2 + nLenXmlStr;