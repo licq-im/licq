licq-im/licq

commit 7ba64d8391bc9f471564b3e020080ede01638e54

api: Assign values to all enum constants
Have fixed values makes it easier to keep binary compatibility between
releases in case values are added or removed.

licq/include/licq/logging/log.h
@@ -40,20 +40,11 @@ public:
 
   enum Level
   {
-    /// Unknown packets or bytes.
-    Unknown,
-
-    /// Basic information about what's going on.
-    Info,
-
-    /// Warnings which are not critical but could be important.
-    Warning,
-
-    /// Critical errors which should be brought to the attention of the user.
-    Error,
-
-    /// Debugging aid.
-    Debug
+    Unknown     = 0, /// Unknown packets or bytes.
+    Debug       = 1, /// Debugging aid.
+    Info        = 2, /// Basic information about what's going on.
+    Warning     = 3, /// Warnings which are not critical but could be important.
+    Error       = 4, /// Critical errors which should be brought to the attention of the user.
   };
 
   class Stream;

licq/include/licq/contactlist/user.h
@@ -65,10 +65,10 @@ const unsigned int MAX_CATEGORIES = 4;
 
 typedef enum
 {
-  CAT_INTERESTS,
-  CAT_ORGANIZATION,
-  CAT_BACKGROUND,
-  CAT_MAX
+  CAT_INTERESTS         = 1,
+  CAT_ORGANIZATION      = 2,
+  CAT_BACKGROUND        = 3,
+  CAT_MAX               = 3,
 } UserCat;
 
 typedef std::vector <class UserEvent*> UserEventList;
@@ -101,24 +101,24 @@ const unsigned short MAX_PICTURE_SIZE      = 8081;
 
 enum EPhoneType
 {
-  TYPE_PHONE,
-  TYPE_CELLULAR,
-  TYPE_CELLULARxSMS,
-  TYPE_FAX,
-  TYPE_PAGER,
-  TYPE_MAX
+  TYPE_PHONE            = 1,
+  TYPE_CELLULAR         = 2,
+  TYPE_CELLULARxSMS     = 3,
+  TYPE_FAX              = 4,
+  TYPE_PAGER            = 5,
+  TYPE_MAX              = 5
 };
 
 enum EGatewayType
 {
   GATEWAY_BUILTIN = 1,
-  GATEWAY_CUSTOM
+  GATEWAY_CUSTOM = 2,
 };
 
 enum EPublish
 {
   PUBLISH_ENABLE = 1,
-  PUBLISH_DISABLE
+  PUBLISH_DISABLE = 2,
 };
 
 class ICQUserPhoneBook
@@ -328,9 +328,9 @@ public:
 
   enum usprintf_quotes
   {
-    usprintf_quotenone,
-    usprintf_quotepipe,
-    usprintf_quoteall,
+    usprintf_quotenone  = 0,
+    usprintf_quotepipe  = 1,
+    usprintf_quoteall   = 2,
   };
 
   /**
@@ -544,19 +544,19 @@ public:
 
   enum DirectFlags
   {
-    DirectDisabled,             // Direct contact not possible
-    DirectAnyone,               // Direct contact with anyone
-    DirectListed,               // Direct contact only with contacts in list
-    DirectAuth,                 // Direct contact only with authorized contacts
+    DirectDisabled      = 0,    // Direct contact not possible
+    DirectAnyone        = 1,    // Direct contact with anyone
+    DirectListed        = 2,    // Direct contact only with contacts in list
+    DirectAuth          = 3,    // Direct contact only with authorized contacts
   };
   unsigned directFlag() const                   { return myDirectFlag; }
   void setDirectFlag(unsigned n)                { myDirectFlag = n; }
 
   enum SecureChannelSupport
   {
-    SecureChannelUnknown,
-    SecureChannelNotSupported,
-    SecureChannelSupported,
+    SecureChannelUnknown        = 0,
+    SecureChannelNotSupported   = 1,
+    SecureChannelSupported      = 2,
   };
   unsigned secureChannelSupport() const         { return mySecureChannelSupport; }
   void setSecureChannelSupport(unsigned n)      { mySecureChannelSupport = n; }

licq/include/licq/event.h
@@ -155,26 +155,26 @@ class Event
 public:
   enum ConnectType
   {
-    ConnectServer,
-    ConnectUser,
-    ConnectNone,
+    ConnectNone         = 0,
+    ConnectServer       = 1,
+    ConnectUser         = 2,
   };
 
   enum ResultType
   {
-    ResultAcked,        // Event acked by reciepient
-    ResultSuccess,      // Event successfully sent
-    ResultFailed,       // Event failed
-    ResultTimedout,     // Time out while communicating with remote socket
-    ResultError,        // Other error
-    ResultCancelled,    // Event cancelled by the user
+    ResultAcked         = 1,    // Event acked by reciepient
+    ResultSuccess       = 2,    // Event successfully sent
+    ResultFailed        = 3,    // Event failed
+    ResultTimedout      = 4,    // Time out while communicating with remote socket
+    ResultError         = 5,    // Other error
+    ResultCancelled     = 6,    // Event cancelled by the user
   };
 
   enum SubResultType
   {
-    SubResultAccept,    // Event was accepted by recipient
-    SubResultRefuse,    // Event was rejected by recipient
-    SubResultReturn,    // Recipient is DND/Occupied, message needs to be resent with flags for urgent or to contact list
+    SubResultAccept     = 1,    // Event was accepted by recipient
+    SubResultRefuse     = 2,    // Event was rejected by recipient
+    SubResultReturn     = 3,    // Recipient is DND/Occupied, message needs to be resent with flags for urgent or to contact list
   };
 
   enum Commands

licq/include/licq/filter.h
@@ -47,9 +47,9 @@ struct FilterRule
   // Action to perform if rule matches
   enum ActionType
   {
-    ActionAccept,
-    ActionSilent,
-    ActionIgnore,
+    ActionAccept        = 1,
+    ActionSilent        = 2,
+    ActionIgnore        = 3,
   };
   int action;
 };

licq/include/licq/icq.h
@@ -49,7 +49,6 @@ void *UpdateUsers_tep(void *p);
 
 
 //=====CICQDaemon===============================================================
-enum EDaemonStatus {STATUS_ONLINE, STATUS_OFFLINE_MANUAL, STATUS_OFFLINE_FORCED };
 
 class CICQDaemon : private boost::noncopyable
 {
@@ -155,8 +154,6 @@ public:
 
   virtual void CheckExport() = 0;
 
-  EDaemonStatus Status() const                  { return m_eStatus; }
-
   virtual void UpdateAllUsers() = 0;
   virtual void updateAllUsersInGroup(int groupId) = 0;
 
@@ -194,8 +191,6 @@ public:
 
 protected:
   virtual ~CICQDaemon() { /* Empty */ }
-
-  EDaemonStatus m_eStatus;
 
   bool m_bAutoUpdateInfo, m_bAutoUpdateInfoPlugins, m_bAutoUpdateStatusPlugins;
 

licq/include/licq/proxy.h
@@ -1,6 +1,6 @@
 /*
  * This file is part of Licq, an instant messaging client for UNIX.
- * Copyright (C) 2010 Licq developers
+ * Copyright (C) 2010-2011 Licq developers
  *
  * Licq is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -71,9 +71,9 @@ protected:
 
   enum ErrorType
   {
-    ErrorNone,
-    ErrorErrno,
-    ErrorInternal,
+    ErrorNone           = 0,
+    ErrorErrno          = 1,
+    ErrorInternal       = 2,
   };
 
   ErrorType myErrorType;

licq/include/licq/socket.h
@@ -197,10 +197,10 @@ public:
 protected:
   enum ErrorType
   {
-    ErrorNone,
-    ErrorErrno,
-    ErrorInternal,
-    ErrorProxy,
+    ErrorNone           = 0,
+    ErrorErrno          = 1,
+    ErrorInternal       = 2,
+    ErrorProxy          = 3,
   };
 
   bool SetLocalAddress(bool bIp = true);
@@ -262,10 +262,10 @@ public:
 
   enum ChannelType
   {
-    ChannelUnknown,
-    ChannelNormal,
-    ChannelInfo,
-    ChannelStatus,
+    ChannelUnknown      = 0,
+    ChannelNormal       = 1,
+    ChannelInfo         = 2,
+    ChannelStatus       = 3,
   };
 
   void setChannel(int channel) { myChannel = channel; }

licq/include/licq/utility.h
@@ -1,6 +1,6 @@
 /*
  * This file is part of Licq, an instant messaging client for UNIX.
- * Copyright (C) 2000-2010 Licq developers
+ * Copyright (C) 2000-2011 Licq developers
  *
  * Licq is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -72,7 +72,12 @@ protected:
 class Utility
 {
 public:
-  enum WinType { WinLicq, WinTerm, WinGui };
+  enum WinType
+  {
+    WinLicq     = 1,
+    WinTerm     = 2,
+    WinGui      = 3,
+  };
 
   Utility(const std::string& filename);
   ~Utility();

commit 448ab53141f43fb0adcbd114e64c7c2feafcb391

Fix failing unit test
Broken since 7ba64d83

licq/include/licq/logging/log.h
@@ -40,11 +40,11 @@ public:
 
   enum Level
   {
-    Unknown     = 0, /// Unknown packets or bytes.
-    Debug       = 1, /// Debugging aid.
+    Unknown     = 1, /// Unknown packets or bytes.
     Info        = 2, /// Basic information about what's going on.
     Warning     = 3, /// Warnings which are not critical but could be important.
     Error       = 4, /// Critical errors which should be brought to the attention of the user.
+    Debug       = 5, /// Debugging aid.
   };
 
   class Stream;