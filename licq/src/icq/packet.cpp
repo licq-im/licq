63fa18806abe8408638a43764c19fa41d12be91a
Anders Olofsson <flynd@licq.org>
2012-06-02 16:56:04 +0200

    icq: Include utf8 marker in direct messages

--- licq/src/icq/packet.cpp
+++ licq/src/icq/packet.cpp
@@ -1,8 +1,20 @@
-/* ----------------------------------------------------------------------------
- * Licq - A ICQ Client for Unix
- * Copyright (C) 1998-2011 Licq developers
+/*
+ * This file is part of Licq, an instant messaging client for UNIX.
+ * Copyright (C) 1998-2012 Licq developers <licq-dev@googlegroups.com>
  *
- * This program is licensed under the terms found in the LICENSE file.
+ * Licq is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Licq is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Licq; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 #include "config.h"
@@ -4544,6 +4556,9 @@ CPT_Message::CPT_Message(const string& message, unsigned short nLevel, bool bMR,
         Licq::TCPSocket::ChannelNormal,
         message, true, nLevel, pUser)
 {
+  if (m_nVersion >= 6 && isUtf8)
+    m_nSize += 4 + sizeof(ICQ_CAPABILITY_UTF8_STR)-1;
+
   InitBuffer();
   if (m_nVersion >= 6)
   {
