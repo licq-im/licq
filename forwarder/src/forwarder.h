9a2d6b689f0e2d7043b524f4383ff2e52145d3e3
Anders Olofsson <flynd@licq.org>
2012-06-30 21:52:17 +0200

    forwarder: Cleanup after previous commit

--- forwarder/src/forwarder.h
+++ forwarder/src/forwarder.h
@@ -1,6 +1,6 @@
 /*
  * This file is part of Licq, an instant messaging client for UNIX.
- * Copyright (C) 2000-2011 Licq developers
+ * Copyright (C) 2000-2012 Licq developers <licq-dev@googlegroups.com>
  *
  * Licq is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -85,7 +85,6 @@ public:
   bool ForwardEvent_Email(const Licq::User* u, const Licq::UserEvent* e);
 
 private:
-  bool CreateDefaultConfig();
 
 };
 
