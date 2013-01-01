e8a059ec8928cde3c785ff71c3a1dada61d4a46b
Anders Olofsson <flynd@licq.org>
2013-01-01 11:05:58 +0100

    Remove console plugin
    As "discussed" on the licq-dev mailing list, the console plugin is in
    bad shape and noone is willing to maintain it any longer.
    As of now, it is no longer maintained as a part of Licq.
    
    Rather than keeping dead code around, I've removed all the files. If
    anyone would ever like to revive it, the code can be brought back from
    an older commit.

--- licq/src/licq.cpp
+++ licq/src/licq.cpp
@@ -1,6 +1,6 @@
 /*
  * This file is part of Licq, an instant messaging client for UNIX.
- * Copyright (C) 1998-2012 Licq developers <licq-dev@googlegroups.com>
+ * Copyright (C) 1998-2013 Licq developers <licq-dev@googlegroups.com>
  *
  * Licq is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -592,7 +592,7 @@ bool CLicq::Init(int argc, char **argv)
     }
     else  // If no plugins, try some defaults one by one
     {
-      const char* plugins[] = {"qt4-gui", "kde4-gui", "console"};
+      const char* plugins[] = {"qt4-gui", "kde4-gui"};
       unsigned short i = 0, size = sizeof(plugins) / sizeof(char*);
 
       GeneralPlugin::Ptr plugin;
