fa9224eeb8d50f58a5dd8042759a78e53911f094
Erik Johansson <erijo@licq.org>
2013-01-26 20:28:12 +0100

    api: add new help classes for plugins
    Implements part of the interface to make it easier to implement plugins.

---
+++ licq/include/licq/plugin/generalpluginhelper.h
@@ -0,0 +1,117 @@
+/*
+ * This file is part of Licq, an instant messaging client for UNIX.
+ * Copyright (C) 2013 Licq Developers <licq-dev@googlegroups.com>
+ *
+ * Please refer to the COPYRIGHT file distributed with this source
+ * distribution for the names of the individual contributors.
+ *
+ * Licq is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Licq is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Licq; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef LICQ_GENERALPLUGINHELPER_H
+#define LICQ_GENERALPLUGINHELPER_H
+
+#include "generalplugininterface.h"
+#include "../macro.h"
+
+namespace Licq
+{
+
+/**
+ * Implements part of the GeneralPluginInterface to help make the
+ * implementation of a general plugin easier.
+ */
+class GeneralPluginHelper : public GeneralPluginInterface
+{
+public:
+  // Notification that general plugins can get via its pipe
+  static const char PipeDisable = '0';
+  static const char PipeEnable = '1';
+  static const char PipeEvent = 'E';
+  static const char PipeShutdown = 'X';
+  static const char PipeSignal = 'S';
+
+  /// Returns true without doing anything
+  bool init(int argc, char** argv);
+
+  /// Writes PipeShutdown to the pipe
+  void shutdown();
+
+  /// Returns an empty string
+  std::string configFile() const;
+
+  /// Writes PipeEnable to the pipe
+  void enable();
+
+  /// Writes PipeDisable to the pipe
+  void disable();
+
+  /// Returns true if signalType is in the signal mask set by setSignalMask
+  bool wantSignal(unsigned long signalType) const;
+
+  /// Queues the signal and writes PipeSignal to the pipe
+  void pushSignal(PluginSignal* signal);
+
+  // Queues the event and writes PipeEvent to the pipe
+  void pushEvent(Event* event);
+
+protected:
+  GeneralPluginHelper();
+
+  virtual ~GeneralPluginHelper();
+
+  /**
+   * Get read end of pipe used to communicate with the plugin.
+   *
+   * @return A file descriptor that can be polled for new signals and events.
+   */
+  int getReadPipe() const;
+
+  /**
+   * Specify which signals to forward to plugin
+   *
+   * @param signalMask Mask of signals from PluginSignal::SignalType to accept
+   */
+  void setSignalMask(unsigned long signalMask);
+
+  /**
+   * Get a signal from the signal queue
+   *
+   * The plugin must call this function to fetch a signal after getting
+   * notified via its pipe. The signal must be deleted by the plugin after
+   * processing.
+   *
+   * @return The oldest signal on the queue, or NULL if queue is empty
+   */
+  PluginSignal* popSignal();
+
+  /**
+   * Get an event from the event queue
+   *
+   * The plugin must call this function to fetch an event after getting
+   * notified via its pipe. The event must be deleted by the plugin after
+   * processing.
+   *
+   * @return The oldest event on the queue, or NULL if queue is empty
+   */
+  Event* popEvent();
+
+private:
+  LICQ_DECLARE_PRIVATE();
+};
+
+} // namespace Licq
+
+#endif
---
+++ licq/include/licq/plugin/protocolpluginhelper.h
@@ -0,0 +1,88 @@
+/*
+ * This file is part of Licq, an instant messaging client for UNIX.
+ * Copyright (C) 2013 Licq Developers <licq-dev@googlegroups.com>
+ *
+ * Please refer to the COPYRIGHT file distributed with this source
+ * distribution for the names of the individual contributors.
+ *
+ * Licq is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Licq is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Licq; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef LICQ_PROTOCOLPLUGINHELPER_H
+#define LICQ_PROTOCOLPLUGINHELPER_H
+
+#include "protocolplugininterface.h"
+#include "../macro.h"
+
+namespace Licq
+{
+
+/**
+ * Implements part of the ProtocolPluginInterface to help make the
+ * implementation of a protocol plugin easier.
+ */
+class ProtocolPluginHelper : public ProtocolPluginInterface
+{
+public:
+  // Notification that protocols can get via its pipe
+  static const char PipeShutdown = 'X';
+  static const char PipeSignal = 'S';
+
+  /// Returns true without doing anything
+  bool init(int argc, char** argv);
+
+  /// Writes PipeShutdown to the pipe
+  void shutdown();
+
+  /// Queues the signal and writes PipeSignal to the pipe
+  void pushSignal(ProtocolSignal* signal);
+
+  /// Returns NULL to make UserManager create a Licq::User
+  User* createUser(const UserId& id, bool temporary);
+
+  // Returns NULL to make UserManager create a Licq::Owner
+  Owner* createOwner(const UserId& id);
+
+protected:
+  ProtocolPluginHelper();
+
+  virtual ~ProtocolPluginHelper();
+
+  /**
+   * Get read end of pipe used to communicate with the plugin.
+   *
+   * @return A file descriptor that can be polled for new signals.
+   */
+  int getReadPipe() const;
+
+  /**
+   * Get a signal from the signal queue
+   * Called from protocol plugin
+   *
+   * The plugin must call this function to fetch a signal after getting
+   * notified via its pipe. The signal must be deleted by the plugin after
+   * processing.
+   *
+   * @return The oldest signal on the queue, or NULL if queue is empty
+   */
+  ProtocolSignal* popSignal();
+
+private:
+  LICQ_DECLARE_PRIVATE();
+};
+
+} // namespace Licq
+
+#endif
