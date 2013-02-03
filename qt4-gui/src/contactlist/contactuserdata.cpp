f427d2038bacb9ef9a7517d6765f7a459d5dd133
Erik Johansson <erijo@licq.org>
2013-02-03 14:29:05 +0100

    qt4-gui: initialize variables in the non-icq case
    Avoids valgrind warnings about branching on uninitialized variables.

--- qt4-gui/src/contactlist/contactuserdata.cpp
+++ qt4-gui/src/contactlist/contactuserdata.cpp
@@ -1,6 +1,6 @@
 /*
  * This file is part of Licq, an instant messaging client for UNIX.
- * Copyright (C) 2007-2012 Licq developers <licq-dev@googlegroups.com>
+ * Copyright (C) 2007-2013 Licq developers <licq-dev@googlegroups.com>
  *
  * Licq is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -181,6 +181,12 @@ void ContactUserData::update(const Licq::User* u, unsigned long subSignal)
       myIcqPhoneStatus = icquser->icqPhoneStatus();
       mySharedFilesStatus = icquser->sharedFilesStatus();
     }
+  }
+  else
+  {
+    myPhoneFollowMeStatus = Licq::IcqPluginInactive;
+    myIcqPhoneStatus = Licq::IcqPluginInactive;
+    mySharedFilesStatus = Licq::IcqPluginInactive;
   }
 
   updateExtendedStatus();
