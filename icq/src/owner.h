f864c9680684ef49607fbb34d9fb00cc160dcaef
Erik Johansson <erijo@licq.org>
2013-02-03 14:40:27 +0100

    icq: remove duplicate variable definition

--- icq/src/owner.h
+++ icq/src/owner.h
@@ -1,6 +1,6 @@
 /*
  * This file is part of Licq, an instant messaging client for UNIX.
- * Copyright (C) 2012 Licq developers <licq-dev@googlegroups.com>
+ * Copyright (C) 2012-2013 Licq developers <licq-dev@googlegroups.com>
  *
  * Licq is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -58,7 +58,6 @@ public:
 private:
   virtual void saveOwnerInfo();
 
-  bool m_bWebAware;
   unsigned mySsCount;
   time_t m_nSSTime;
   unsigned myPDINFO;
