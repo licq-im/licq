472811cf027f2183f74801c0e407578c9daa38da
Anders Olofsson <flynd@licq.org>
2013-08-25 11:47:15 +0200

    Update version for Licq 1.9.0-dev

--- licq/cmake/LicqVersion.cmake
+++ licq/cmake/LicqVersion.cmake
@@ -4,7 +4,7 @@
 
 # Licq version
 set(LICQ_VERSION_MAJOR 1)          # 0 <= major   <= 64
-set(LICQ_VERSION_MINOR 8)          # 0 <= minor   <= 99
+set(LICQ_VERSION_MINOR 9)          # 0 <= minor   <= 99
 set(LICQ_VERSION_RELEASE 0)        # 0 <= release <=  9
 set(LICQ_VERSION_EXTRA "-dev")     # Any string
 
