d2e7415312cb60995e203a492d2edc91706741c1
Erik Johansson <erijo@licq.org>
2014-04-06 13:48:24 +0200

    Add missing break in switch

--- icq/src/icq.cpp
+++ icq/src/icq.cpp
@@ -1,6 +1,6 @@
 /*
  * This file is part of Licq, an instant messaging client for UNIX.
- * Copyright (C) 1998-2013 Licq developers <licq-dev@googlegroups.com>
+ * Copyright (C) 1998-2014 Licq developers <licq-dev@googlegroups.com>
  *
  * Licq is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -376,6 +376,7 @@ void IcqProtocol::processSignal(const Licq::ProtocolSignal* s)
           // If we get here something is broken
           assert(false);
       }
+      break;
     }
     default:
     {
