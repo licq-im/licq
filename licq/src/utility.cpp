ec6951bd6643478a2687ef49196b163485e06307
Erik Johansson <erijo@licq.org>
2014-04-06 13:36:09 +0200

    Fix memory leak in error path

--- licq/src/utility.cpp
+++ licq/src/utility.cpp
@@ -1,6 +1,6 @@
 /*
  * This file is part of Licq, an instant messaging client for UNIX.
- * Copyright (C) 1998-2013 Licq developers <licq-dev@googlegroups.com>
+ * Copyright (C) 1998-2014 Licq developers <licq-dev@googlegroups.com>
  *
  * Licq is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -92,6 +92,7 @@ int UtilityManager::loadUtilities(const string& dirname)
     if (p->isFailed())
     {
       gLog.warning(tr("Warning: unable to load utility \"%s\""), res->d_name);
+      delete p;
       continue;
     }
     myUtilities.push_back(p);
